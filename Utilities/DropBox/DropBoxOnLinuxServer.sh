7aa5f4aab351ec0093862304ee188d5d722b0e74
Charles Calvert <java@elvenware.com>
2013-02-13 14:52:57 -0800

    Updating DropBox script

--- Utilities/DropBox/DropBoxOnLinuxServer.sh
+++ Utilities/DropBox/DropBoxOnLinuxServer.sh
@@ -1,4 +1,16 @@
+# Script for downloading and installing DropBox on Linux server
+# and on other headless Linux boxes that have no GUI frontend.
+# You can use dropbox.py to help you manage dropbox.
+
 dropBoxName=dropbox.tar.gz
+wget -O dropbox.py "https://www.dropbox.com/download?dl=packages/dropbox.py
 wget -O $dropBoxName "https://www.dropbox.com/download?plat=lnx.x86"
 tar xzf $dropBoxName
-# ~/.dropbox-dist/dropboxd
+if [ ! -e ~/.dropbox-dist/dropboxd ]
+then
+	mv .dropbox-dist ~/.
+else
+	echo "I see that /home/$USER/.dropbox-dist already exists."
+	echo "I didn't expect that. I'm going to exit and let you"
+	echo "handle this manually."
+fi
