82f83b9fe1a5bb278041e287da0a5a6d7eee6750
Charles Calvert <java@elvenware.com>
2013-02-21 12:19:37 -0800

    Adding jsonRead

---
+++ JavaScriptObjects/NodeCode/jsonRead/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+var port = process.env.PORT || 30025;
+
+app.get('/', function(req, res) {
+	var html = fs.readFileSync('public/index.html');
+	res.writeHeader(200, {"Content-Type": "text/html"});
+	res.end(html);
+});
+
+
+app.get('/read', function(req, res){
+	var obj;
+	
+	function readData(err, data) {
+		if (err) throw err;
+		obj = JSON.parse(data);
+		res.send(obj);
+	}
+
+	// Asynch call 
+	fs.readFile('presidents.json', 'utf8', readData);
+});
+
+app.use("/public", express.static(__dirname + '/public'));
+
+app.listen(port);
+console.log('Listening on port :' + port);
