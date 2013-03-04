eac52652eb747ad1cea1b048e13176ae95675b99
charlie <java@elvenware.com>
2013-03-04 11:13:24 -0800

    Adding Example 3, a readme and License for the whole project

---
+++ JavaScriptObjects/DataUsers/Example03/assets/www/js/index.js
@@ -0,0 +1,51 @@
+/*jshint devel: true, browser: true, jquery: true, strict: true */
+/*global App: false */
+
+App.Main = (function() {'use strict';
+
+	var util = new App.Utility();
+	
+	// Application Constructor
+	function Main() {
+		console.log("debug: App constructor called");
+		bindEvents();
+	}
+
+	/*
+	 * Bind any events that are required on startup.
+	 * Common events: 'load', 'deviceready', 'offline', and 'online'.
+	 */
+	var bindEvents = function() {
+		console.log("debug: App bindEvents called");
+		document.addEventListener('deviceready', onDeviceReady, false);
+		$("#data01").load("data.html #div01");
+		$("#select01").on("change", util.changer);
+	};
+
+	// Called when device is fully initialized
+	var onDeviceReady = function() {
+		console.log("debug: App onDeviceReady called");
+		showProgramState();
+		showDate();
+	};
+
+	var showProgramState = function() {
+		var listeningElement = $('.listening');
+		var receivedElement = $('.received');
+		listeningElement.attr('style', 'display:none;');
+		receivedElement.attr('style', 'display:block;');
+	};
+
+	var showDate = function() {
+		console.log('debug: showDate called');
+		
+		$("#curDate").html("Current Date: " + util.getToday());
+	};
+
+	return Main;
+})();
+
+$(document).ready(function() {"use strict";
+	new App.Main();
+
+});
---
+++ JavaScriptObjects/DataUsers/README.md
@@ -0,0 +1,10 @@
+Data Users
+==========
+
+There are currently two examples:
+
+-   Example 1: Web Application that loads an HTML file and uses it to insert
+    data into index.html.
+
+-   Example 2: Web application that loads an JSON file and uses it to insert
+    data into index.html.
