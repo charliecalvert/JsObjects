4d57cd9d25c4ecb429693a173bee0a9bb80b3f0a
charlie <java@elvenware.com>
2013-03-04 13:42:33 -0800

    Adding Example04, updating .gitignore

--- JavaScriptObjects/DataUsers/Example02/index.js
+++ JavaScriptObjects/DataUsers/Example02/index.js
@@ -18,5 +18,11 @@ $(document).ready(function() {"use strict";
      
     $.getJSON("index.json", function(json) {
         $.each(json, usePresident);
-    });
+    }).success(function() {
+			console.log("csc: success. Loaded index.json");
+		}).error(function(jqXHR, textStatus, errorThrown) {
+			alert("error calling JSON:" + errorThrown + ". Try JSONLint or JSLint: " + textStatus);
+		}).complete(function() {
+			console.log("csc: completed call to get index.json");
+		});
 }); 
