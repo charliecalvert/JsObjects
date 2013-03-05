f55c3c89ab2336f2443821a7ad505e2ba16dd15e
Charles Calvert <charlie@ShantiServer03.(none)>
2013-03-05 08:26:52 -0800

    Refining SetupLinux.sh

--- Utilities/SetupLinuxBox/SetupLinux.sh
+++ Utilities/SetupLinuxBox/SetupLinux.sh
@@ -6,25 +6,26 @@
 
 User=$ELF_USER
 SshUser=$User@$ELF_IP
+HomeDir=/home/$ELF_USER
 CreateBin=CreateBin.sh
 SetupGitScript=SetupGit.sh
 SetupSshScript=SetupSsh.sh
 SetupNodeScript=SetupNode.sh
 
 # Create Bin
-scp $CreateBin $SshUser:$Home/$CreateBin
-ssh -t $SshUser /bin/chmod +x $Home/$CreateBin
-ssh -t $SshUser $Home/$CreateBin
-ssh -t $SshUser /bin/rm $Home/$CreateBin
+scp $CreateBin $SshUser:$HomeDir/$CreateBin
+ssh $SshUser /bin/chmod +x $HomeDir/$CreateBin
+ssh $SshUser $HomeDir/$CreateBin
+ssh $SshUser /bin/rm $HomeDir/$CreateBin
 
 # Copy Scripts
-scp $SetupSshScript $SshUser:$Home/bin/$SetupSshScript
-scp $SetupGitScript $SshUser:$Home/bin/$SetupGitScript
-scp $SetupNodeScript $SshUser:$Home/bin/$SetupNodeScript
-ssh -t $SshUser /bin/chmod +x $Home/bin/$SetupSshScript
-ssh -t $SshUser /bin/chmod +x $Home/bin/$SetupGitScript
-ssh -t $SshUser /bin/chmod +x $Home/bin/$SetupNodeScript
+scp $SetupSshScript $SshUser:$HomeDir/bin/$SetupSshScript
+scp $SetupGitScript $SshUser:$HomeDir/bin/$SetupGitScript
+scp $SetupNodeScript $SshUser:$HomeDir/bin/$SetupNodeScript
+ssh $SshUser /bin/chmod +x $HomeDir/bin/$SetupSshScript
+ssh $SshUser /bin/chmod +x $HomeDir/bin/$SetupGitScript
+ssh $SshUser /bin/chmod +x $HomeDir/bin/$SetupNodeScript
 
 # Run Scripts 
-ssh -t $SshUser $Home/bin/$SetupNodeScript
-ssh -t $SshUser $Home/bin/$SetupGitScript
+ssh $SshUser $HomeDir/bin/$SetupNodeScript
+ssh $SshUser $HomeDir/bin/$SetupGitScript
