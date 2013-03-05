bfc602047ffe9ea9e06d08616e1fefc2d2f81575
charlie <java@elvenware.com>
2013-03-05 11:56:12 -0800

    Setup Git For Histroy Code, updated SetupLinux.bat, new name for SetupGit for Pres Test

---
+++ Utilities/SetupLinuxBox/SetupGitForHistoryCode.sh
@@ -0,0 +1,21 @@
+GIT_DIR=~/Git
+HISTORY_CODE_DIR=historycode
+HISTORY_CODE_LINK=~/HistoryCode
+
+if [ ! -d "$GIT_DIR" ]; then
+	mkdir $GIT_DIR
+fi
+
+cd $GIT_DIR
+
+if [ ! -d "$HISTORY_CODE_DIR" ]; then
+	git clone https://ccalvert@bitbucket.org/ccalvert/historycode.git
+fi
+
+if [ ! -L "$HISTORY_CODE_LINK" ]; then
+	ln -s ~/Git/historycode/Presidents/ $HISTORY_CODE_LINK
+fi
+
+cd $HISTORY_CODE_LINK
+npm install
+nodejs Server.js
--- Utilities/SetupLinuxBox/SetupLinux.bat
+++ Utilities/SetupLinuxBox/SetupLinux.bat
@@ -9,8 +9,8 @@ set User=%ELF_USER%
 set SshUser=%User%@%ELF_IP%
 set HomeDir=/home/%User%
 set CreateBin=CreateBin.sh
-set SetupGitScript=SetupGit.sh
-set SetupSshScript=SetupSsh.sh
+set SetupPresTestScript=SetupGitForPresTest.sh
+set SetupHistoryScript=SetupGitForHistoryCode.sh
 set SetupNodeScript=SetupNode.sh
 
 REM Create Bin
@@ -20,13 +20,13 @@ plink -t %SshUser% %HomeDir%/%CreateBin%
 plink -t %SshUser% /bin/rm %HomeDir%/%CreateBin%
 
 REM Copy Scripts
-pscp %SetupSshScript% %SshUser%:%HomeDir%/bin/%SetupSshScript%
-pscp %SetupGitScript% %SshUser%:%HomeDir%/bin/%SetupGitScript%
+pscp %SetupHistoryScript% %SshUser%:%HomeDir%/bin/%SetupHistoryScript%
+pscp %SetupPresTestScript% %SshUser%:%HomeDir%/bin/%SetupPresTestScript%
 pscp %SetupNodeScript% %SshUser%:%HomeDir%/bin/%SetupNodeScript%
-plink -t %SshUser% /bin/chmod +x %HomeDir%/bin/%SetupSshScript%
-plink -t %SshUser% /bin/chmod +x %HomeDir%/bin/%SetupGitScript%
+plink -t %SshUser% /bin/chmod +x %HomeDir%/bin/%SetupHistoryScript%
+plink -t %SshUser% /bin/chmod +x %HomeDir%/bin/%SetupPresTestScript%
 plink -t %SshUser% /bin/chmod +x %HomeDir%/bin/%SetupNodeScript%
 
 REM Run Scripts 
 plink -t %SshUser% %HomeDir%/bin/%SetupNodeScript%
-plink -t %SshUser% %HomeDir%/bin/%SetupGitScript%
+plink -t %SshUser% %HomeDir%/bin/%SetupHistoryScript%
