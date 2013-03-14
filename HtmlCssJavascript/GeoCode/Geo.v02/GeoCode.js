7f534e15e1b62b1111613036ef03b0385f4d2e4a
Charles Calvert <java@elvenware.com>
2013-03-14 10:59:59 -0700

    Rename of Projects for versions 1 and 2

---
+++ HtmlCssJavascript/GeoCode/Geo.v02/GeoCode.js
@@ -0,0 +1,61 @@
+/**
+ * @author Charlie Calvert
+ * Released under the MIT License
+ */
+
+/*global google: false */
+/*jshint devel: true, browser: true, jquery: true, strict: true */
+
+var GeoCode = (function() {'use strict';
+
+	function GeoCode(latitude, longitude) {
+		initialize(latitude, longitude);
+	}
+
+	var geocoder;
+	var map;
+
+	var initialize = function(latitude, longitude) {
+		geocoder = new google.maps.Geocoder();
+		var latlng = new google.maps.LatLng(latitude, longitude);
+		var mapOptions = {
+			zoom : 8,
+			center : latlng,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		};
+		map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+	}
+
+	GeoCode.prototype.getCoordinates = function() {		
+		var address = $('#address').val();
+		geocoder.geocode({
+			'address' : address
+		}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				var pos = results[0].geometry.location;
+				var latititude = pos.ib;
+				var longitude = pos.jb;
+				var titleString = address + " Latitude: " + latititude + " Longitude: " + longitude;
+				$("#position").html(titleString);
+				map.setCenter(results[0].geometry.location);
+				var marker = new google.maps.Marker({
+					map : map,
+					position: pos,
+					title: titleString
+				});
+			} else {
+				alert('Geocode error: ' + status);
+			}
+		});
+	}
+
+	return GeoCode;
+})();
+
+
+$(document).ready(function() {
+  "use strict";
+  var geoCode = new GeoCode(47.6062095, -122.3320708);
+  $("#getCoordinates").click(geoCode.getCoordinates);
+  
+});
