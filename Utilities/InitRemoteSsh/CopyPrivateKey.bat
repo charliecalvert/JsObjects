89c29ba530f1c548961a6fc5709141ade65a79f7
Charlie Calvert <java@elvenware.com>
2013-03-16 13:58:06 -0700

    Updating

--- Utilities/InitRemoteSsh/CopyPrivateKey.bat
+++ Utilities/InitRemoteSsh/CopyPrivateKey.bat
@@ -2,6 +2,7 @@ set User=%ELF_USER%
 set SshUser=%User%@%ELF_IP%
 set SshKey01=CharlieMainKey.pem
 set SshKey02=github_rsa
+set GitSshScript=SetupSshAgent.sh
 
 REM Copy Key 01
 pscp %USERPROFILE%\documents\Data\Putty\%SshKey01% %SshUser%:/home/%User%/.ssh/.
@@ -10,3 +11,10 @@ plink %SshUser% /bin/chmod 400 /home/%User%/.ssh/%SshKey01%
 REM Copy Key 02
 pscp %USERPROFILE%\documents\Data\Putty\%SshKey02% %SshUser%:/home/%User%/.ssh/.
 plink %SshUser% /bin/chmod 400 /home/%User%/.ssh/%SshKey02%
+
+REM Copy Script that pulls from Repository
+pscp %GitSshScript% %SshUser%:/home/%User%/bin/.
+plink %SshUser% /bin/chmod 700 /home/%User%/bin/%GitSshScript%
+plink %SshUser% /home/%User%/bin/%GitSshScript%
+
+
--- Utilities/InitRemoteSsh/SetupSshAgent.sh
+++ Utilities/InitRemoteSsh/SetupSshAgent.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
 eval `ssh-agent`
-ssh-add ~\.ssh\CharlieMainKey.pem
-ssh-add ~\.ssh\github_rsa
+ssh-add ~/.ssh/CharlieMainKey.pem
+ssh-add ~/.ssh/github_rsa
+
+git clone git@bitbucket.org:ccalvert/test03-calvert.git
