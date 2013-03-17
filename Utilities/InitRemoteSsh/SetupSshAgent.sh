6006277fcc0648c80cad12b7f2b31521caf25454
charlie <java@elvenware.com>
2013-03-16 18:37:58 -0700

    Add code to ensure that server key is cached

--- Utilities/InitRemoteSsh/SetupSshAgent.sh
+++ Utilities/InitRemoteSsh/SetupSshAgent.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# Load the private key
 eval `ssh-agent`
 ssh-add ~/.ssh/CharlieMainKey.pem
+
+# Ensure server key is cached
 ssh foo@bitbucket.org
+
+# Clone the repository
 git clone git@bitbucket.org:ccalvert/test03-calvert.git
