e359dadd36683352d25d9d20d7c6a6d708aef9e0
charlie <java@elvenware.com>
2013-03-21 22:01:45 -0700

    Minor stuff

---
+++ Utilities/ClonePrivateRepository/CleanUp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# If you modified this script, you could use it
+# during debug sessions when you are planning your
+# deployment.
+cd ~/
+rm -rf .ssh/CharlieMainKey.pem
+rm -rf Git/test03-calvert
+rm bin/PullGitRepository.sh
