553a10e0d883cf8e0ff5d4bf56616a8db600cd54
Charles Calvert <java@elvenware.com>
2013-04-10 16:51:22 -0700

    Key Down handler added

---
+++ JavaScript/Syntax/KeyDown/index.js
@@ -0,0 +1,39 @@
+/**
+ * @author Charlie
+ */
+
+var App = (function() {
+	function App() {
+		window.addEventListener('keydown', doKeyDown, true);	
+	}
+	
+		var doKeyDown = function(evt) {
+		switch (evt.keyCode) {
+			case 38:
+				/* Up arrow was pressed */
+				$("#debug").html("up");
+				break;
+			case 40:
+				/* Down arrow was pressed */
+				$("#debug").html("down");
+				break;
+			case 37:
+				/* Left arrow was pressed */
+				$("#debug").html("left");
+				break;
+			case 39:
+				/* Right arrow was pressed */
+				$("#debug").html("right");
+				break;
+		}
+	};
+
+	
+	return App;
+	
+})();
+
+
+$(document).ready(function() {
+	new App();
+});
