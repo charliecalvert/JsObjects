f897a0ffa44b392bc371b2ead19351831ac7fd56
Charles Calvert <java@elvenware.com>
2013-04-17 17:33:21 -0700

    Array Demo

---
+++ JavaScript/Syntax/ArrayTwoDimensions/index.js
@@ -0,0 +1,29 @@
+/*jshint jquery: true, devel: true, browser: true, strict: true */
+
+var App = (function() {
+
+	var MAX = 3;
+	var grid = [[0, 1, 0], 
+		[1, 0, 1], 
+		[0, 1, 0]
+	];
+
+	function App() {
+		for (var column = 0; column < MAX; column++) {
+			for (var row = 0; row < MAX; row++) {
+				var item = "#item" + row + column;
+				if (grid[row][column] == 0) {					
+					$(item).html("0");
+				} else {
+					$(item).html("X");
+				}
+			}
+		}
+	}
+
+	return App;
+})();
+
+$(document).ready(function() {
+	new App();
+}); 
