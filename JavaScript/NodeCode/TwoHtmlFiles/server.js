ba273b147e0368f848e1666edccfc1e6e2806278
charlie <java@elvenware.com>
2013-04-20 20:14:59 -0700

    Adding a Two HTML Files example to show how to load more than one HTML file from Node server

---
+++ JavaScript/NodeCode/TwoHtmlFiles/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+var port = process.env.PORT || 30025;
+
+app.get('/', function(req, res) {
+	var html = fs.readFileSync('Public/index.html');
+	res.writeHeader(200, {"Content-Type": "text/html"});
+	res.end(html);
+});
+
+app.get('/launchSecondHtmlFile', function(request, response) {	
+	console.log('launchSecondHtmlFile called');
+	try {
+		var html = fs.readFileSync('Public/SecondFile.html');
+		response.writeHeader(200, {"Content-Type": "text/html"});
+		response.end(html);
+	} catch(error) {
+		console.log(error);
+	}
+});
+
+app.use("/Public", express.static(__dirname + '/Public'));
+
+app.listen(port);
+console.log('Listening on port :' + port);
