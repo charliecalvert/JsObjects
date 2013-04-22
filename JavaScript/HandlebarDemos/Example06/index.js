2236f042421a7c5f1ae1cd6084a6c96fff49ede0
charlie <java@elvenware.com>
2013-04-22 11:16:19 -0700

    Clean up Example 06 module pattern, provide support for projects in sub projects

--- JavaScript/HandlebarDemos/Example06/index.js
+++ JavaScript/HandlebarDemos/Example06/index.js
@@ -4,37 +4,46 @@
 var fs = require('fs');
 var handlebars = require('handlebars');
 
-/*jshint jquery:true, browser: true */
-/*global Handlebars: false */
-function writeHtml(fileName, data) {
-	fs.writeFile(fileName, data, function(err) {
-		if(err) {
-		  console.log(err);
-		} else {
-		  console.log("JSON saved to " + fileName);
-		}
-	});	
-}
+/* jshint jquery:true, browser: true, strict: true */
+/* global Handlebars: false */
 
-// Please not that we convert to a string.
-function readHtml() {
-	return String(fs.readFileSync('index.html'));
-}
+var App = (function() {	'use strict';
 
-function addNames(initFirstName, initLastName) {
-    'use strict';
-            
-    var script = readHtml();    
-	// console.log(script);
+	function App() {	
+		addNames();
+	}
+
+	var writeHtml = function(fileName, data) {
+		fs.writeFile(fileName, data, function(err) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log("JSON saved to " + fileName);
+			}
+		});
+	}
+
+	// Please not that we convert to a string.
+	var readHtml = function() {
+		return String(fs.readFileSync('index.html'));
+	}
+
+	var addNames = function(initFirstName, initLastName) {
+		var script = readHtml();
+		// console.log(script);
+
+		var template = handlebars.compile(script);
+
+		var result = template({
+			MyStuff : 'This is what we insert.',
+			OtherStuff : 'This is the other stuff'
+		});
+
+		writeHtml('upLoadMe.html', result);
+	}
 	
-    var template=handlebars.compile(script);    
-    
-    var result = template({
-        MyStuff: 'This is what we insert.',
-		OtherStuff: 'This is the other stuff'
-    });    
-    
-    writeHtml('upLoadMe.html', result); 
-}
+	return App;
 
-addNames();
+})();
+
+new App();
