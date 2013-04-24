8b990f0535cf279e28362f76a4b87aed3f230027
Charles Calvert <java@elvenware.com>
2013-04-24 15:54:17 -0700

    Simplified example of reading and writing JSON

---
+++ JavaScript/NodeCode/SimpleReadWriteJson/public/index.js
@@ -0,0 +1,56 @@
+function addNames(initFirstName, initLastName, initAge) {
+    'use strict';
+            
+   $("#firstName").val(initFirstName);
+   $("#lastName").val(initLastName);
+   $("#age").val(initAge);
+    
+    
+}
+
+function readJson() {
+
+	$.getJSON('/read', function(data) {
+		addNames(data.firstName, data.lastName, data.age); 
+	})
+	.success(function() { console.log("csc: success. Loaded index.json"); })
+	.error(function(jqXHR, textStatus, errorThrown) { 
+		alert("error calling JSON. Try JSONLint or JSLint: " + textStatus); 
+	})
+	.complete(function() { console.log("csc: completed call to get index.json"); });
+}
+
+var writeJson = function() {
+	var data = { firstName: $('#firstName').val(), lastName: $('#lastName').val(), age: $('#age').val() };
+	$.ajax(
+    {
+      type: 'GET',
+      url: '/write',
+      dataType: 'json',
+      cache: 'False',
+      data: data, 
+      success: function(data) {
+        display.showDebug(data.result);
+      },
+      error: showError      
+    });
+};
+
+var showError = function(request, ajaxOptions, thrownError) {
+	showDebug("Error occurred: = " + ajaxOptions + " " + thrownError );
+	showDebug(request.status);
+	showDebug(request.statusText);
+	showDebug(request.getAllResponseHeaders());
+	showDebug(request.responseText);
+};
+
+var showDebug = function(textToDisplay)
+{
+	$("#debug").append('<li>' + textToDisplay + '</li>');
+};
+
+
+$(document).ready(function() {
+	$("#buttonRead").click(readJson);
+	$("#buttonWrite").click(writeJson);
+});
