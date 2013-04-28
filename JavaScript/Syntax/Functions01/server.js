a989d1f7cab980dd29a65f6d992714b6795013a5
charlie <java@elvenware.com>
2013-04-27 21:21:52 -0700

    Adding functions name example

---
+++ JavaScript/Syntax/Functions01/server.js
@@ -0,0 +1,8 @@
+var variable00;
+var func01 = function() {};
+function func02() {}
+
+console.log("variable00: " + variable00);
+console.log("func01.name: " + func01.name);
+console.log("type of func01.name: " + typeof func01.name);
+console.log("func02.name: " + func02.name);
