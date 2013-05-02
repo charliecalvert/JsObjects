e91946b83e1b25a21b940e3949622c44d7e2631f
Charles Calvert <java@elvenware.com>
2013-05-02 15:21:10 -0700

    Updating test for this in an object

--- JavaScript/Objects/ObjectThis/ObjectThis.js
+++ JavaScript/Objects/ObjectThis/ObjectThis.js
@@ -12,6 +12,21 @@ function RunMe() {
 	$('#test03').append(this instanceof RunMe);
 }
 
+var App = (function() {
+	
+	function App() {
+		$("#testButton").click(buttonClick);
+		$('#test05').append(this instanceof App);
+	}
+	
+	var buttonClick = function() {
+		var result = this instanceof App;
+		$('#test06').append(result.toString());
+	};
+	
+	return App;
+})();
+
 $(document).ready(function() {
     "use strict";
     var functionContext = runMe();
@@ -21,5 +36,8 @@ $(document).ready(function() {
     $('#test02').append(functionContext2 === myObject);
     
     var functionContext3 = new RunMe();
-	$('#test04').append(functionContext3 instanceof RunMe);   
+	$('#test04').append(functionContext3 instanceof RunMe);  
+
+	new App();
+
 });
