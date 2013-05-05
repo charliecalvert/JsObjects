2d9253079a3c552fc6b4efb1779bdc40d1792f6d
charlie <java@elvenware.com>
2013-05-04 21:35:21 -0700

    Code Cleanup

--- JavaScript/Objects/ShareVariables01/ShowPlayer.js
+++ JavaScript/Objects/ShareVariables01/ShowPlayer.js
@@ -2,21 +2,24 @@
  * @author Charlie
  */
 
+/*jshint jquery:true, browser:true, devel:true, strict:true */
+/*global ELF:true */
+
 var ELF = {};
 ELF.own = {};
 
-ELF.own.ShowPlayer = (function() {
-	
+ELF.own.ShowPlayer = (function() { 'use strict';
+
 	var data = null;
-	
+
 	function ShowPlayer(initData) {
 		data = initData;
 		$('#buttonShowPlayer').click(showPlayerXY);
 	}
-	
-    var showPlayerXY = function() {
-		$('#test01').html('X = ' + data.playerX + ' Y = ' + data.playerY);    	
-    }
+
+	var showPlayerXY = function() {
+		$('#test01').html('X = ' + data.playerX + ' Y = ' + data.playerY);
+	};
 
 	return ShowPlayer;
-})();
+})(); 
--- JavaScript/Objects/ShareVariables01/index.js
+++ JavaScript/Objects/ShareVariables01/index.js
@@ -1,30 +1,32 @@
 /**
  * @author Charlie Calvert
  */
- 
+
+/*jshint jquery:true, browser:true, devel:true, strict:true */
+/*global ELF:true */
+
 ELF.own.Player = (function() {
-    'use strict';
+	'use strict';
 	var that = {};
 	that.playerX = 1;
 	that.playerY = 2;
-	
-  	// Constructor
-    function Player() {
-       	new ELF.own.ShowPlayer(that);
-       	$('#buttonChangePlayer').click(changePlayer);
-	}    
+
+	// Constructor
+	function Player() {
+		new ELF.own.ShowPlayer(that);
+		$('#buttonChangePlayer').click(changePlayer);
+	}
 
 	var changePlayer = function() {
 		that.playerX += 1;
 		that.playerY += 2;
 	};
-	
-    return Player;
+
+	return Player;
 })();
 
-
 window.onload = function() {
-    'use strict';
-    
-    new ELF.own.Player();
+	'use strict';
+
+	new ELF.own.Player();
 };
