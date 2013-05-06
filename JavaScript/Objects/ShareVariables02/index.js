a662c24978f3e4619127eb818ce4cf98ad0f7a2f
charlie <java@elvenware.com>
2013-05-06 13:55:01 -0700

    Start work on ShareVariable02

---
+++ JavaScript/Objects/ShareVariables02/index.js
@@ -0,0 +1,50 @@
+/**
+ * @author Charlie Calvert
+ */
+ 
+var Point = (function() {
+    'use strict';
+	
+  	// Constructor
+    function Point(x1, y1, selector) {
+       	Object.defineProperty(this, "x", withValue(x1));
+		Object.defineProperty(this, "y", withValue(y1));	
+		$(selector).click({that: this}, accessProperty);
+	}    
+    
+	var withValue = function(value) {
+		var d = withValue.d || (withValue.d = {
+			enumerable : false,
+			writable : true,
+			configurable : true,
+			value : null
+		});
+		d.value = value;
+		return d;
+	}
+	    
+	var accessProperty = function(e) {
+		privateShowXY('#test03', e.data.that);
+	}
+	
+	// Private functions	
+	var privateShowXY = function(selector, obj) {
+		$(selector).html('X = ' + obj.x + ' Y = ' + obj.y);
+	}
+	
+    Point.prototype.showXY = function(selector) {
+		privateShowXY(selector, this);    	
+    }
+
+    return Point;
+})();
+
+
+window.onload = function() {
+    'use strict';
+    
+    var point1 = new Point(3, 4, '#button01');
+    point1.showXY('#test01');
+    var point2 = new Point(5, 6, '#button02');
+    point2.showXY('#test02');
+};
