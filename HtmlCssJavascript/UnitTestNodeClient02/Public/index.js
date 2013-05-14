636ab56b0b755fcafda0371b55b509ccf01b4455
charlie <java@elvenware.com>
2013-05-14 16:22:03 -0700

    Pass in params rather than reduntant code for tests

--- HtmlCssJavascript/UnitTestNodeClient02/Public/index.js
+++ HtmlCssJavascript/UnitTestNodeClient02/Public/index.js
@@ -35,9 +35,9 @@ function mathTests() {
 	});
 };
 
-var postTests = function() {
+var ajaxTests = function(moduleName, typeRequest) {
 
-	module('POST Tests');
+	module(moduleName);
 	
 	asyncTest('writeJson', function() {
 		var app = new App();
@@ -55,7 +55,7 @@ var postTests = function() {
 			names: JSON.stringify(nameIt)			
 		};
 		
-		app.writeJsonTest(data, 'POST', function(data) {
+		app.writeJsonTest(data, typeRequest, function(data) {
 			equal(data.Result, 'Success');
 			start();
 		},
@@ -151,7 +151,7 @@ var postTests = function() {
 			grid: JSON.stringify(grid)		
 		};
 		
-		app.writeJsonTest(data, 'POST', function(data) {
+		app.writeJsonTest(data, typeRequest, function(data) {
 			equal(data.Result, 'Success');
 			start();
 		},
@@ -214,185 +214,8 @@ var postTests = function() {
 	
 };
 
-var getTests = function() {
-
-	module('GET Tests');
-	
-	asyncTest('getWriteJsonSimple', function() {
-		var app = new App();
-		
-		var dataInfo = {
-	    	dataType: 0,
-			fileName: 'Names.json',
-	    };
-	    
-		var nameIt = { "firstName": "Suzie",
-					   "lastName": "Lu",
-					   "age": 3 };
-		var data = { 
-			dataInfo: JSON.stringify(dataInfo),
-			names: JSON.stringify(nameIt)			
-		};
-		
-		app.writeJsonTest(data, 'POST', function(data) {
-			equal(data.Result, 'Success');
-			start();
-		},
-		function(request, ajaxOptions, thrownError) {
-			ok(false, 'call to readJson failed: ' + request.responseText);
-			start();
-		});
-	});
-	
-	asyncTest('getWriteJsonComplex', function() {
-		var app = new App();
-		
-		var person = { 
-			"firstName": "Suzie",
-			"lastName": "Lu",
-			"age": 3 
-		};
-		
-		var npcs = [
-		{
-	        "column": 1,
-	        "row": 6,
-	        "npcNumber": 5,
-	        "traits": {
-	            "Core": {
-	                "Type": "Orc",
-	                "Confidence": 0,
-	                "Experience": 0,
-	                "Health": -1,
-	                "Knowledge": 0,
-	                "Moves": 0,
-	                "Strength": 3,
-	                "Wisdom": 1
-	            },
-	            "Items": {
-	                "Bread": 0,
-	                "Plants": 0,
-	                "Water": 0
-	            }
-	        },
-	        "npcName": "Orc-1-6-5"
-	    },
-	    {
-	        "column": 5,
-	        "row": 1,
-	        "npcNumber": 1,
-	        "traits": {
-	            "Core": {
-	                "Type": "Orc",
-	                "Confidence": 0,
-	                "Experience": 0,
-	                "Health": 5,
-	                "Knowledge": 0,
-	                "Moves": 0,
-	                "Strength": 3,
-	                "Wisdom": 1
-	            },
-	            "Items": {
-	                "Bread": 0,
-	                "Plants": 0,
-	                "Water": 0
-	            }
-	        },
-	        "npcName": "Orc-5-1-1"
-	    }];
-	    
-	    var grid = [
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-		];
-		
-	    var dataInfo = {
-	    	dataType: 1,
-			fileNamePerson: 'Person.json',
-			fileNameNpcs: 'Npcs.json',
-			fileNameGrid: 'Grid.json'
-	    };
-	    
-		var data = { 
-			dataInfo: JSON.stringify(dataInfo), 
-			person: JSON.stringify(person),
-			npcs: JSON.stringify(npcs),
-			grid: JSON.stringify(grid)		
-		};
-		
-		app.writeJsonTest(data, 'POST', function(data) {
-			equal(data.Result, 'Success');
-			start();
-		},
-		function(request, ajaxOptions, thrownError) {
-			ok(false, 'call to readJson failed: ' + request.responseText);
-			start();
-		});
-	});
-	
-	asyncTest('getReadJsonPerson', function() {
-		var app = new App();
-		app.readJsonTest('Person.json', function(data) {
-			try {
-				equal(data.firstName, 'Suzie');
-				equal(data.age, 3);
-				ok(typeof data.age === "number");
-			} finally {
-				start();
-			}
-		},
-		function(request, ajaxOptions, thrownError) {
-			ok(false, 'call to readJson failed: ' + request.responseText);
-			start();
-		});
-	});
-	
-	asyncTest('getReadJsonNpcs', function() {
-		var app = new App();
-		app.readJsonTest('Npcs.json', function(data) {
-			try {
-				equal(data[0].row, 6);			
-				ok(typeof data[0].row === "number");
-				equal(data[0].traits.Core.Health, -1);
-			} finally {
-				start();
-			}
-		},
-		function(request, ajaxOptions, thrownError) {
-			ok(false, 'call to readJson failed: ' + request.responseText);
-			start();
-		});
-	});
-	
-	asyncTest('getReadJsonGrid', function() {
-		var app = new App();
-		app.readJsonTest('Grid.json', function(data) {
-			try {
-				equal(data[0][0], 0);			
-				ok(typeof data[0][0] === "number");
-				equal(data[2][4], 1);
-			} finally {
-				start();
-			}
-		},
-		function(request, ajaxOptions, thrownError) {
-			ok(false, 'call to readJson failed: ' + request.responseText);
-			start();
-		});
-	});
-} 
  $(document).ready(function() {
  	mathTests();
- 	postTests();
- 	getTests();	
+ 	ajaxTests('POST Tests', 'POST');
+ 	ajaxTests('GET Tests', 'GET');
  });
