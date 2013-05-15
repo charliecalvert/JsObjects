293bbbf4ae51b96bda6a0a9fa6e151867b6749c4
Charlie Calvert <java@elvenware.com>
2013-05-14 19:13:14 -0700

    Updating PullMe.sh with some comments

--- PullMe.sh
+++ PullMe.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# Unless you are Charlie, you probably don't have rights to
+# modify this repository. As a result, you don't need to use
+# this file. Just type git pull to update the repository.
+# You might, however, want to use this file as a model for
+# a script you might use in a repository that you do own.
+
 eval `ssh-agent`
-ssh-add ~/.ssh/CharlieMainKey02.pem
+ssh-add ~/.ssh/GitHub.pem
 git pull
 
