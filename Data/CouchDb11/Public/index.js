2920536467481c1625388440ccdf0ae679ab0f34
Charles Calvert <java@elvenware.com>
2013-05-22 12:56:48 -0700

    Adding attachment info

---
+++ Data/CouchDb11/Public/index.js
@@ -0,0 +1,190 @@
+/*jshint devel: true, browser: true, jquery: true, strict: true */
+
+var App = (function() { 'use strict';    
+    
+    function App() {
+		$("#buttonClear").click(clear);
+        $("#buttonRead").click(readJson);
+        $("#buttonCreate").click(create);
+        $("#buttonDatabaseName").click(databaseName);
+        $("#buttonWrite").click(write);
+        $("#buttonDocNames").click(docNames);
+        $("#buttonDesignDoc").click(designDoc);
+        $("#buttonView").click(view);
+        $("#buttonAttachHtml").click(attachHtml);
+        $("#buttonGetAttachedHtml").click(getAttachedHtml);
+        $("#buttonUpdateAttachedHtml").click(updateAttachedHtml);
+    }
+
+    var updateAttachedHtml = function() {
+        $.ajax({
+            type : 'GET',
+            url : '/attachUpdateHtml',
+            dataType : 'json',
+            success : function(data) {
+                showDebug(data.Result);
+            },
+            error: showNanoError
+        });
+    };
+
+
+    var getAttachedHtml = function() {
+        $.ajax({
+            type : 'GET',
+            url : '/getAttachedHtml',
+            dataType : 'html',
+            success : function(data) {
+                $('#div01').html(data);
+            },
+            error: showNanoError
+        });
+    };
+    
+    var attachHtml = function() {
+        $.ajax({
+            type : 'GET',
+            url : '/attachHtml',
+            dataType : 'json',
+            success : function(data) {
+                showDebug(data.Result);
+            },
+            error: showNanoError
+        });
+    }
+    
+    var databaseName = function() {
+    	$.ajax({
+            type : 'GET',
+            url : '/databaseName',
+            dataType : 'json',
+            success : function(data) {
+                showDebug(data.Result);
+            },
+            error : showError
+        });
+    }
+    
+    var view = function() {
+        $.ajax({
+            type : 'GET',
+            url : '/view01',
+            dataType : 'html',
+            success : function(data) {
+                $('#div01').html(data);
+            },
+            error : showNanoError
+        });    
+    };
+    
+    var designDoc = function() {
+        $.ajax({
+            type : 'GET',
+            url : '/designDoc',
+            dataType : 'json',
+            success : function(data) {
+                showDebug(data.Result);
+            },
+            error : showError
+        });
+    };
+    
+    function addNames(docName, initFirstName, initLastName, initAge) {
+        $('#docName').val(docName);
+        $("#firstName").val(initFirstName);
+        $("#lastName").val(initLastName);
+        $("#age").val(initAge);
+    }
+
+	var clear = function() {
+		$('#debug').empty();
+	}
+    function readJson() {
+        var docName = {
+            "docName" : $('#docName').val()
+        };
+        $.ajax({
+            type : 'GET',
+            url : '/read',
+            data : docName,
+            dataType : 'json',
+            success : function(data) {
+                addNames(data.docName, data.firstName, data.lastName, data.age);
+            },
+            error : showNanoError
+        });
+    }
+
+    function docNames() {
+        $.getJSON('/docNames', function(data) {
+            for (var i = 0; i < data.length; i++) {
+                showDebug(data[i]);
+            }
+        }).error(showNanoError);
+    }
+
+    var write = function() {
+        var userInput = {
+            docName : $('#docName').val(),
+            firstName : $('#firstName').val(),
+            lastName : $('#lastName').val(),
+            age : $('#age').val()
+        };
+
+        $.ajax({
+            type : 'GET',
+            url : '/write',
+            dataType : 'json',
+            data : userInput,
+            success : function(data) {
+                showDebug(data.Result);
+            },
+            error : showError
+        });
+    };
+
+    var create = function() {
+        $.ajax({
+            type : 'GET',
+            url : '/create',
+            dataType : 'json',
+            success : function(data) {
+                showDebug(data.Result);
+            },
+            error : showNanoError
+        });
+    };
+
+    var showNanoError = function(request, ajaxOptions, thrownError) {
+        var responseText = "";
+        try {
+            responseText = JSON.parse(request.responseText);
+        } catch(error) {
+            showDebug(request.responseText);
+            return;
+        }
+        
+        if (typeof responseText.p282special !== 'undefined') {
+            showDebug(responseText.p282special);
+        }
+        showDebug(responseText.message);
+        showError(request, ajaxOptions, thrownError);
+    };
+    
+    var showError = function(request, ajaxOptions, thrownError) {
+        showDebug("Error occurred: = " + ajaxOptions + " " + thrownError);
+        showDebug(request.status);
+        showDebug(request.statusText);
+        showDebug(request.getAllResponseHeaders());
+        showDebug(request.responseText);
+    };
+
+    var showDebug = function(textToDisplay) {
+        $("#debug").append('<li>' + textToDisplay + '</li>');
+    };
+    return App;
+})();
+
+$(document).ready(function() {'use strict';
+    new App();
+}); 
---
+++ Data/CouchDb12/server.js
@@ -0,0 +1,295 @@
+/*jshint devel: true, browser: true, jquery: true, strict: true, node: true, es5:true */
+/*global emit:true */
+
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var templater = require('./Library/Templater');
+
+// See this: http://mahoney.eu/2012/05/23/couchdb-cookie-authentication-nodejs-nano/#.UZpztbXrw6o
+//var nano = require('nano')('http://192.168.2.21:5984');
+//var nano = require('nano')('http://ccalvert:foobar@192.168.2.21:5984');
+var nano = require('nano')('http://ccalvert:foobar@localhost:5984');
+// var nano = require('nano')('http://127.0.0.1:5984');
+
+var port = process.env.PORT || 30025;
+
+// var fileName = 'person.json';
+
+app.get('/', function(req, res) { 'use strict';
+    var html = fs.readFileSync('Public/index.html');
+    res.writeHeader(200, {
+        "Content-Type" : "text/html"
+    });
+    res.end(html);
+});
+
+var dbName = 'prog28212';
+var docName = 'doc01';
+
+app.get('/databaseName', function(request, response) {'use strict';
+	console.log("\/databaseName called.")
+	response.send({ 'Result': dbName} );
+});
+
+var firstAndLast = function(doc) {'use strict';
+    if (doc.firstName && doc.lastName) {
+        var name = doc.firstName + ' ' + doc.lastName;
+        emit(doc._id, name);
+    }
+};
+
+var lastOnly = function(doc) {'use strict';
+    if (doc.firstName && doc.lastName) {
+        var name = doc.lastName;
+        emit(doc._id, name);
+    }
+};
+
+
+app.get('/designDoc', function(request, response) { 'use strict';
+    console.log("Design Doc Called");
+
+    var prog = nano.db.use(dbName);
+    prog.insert({
+        "views" : {
+            "firstAndLast" : {
+                "map" : firstAndLast
+            }
+        }
+    }, '_design/people', 
+    function(error, body) {
+        if (!error) {
+            console.log(body);
+            response.send({
+                 'Result' : 'Success',
+                 'body': body
+            });
+        } else {
+            console.log("error: " + error);
+            response.send({
+                 'Result' : 'The document might already exist. ' + error
+            });
+        }
+    });
+    
+});
+
+app.get('/view01', function(request, response) { 'use strict';
+    console.log("view Called");
+
+    var prog = nano.db.use(dbName);
+    prog.view('people', 'firstAndLast', function(err, body) {
+        if (!err) {
+            var result = [];
+            body.rows.forEach(function(doc) {
+                result.push(doc);
+                console.log(doc.value);
+            });
+            var html = templater.template.addNames('Templates/Basic.html', result);
+            response.send(html);
+        } else {
+            console.log(err);
+            response.send(500, err);
+        }
+    });
+});
+
+app.get('/create', function(request, response) { 'use strict';
+    console.log('create called.');
+    nano.db.create(dbName, function(err, body) {
+        if (!err) {
+            console.log(body);
+        } else {
+            console.log('Could not create database');
+            console.log(err);
+            response.send(500, err);
+            return;
+        }        
+    });
+    
+    console.log('use database');
+    var prog = nano.db.use(dbName);
+
+    prog.insert({
+        firstName : 'Suzie',
+        lastName : 'Fredrick',
+        age : 38
+    }, docName, function(err, body) {
+        if (!err) {
+            console.log(body);
+            response.send({
+                 'Result' : 'Success'
+             });
+        } else {
+            console.log(err);
+            response.send({'Result': err });
+        }
+        
+    });
+
+});
+
+app.get('/read', function(request, response) { 'use strict';
+    console.log('Read called: ' + JSON.stringify(request.query));
+
+    var prog = nano.db.use(dbName);
+    prog.get(request.query.docName, {
+        revs_info : true
+    }, function(err, body) {
+        if (!err) {
+            console.log(body);
+            response.send(body);
+        } else {
+            var cscMessage = "No such record as: " + request.query.docName +
+                ". Use a the Get Doc Names button to find " +
+                "the name of an existing document."
+            err.p282special = cscMessage;
+        	response.send(500, err);
+        }
+        	
+    });
+});
+
+app.get('/docNames', function(request, response) { 'use strict';
+    // var url = 'http://localhost:5984/prog28202/_all_docs';
+    var prog = nano.db.use(dbName);
+    var result = [];
+    prog.list(function(err, body) {
+        if (!err) {
+            body.rows.forEach(function(doc) {
+                console.log(doc);
+                result.push(doc.key);
+            });
+            console.log(result);
+            response.send(result);
+        } else {
+            console.log(err);
+            response.send(500, err);
+            return;
+        }
+    });
+});
+
+app.get('/write', function(request, response) { 'use strict';
+    console.log('Write called: ' + request.query);
+    var person = request.query;
+    var personString = JSON.stringify(person, null, 4);
+    console.log('PersonString: ' + personString);
+
+    var prog = nano.db.use(dbName);
+    prog.insert(person, person.docName, function(err, body) {
+        if (!err) {
+            console.log(body);
+        } else {
+            console.log(err);
+            response.send(err);
+            return;
+        }
+    });
+    response.send({
+        'Result' : 'Success'
+    });
+});
+
+app.get("/attachPng", function(request, response) {'use strict';
+   console.log('/attachPng called');
+   var fs = require('fs');
+
+    fs.readFile('Images/rabbit.png', function(err, data) {
+        if (!err) {
+            var prog = nano.db.use(dbName);
+            prog.attachment.insert('rabbit', 'rabbit.png', data, 'image/png',
+                { rev: '12-150985a725ec88be471921a54ce91452' }, function(err, body) {
+                if (!err) {
+                    console.log(body);
+                } else {
+                    console.log(err);
+                }
+            });
+        }
+    }); 
+});
+
+/**
+ * If rev is null, this is an insert, else, it is an update
+ * See the attachUpdateHtml handler below
+ */
+var doInsert = function(rev, response, key, docName) {
+    var fs = require('fs');
+
+    fs.readFile(__dirname + '/Templates/' + docName, function(err, data) {
+        if (!err) {
+            var prog = nano.db.use(dbName);
+            prog.attachment.insert(key, docName, data, 'text/html', rev,
+                function(err1, body) {
+                if (!err1) {
+                    console.log(body);
+                    response.send({"Result":"Success"});
+                } else {
+                    console.log(err1);
+                    err1.p282special = "Document conflict means document already exists. Try an update."
+                    response.send(500, err1);
+                }
+            });
+        } else {
+            console.log(err);
+            response.send(500, err);
+        }
+    }); 
+}
+
+app.get("/attachHtml", function(request, response) {'use strict';
+   console.log('/attachHtml called');
+   console.log(request.query);
+   console.log(request.query.doc);
+   console.log(request.query.key);
+
+   // Null means we are trying to do an insert
+   doInsert(null, 
+       response, 
+       request.query.key, 
+       request.query.doc);
+});
+
+app.get("/getAttachedHtml", function(request, response) {'use strict';
+   console.log('/getAttachedHtml called');
+   console.log(request.query);
+   
+   var prog = nano.db.use(dbName);
+    prog.attachment.get(request.query.key, 
+        request.query.doc, function(err, body) {
+        if (!err) {
+            console.log(body);
+            response.send(body);
+        } else {
+            console.log(err);
+            response.send(500, err);
+        }   
+    }); 
+});
+
+app.get("/attachUpdateHtml", function(request, response) {'use strict';
+    console.log('/attachUpdate called');
+    var key = request.query.key;
+    var doc = request.query.doc;    
+    
+    var prog = nano.db.use(dbName);
+    prog.get('attachMe', function(error, existing) { 
+        if(!error) { 
+            console.log(existing);
+            console.log(existing._rev);
+            doInsert({ "rev":existing._rev }, 
+                response, key, doc);
+        }  else {
+            console.log(error);
+            response.send(500, error);
+        }
+    });
+});
+
+app.use("/", express.static(__dirname + '/Public'));
+app.use("/", express.static(__dirname + '/Library'));
+
+app.listen(port);
+console.log('Listening on port :' + port); 
