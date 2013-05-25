16b0a7bf6d97aef4ec7b6cb37bddaa7e103261dd
charlie <java@elvenware.com>
2013-05-24 22:31:12 -0700

    Erica

--- Utilities/NodeInstall/Erica.sh
+++ Utilities/NodeInstall/Erica.sh
@@ -3,16 +3,19 @@
 sudo apt-get install erlang
 
 GIT_DIR=~/git
+ERICA_DIR=$GIT_DIR/erica
 
 if [ ! -d "$GIT_DIR" ]; then
 	/bin/mkdir $GIT_DIR
 fi
 
-cd $GIT_DIR
-git clone git://github.com/benoitc/erica.git
-cd erica
-make
-make install
+if [ ! -d "$ERICA_DIR" ]; then
+	cd $GIT_DIR
+	git clone git://github.com/benoitc/erica.git
+	cd erica
+	make
+	make install
+fi
 
 # build an app
 WORK_DIR=~/dev
@@ -22,7 +25,7 @@ if [ ! -d "$WORK_DIR" ]; then
 	/bin/mkdir $WORK_DIR
 fi
 
-cd ~/$WORK_DIR
+cd $WORK_DIR
 if [ ! -d "$APP_DIR" ]; then
 	mkdir $APP_DIR
 	cd $APP_DIR
