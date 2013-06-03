c9b26cd75c4619c6c9583e79c4b28519755217dd
Charles Calvert <java@elvenware.com>
2013-06-03 13:16:53 -0700

    Cleaning up interface

--- Data/CouchSession01/server.js
+++ Data/CouchSession01/server.js
@@ -1,20 +1,29 @@
 // http://blog.modulus.io/nodejs-and-express-sessions
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var port = process.env.PORT || 30025;
 
 app.use(express.cookieParser());
 app.use(express.session({secret: '1234567890QWERTY'}));
 
 var previous = 'Previously you visited: ';
 
+app.get('/', function(request, response) {
+	var html = fs.readFileSync('public/index.html');
+	response.writeHeader(200, {"Content-Type": "text/html"});
+	console.log(html);
+	response.end(html);
+});
+
 app.get('/page01', function(req, res) {
 	var info = "";
 	if(req.session.lastPage) {
-		info =	previoius + req.session.lastPage + '. ';
+		info =	previous + req.session.lastPage + '. ';
 	}
 
 	req.session.lastPage = '/page01';
-	res.send('Welcome to Page01: ' + info);
+	res.send('Welcome to Page01.<br />' + info);
 });
 
 app.get('/page02', function(req, res) {
@@ -24,7 +33,7 @@ app.get('/page02', function(req, res) {
 	}
 
 	req.session.lastPage = '/page02';
-	res.send('Welcome to Page02 ' + info);
+	res.send('Welcome to Page02.<br />' + info);
 });
 
 app.get('/page03', function(req, res) {
@@ -35,7 +44,9 @@ app.get('/page03', function(req, res) {
 	}
 
 	req.session.lastPage = '/page03';
-	res.send('Welcome to Page03: ' + info);
+	res.send('Welcome to Page03.<br />' + info);
 });
 
-app.listen(process.env.PORT || 30025);
+app.use('/', express.static(__dirname + '/public'));
+console.log('Listening on port: ', port);
+app.listen(port);
