bbe7510fb75b4ef0f8cae3f03d805c8ce5cfe40d
charlie <java@elvenware.com>
2013-06-09 17:27:20 -0700

    Adding SimpleButtons02

--- HtmlCssJavascript/SimpleButtons02/index.js
+++ HtmlCssJavascript/SimpleButtons02/index.js
@@ -1,17 +1,17 @@
 var App = (function() {
-	
+
 	function App() {
 		$("#link01").click(linkClick);
 		$("#button01").click(buttonClick);
 	}
-	
-var linkClick = function() {
-	$("#data01").html("Link clicked");
-};
 
-var buttonClick = function() {
-	$("#data01").html("Button Clicked");
-};
+	var linkClick = function() {
+		$("#data01").html("Link clicked");
+	};
+
+	var buttonClick = function() {
+		$("#data01").html("Button Clicked");
+	};
 
 	return App;
 
