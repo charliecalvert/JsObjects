5ae0d6bf4ed441bb6b901dcb09de245fd2cb2e7d
charlie <java@elvenware.com>
2013-09-01 14:18:23 -0700

    Looping JavaScript Example

---
+++ JavaScript/Syntax/Loops01/index.js
@@ -0,0 +1,26 @@
+/**
+ * @author Charlie Calvert
+ */
+
+var app = {
+	basicForLoop : function() {
+		for (var i = 0; i < 5; i++) {
+			console.log(i);
+		}
+	},
+
+	basicWhileLoop : function() {
+		var count = 0;
+
+		while (count < 5) {
+			console.log(count++);
+		}
+	}
+};
+
+console.log('\nHere is the for loop:');
+
+app.basicForLoop();
+
+console.log('\nHere is the while loop:');
+app.basicWhileLoop();
