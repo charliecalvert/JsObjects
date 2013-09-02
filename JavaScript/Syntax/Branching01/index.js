d7721ed04668debf1d7cb69a795fb4fa4df7f07d
charlie <java@elvenware.com>
2013-09-01 23:40:24 -0700

    This update

--- JavaScript/Syntax/Branching01/index.js
+++ JavaScript/Syntax/Branching01/index.js
@@ -1,15 +1,62 @@
-var app = {
+var App = (function() {
 
-	isEven: function(input) {
+	Alabama = 'AL',
+	California = 'CA',
+	Texas = 'TX',
+	Washington = 'WA';
+
+	function App() {
+		isEven(2);
+		isEven(3);
+		isEven(4);
+		isEven(5);
+		runState();
+	};
+	
+	var isEven = function(input) {
 		if (input % 2 === 0) {
 			console.log('Your input of ' + input + ' is even');
 		} else {
 			console.log('Your input of ' + input + ' is odd');
 		}
-	}
-};
+	};
+	
+	var statePopulation =  function(stateAbbreviation) {
+		var result = 0;
+		
+		switch (stateAbbreviation) {
+			case 'AL': 
+				result = 4800736;
+				break;
 
-app.isEven(2);
-app.isEven(3);
-app.isEven(4);
-app.isEven(5);
+			case 'CA': 
+				result = 38053956;
+				break;
+
+			case 'TX':
+				result = 25901361;
+				break;
+
+			case 'WA': 
+				result = 6830038;
+				break;
+
+			default: 
+				result = -1;
+		}
+
+		console.log('The population of ' + stateAbbreviation + ' is ' + result);
+	};
+
+	var runState = function() {
+		statePopulation(Alabama);
+		statePopulation(California);
+		statePopulation(Texas);
+		statePopulation(Washington);
+		statePopulation('Unknown');
+}
+
+	return App;
+})();
+
+var app = new App();
