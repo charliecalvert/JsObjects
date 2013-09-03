cb00628034657d879b30e36d2dc11f060a0179a7
charlie <java@elvenware.com>
2013-09-03 12:03:42 -0700

    Function Arguments Example

---
+++ JavaScript/Syntax/FunctionArguments/index.js
@@ -0,0 +1,19 @@
+function UserException(message) {
+   this.message = message;
+   this.name = "UserException";
+}
+
+function add() {
+	if (arguments.length === 2) {
+		return arguments[0] + arguments[1];
+	} else if (arguments.length === 1) {
+		return arguments[0] * 2;
+	} else {
+		throw new UserException('You must pass in either one or two parameters.');
+	}
+}
+
+
+console.log(add(2));
+console.log(add(2, 3));
+console.log(add());
