4c6c6807e617f0ff6eb7ea6cee9b8ed1ec809612
charlie <java@elvenware.com>
2013-09-25 10:44:20 -0700

    don't use this version of Primitive types

--- JavaScript/Syntax/PrimitiveTypes/PrimitiveTypes.js
+++ JavaScript/Syntax/PrimitiveTypes/PrimitiveTypes.js
@@ -5,11 +5,10 @@ var aNull = null;
 var aUndefined = undefined;
 var foo;
 
-console.log(typeof aNumber);
-console.log(typeof aString);
+console.log("var aNumber = 3 yields this type: " + typeof aNumber);
+console.log("var aString = 'string' yields this type: " + typeof aString);
 console.log(typeof aBoolean);
 console.log(typeof aNull);
 console.log(typeof aUndefined);
 console.log(typeof foo);
-
 console.log(undefined == null)
---
+++ JavaScript/Syntax/PrimitiveTypes/RunMe.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+ECHO This code:
+type PrimitiveTypes.js
+Pause
+cls 
+ECHO Yields these results:
+node PrimitiveTypes.js
