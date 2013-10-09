7d14884ad210fb70d18ef8a0bd605aafb0b3ac6c
Charlie CedarIsle Calvert <charlie@elvenware.com>
2013-10-09 10:47:40 -0700

    Adding repeater example

---
+++ JavaScript/Design/AngularRepeater/index.js
@@ -0,0 +1,12 @@
+/**
+ * @author Charlie Calvert
+ */
+
+var PersonController = function($scope) {
+	$scope.people = [
+		{firstName: "George", lastName: "Washington", sequence: 1},
+		{firstName: "John", lastName: "Adams", sequence: 2},
+		{firstName: "Thomas", lastName: "Jefferson", sequence: 3},
+		{firstName: "James", lastName: "Madison", sequence: 4}
+	];
+};
