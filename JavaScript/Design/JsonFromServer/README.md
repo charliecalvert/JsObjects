0467ca276371b4907bb887c23c1d792423997947
Charlie CedarIsle Calvert <charlie@elvenware.com>
2013-11-02 14:42:52 -0700

    Clean up readme

--- JavaScript/Design/JsonFromServer/README.md
+++ JavaScript/Design/JsonFromServer/README.md
@@ -1,6 +1,4 @@
 DataMongo01
 ===========
 
-https://api.mongolab.com/api/1/databases?apiKey=<your-api-key>
 
-uoiZwKHNa_2xtlDVLC0VRUGbyIutEcM9
--- JavaScript/Design/JsonFromServer/index.js
+++ JavaScript/Design/JsonFromServer/index.js
@@ -5,6 +5,7 @@
 function MyController($scope, $http) {
     $scope.hint = "<p>Start with <strong>node server.js</strong> to retrieve JSON from Server</p>";
     
+    
 	var getDataJson = $http.get('data.json');
 
 	getDataJson.success(function(data, status, headers, config)  {
