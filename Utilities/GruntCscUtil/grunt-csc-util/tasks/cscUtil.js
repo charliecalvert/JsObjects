e03d18ec560d09b8ce4811ca24efc3f1f5f011d6
Charlie CedarIsle Calvert <charlie@elvenware.com>
2013-11-12 09:56:25 -0800

    Updating Grunt utils attempt

--- Utilities/GruntCscUtil/grunt-csc-util/tasks/cscUtil.js
+++ Utilities/GruntCscUtil/grunt-csc-util/tasks/cscUtil.js
@@ -1,11 +1,14 @@
 module.exports = function(grunt) { 'use strict';
 
 	grunt.registerMultiTask('cscUtil', 'cscUtil', function() {
-		var len = this.filesSrc.length;
 
 		var dirList = [];
 		var ignoreList = this.options().ignoreFolder;
-		console.log(ignoreList);
+		var subDirs = this.options().subDirs; 
+		if (typeof subDirs === 'undefined') {
+			subDirs = false;
+		}
+		console.log('Use subDirs: ' + subDirs);
 		
 		var isUnique = function(dir) {
 			return dirList.indexOf(dir) === -1;
@@ -21,7 +24,47 @@ module.exports = function(grunt) { 'use strict';
 		};
 		
 		var nestedDir = function(subDir) {
-			return subDir.indexOf('\/') !== -1;
+			if (subDirs) {
+				return false;
+			} else {
+				return subDir.indexOf('\/') !== -1;
+			}
+		}
+		
+		var done = this.async();
+		
+		
+		var runGitStatus = function(subDir) {
+			var curDir = cwd + subDir;
+			console.log('cd ' + curDir);
+			console.log('git pull');
+		}
+			
+		var runGitStatusOld = function(subDir) {
+			var curDir = cwd + subDir;
+			console.log("new Dir: " + curDir);
+			try {
+				process.chdir(curDir);
+				//console.log('New directory: ' + process.cwd());
+			}
+			catch (err) {
+				console.log('chdir: ' + err);
+			}
+			
+			console.log("runGitStatus: " + subDir);
+			var foo = grunt.util.spawn({
+				cmd : "git",
+				args : [ "status" ]
+			}, function (err, result, code) {
+					if (err) {
+						console.log('error');
+						grunt.log.error(err);
+						done(result);
+						return false;
+					}
+					console.log('Result' + result);
+					done(result);
+			});
 		}
 		
 		var iterateTroughFiles = function(abspath, rootdir, subDir, filename){
@@ -31,15 +74,21 @@ module.exports = function(grunt) { 'use strict';
 					if (!ignoreDir(subDir)) {
 						if (isUnique(subDir)) {
 							dirList.push(subDir);
-							console.log(subDir);
+							runGitStatus(subDir);
 						}
 					}
 				}
 			}
 		};
-
-		for (var x = 0; x < len; x++) {
-			grunt.file.recurse(this.filesSrc[x], iterateTroughFiles);
+		
+		var skipFirstDirs = function() {
+			var len = this.filesSrc.length;
+			for (var x = 0; x < len; x++) {
+				grunt.file.recurse(this.filesSrc[x], iterateTroughFiles);
+			}
 		}
+		
+		var cwd = process.cwd() + '\\';
+		grunt.file.recurse(".", iterateTroughFiles);
 	});
 };
