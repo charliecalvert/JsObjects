c8bea68021fe883540a18077c670867de587791d
Charles Calvert <java@elvenware.com>
2013-11-12 13:23:20 -0800

    Update of CRLF.PY

--- Python/PythonUtils/crlf.py
+++ Python/PythonUtils/crlf.py
@@ -1,23 +1,35 @@
 #! /usr/bin/env python
 "Replace CRLF with LF in argument files.  Print names of changed files."
 
-import sys, os
+import sys, os, locale
+
+def getFileSystemInfo():
+	print(locale.getpreferredencoding())
+	print(sys.getfilesystemencoding())
+	return
 
 def main():
-    for filename in sys.argv[1:]:
-        if os.path.isdir(filename):
-            print filename, "Directory!"
-            continue
-        data = open(filename, "rb").read()
-        if '\0' in data:
-            print filename, "Binary!"
-            continue
-        newdata = data.replace("\r\n", "\n")
-        if newdata != data:
-            print filename
-            f = open(filename, "wb")
-            f.write(newdata)
-            f.close()
+	
+	#s = u'\x1A'
+	#print(s.encode())	
+	#return 
+	
+	for filename in sys.argv[1:]:
+		if os.path.isdir(filename):
+			print(filename, "Directory!")
+			continue
+		data = open(filename, "rb").read()
+		dataTest = data.decode()		
+		if '\0' in dataTest:
+			print(filename, "Binary!")
+			continue		
+		newdata = dataTest.replace("\r\n", "\n")
+		newdata = newdata.replace(u'\x1A', '')
+		if newdata != data:
+			print(filename)
+			f = open(filename, "wb")
+			f.write(bytes(newdata, 'UTF-8'))
+			f.close()
 
 if __name__ == '__main__':
-    main()
+	main()
