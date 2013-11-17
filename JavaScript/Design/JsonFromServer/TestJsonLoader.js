b1236cc3f87fb57ce07c095f07a8a63a0b9f5c71
Charlie CedarIsle Calvert <charlie@elvenware.com>
2013-11-17 11:25:48 -0800

    JsonFromServer updated to include unit tests and mock tests

---
+++ JavaScript/Design/JsonFromServer/TestJsonLoader.js
@@ -0,0 +1,52 @@
+/**
+ * @author Charlie
+ */
+
+describe("mycontrollertest", function() {'use strict';
+    var myController = null;
+    var $httpBackend = null;
+
+    beforeEach(inject(function($rootScope, $controller) {
+        myController = $rootScope.$new();
+        $controller('MyController', { $scope: myController }); 
+    }));
+    
+    beforeEach(inject(function(_$httpBackend_) {
+		$httpBackend = _$httpBackend_;
+	}));
+
+    it("Test hint", function() {
+        expect(myController.hint).toEqual("<p>Start with a web server such as <strong>node server.js</strong> to retrieve JSON from Server</p>");
+    });  
+    
+    it("Test load json name", function() {
+    	$httpBackend.expectGET('data.json').respond({ 
+    		"name": "NPC01",	
+    		"hitPoints": 1,
+			"damage": 2
+		});		
+    	myController.loadJson();
+    	$httpBackend.flush();
+		expect(myController.data.name).toEqual("NPC01");
+    });
+    
+    it("Test load json hitPoints", function() {
+    	$httpBackend.expectGET('data.json').respond({
+    		"name": "NPC01", 
+   			"hitPoints": 37, 
+   			"damage": 5});
+    	myController.loadJson();
+    	$httpBackend.flush();
+		expect(myController.data.hitPoints).toEqual(37);
+    });
+    
+    it("Test load json damage", function() {
+    	$httpBackend.expectGET('data.json').respond({
+    		"name": "NPC01", 
+   			"hitPoints": 37, 
+   			"damage": 5});
+    	myController.loadJson();
+    	$httpBackend.flush();
+		expect(myController.data.damage).toEqual(5);
+    });    
+});
--- JavaScript/Design/JsonFromServer/index.js
+++ JavaScript/Design/JsonFromServer/index.js
@@ -3,17 +3,18 @@
  */
 
 function MyController($scope, $http) {
-    $scope.hint = "<p>Start with <strong>node server.js</strong> to retrieve JSON from Server</p>";
-    
-    
-	var getDataJson = $http.get('data.json');
-
-	getDataJson.success(function(data, status, headers, config)  {
-		$scope.data = data;
-	});
+    $scope.hint = "<p>Start with a web server such as <strong>node server.js</strong> to retrieve JSON from Server</p>";
+  
+  
+  	$scope.loadJson = function() {
+		var getDataJson = $http.get('data.json');
 	
-	getDataJson.error(function(data, status, headers, config) {
-		throw new Error('Oh no! An Error!');
-	});
-
+		getDataJson.success(function(data, status, headers, config)  {
+			$scope.data = data;
+		});
+		
+		getDataJson.error(function(data, status, headers, config) {
+			throw new Error('Oh no! An Error!');
+		});
+	};
 }
