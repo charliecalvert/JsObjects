3d7e606d6f436a00e5cadf63d4a4b49e088d6723
Charlie CedarIsle Calvert <charlie@elvenware.com>
2013-12-10 15:38:55 -0800

    Update to dot syntax

--- JavaScript/Design/AngularThreeModules03/Module01.js
+++ JavaScript/Design/AngularThreeModules03/Module01.js
@@ -2,13 +2,12 @@
  * @author Charlie Calvert
  */
 
-angular.module('elvenApp', ['tools']);
-
-function BoatController($scope, boat, sailboat) { 'use strict';
+angular.module('elvenApp', ['tools'])
+.controller('BoatController', function($scope, boat, sailboat) { 'use strict';
 	$scope.simple = "Simple Boat";
 	$scope.boatType = boat.description;
 	$scope.sailBoat = sailboat.description;
 	$scope.getNine = function() {
 		return sailboat.getNine();
 	};
-}
+});
