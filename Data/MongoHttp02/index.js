8d21dd757f494ae8636da4060928c7f05151f250
Charlie CedarIsle Calvert <charlie@elvenware.com>
2013-12-13 22:12:26 -0800

    More support in HTTP data transfer

--- Data/MongoHttp02/index.js
+++ Data/MongoHttp02/index.js
@@ -39,6 +39,66 @@ angular.module('customResourceDemo', ['mongoMod'])
 .controller('addressController', function($scope, address) {
 
     address.query().then(function(address) {
-        $scope.address = address;
+        $scope.addressData = address;
     });
+    
+    function setData(index) {
+    	$scope.addressData[index].firstName = 'Paul';
+    	$scope.addressData[index].lastName = 'Jones';
+    	$scope.addressData[index].address = '222 Second Street';
+    	$scope.addressData[index].city = 'Secondville';
+    	$scope.addressData[index].state = 'WA';
+    	$scope.addressData[index].zip = '22222';
+    	$scope.addressData[index].phoneHome = '22222';
+    	$scope.addressData[index].phoneMobile = '222-222-2222';
+    	$scope.addressData[index].email = '222@222.com';
+    }
+    
+    function defaultAddress() {
+    	return {
+            "firstName" : "Paul",
+            "lastName" : "Jones",
+            "address": "123 First Avenue",
+            "city": "New York",
+            "state": "NY",
+            "zip": "12345",
+            "phoneHome": "111-222-3333",
+            "phoneMobile": "555-666-8888",
+            "email": "one@two.com"            
+        };
+    }
+    
+    $scope.defaultData = function() {
+    	var address = defaultAddress();
+    	$scope.firstName = address.firstName;
+    	$scope.lastName = address.lastName;
+    	$scope.address = address.address;
+    	$scope.city = address.city;
+    	$scope.state = address.state;
+    	$scope.zip = address.zip;
+    	$scope.phoneHome = address.phoneHome;
+    	$scope.phoneMobile = address.phoneMobile;
+    	$scope.email = address.email;
+    };
+    
+    $scope.save = function() {    	
+		var  newAddress = {
+            "firstName" : $scope.firstName,
+            "lastName" : $scope.lastName,
+            "address": $scope.address,
+            "city": $scope.city,
+            "state": $scope.state,
+            "zip": $scope.zip,
+            "phoneHome": $scope.phoneHome,
+            "phoneMobile": $scope.phoneMobile,
+            "email": $scope.email            
+        };    	
+        new address(newAddress).$save(function(address, r) {
+        	 $scope.addressData.push(address);
+        });    	
+    };
+    
+    $scope.remove = function() {
+    	address.remove($scope.addressData[0]);
+    };
 });
