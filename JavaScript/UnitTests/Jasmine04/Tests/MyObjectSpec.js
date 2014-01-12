48a32ec3fd41d0623f2c64e51f9d41fb9321e30b
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-01-12 15:50:08 -0800

    Jasmine four tests cleaned up and working

--- JavaScript/UnitTests/Jasmine04/Tests/MyObjectSpec.js
+++ JavaScript/UnitTests/Jasmine04/Tests/MyObjectSpec.js
@@ -4,11 +4,13 @@ describe("MyObject", function () {
     afterEach(function () {
         obj.setState("clean");
     });
+    
     it("changes state", function () {
         obj.setState("dirty");
         console.log(obj);
         expect(obj.getState()).toEqual("dirty");
     });
+    
     it("adds states", function () {
         obj.addState("packaged");
         expect(obj.getState()).toEqual(["clean", "packaged"]);
@@ -21,33 +23,49 @@ describe("MyObject", function () {
     beforeEach(function () {
         obj.setState("clean");
     });
+    
     it("changes state", function () {
         obj.setState("dirty");
         expect(obj.getState()).toEqual("dirty");
     });
+    
     it("adds states", function () {
         obj.addState("packaged");
         expect(obj.getState()).toEqual(["clean", "packaged"]);
     });
 });
 	
-describe("test", function () {
+describe("Test Custom Matcher called toBeBetween", function () {
     beforeEach(function () {
-		this.addMatchers({
-			toBeBetween: function (rangeFloor, rangeCeiling) {
-				if (rangeFloor > rangeCeiling) {
-					var temp = rangeFloor;
-					rangeFloor = rangeCeiling;
-					rangeCeiling = temp;
-				}
-				return this.actual > rangeFloor && this.actual < rangeCeiling;
+		jasmine.addMatchers({
+			toBeBetween: function() {
+				return {
+					compare: function(actual, rangeFloor, rangeCeiling) {
+						if (rangeFloor > rangeCeiling) {
+							var temp = rangeCeiling;
+							rangeCeiling = rangeFloor;
+							rangeFloor = temp;
+						}
+						return {
+							pass: actual > rangeFloor && actual < rangeCeiling,
+							message: actual + ' is not between ' + rangeFloor + " and " + rangeCeiling
+						};  
+					}
+				};
 			}
 		});
 	});
-	it("works", function () {
+	
+	it("10 is between 5 and 30", function () {
 	    expect(10).toBeBetween(5, 30);
 	});
-	it("works too", function () {
+	
+	it("100 is between 500 and 30", function () {
 	    expect(100).toBeBetween(500, 30);
 	});
+	
+	it("29 is between 5 and 30", function () {
+	    expect(29).toBeBetween(5, 30);
+	});
+
 });
