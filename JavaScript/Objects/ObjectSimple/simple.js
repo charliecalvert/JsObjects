aca5691f2004c01bb1b619b90d07c2c568d32d25
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-01-13 23:15:37 -0800

    Simple Objects

--- JavaScript/Objects/ObjectSimple/simple.js
+++ JavaScript/Objects/ObjectSimple/simple.js
@@ -1,20 +1,24 @@
-var myObject = {
-	a: 1,
-	b: 2,
-	c: 'three',
-	four: function() {}
-};
-
-var myObject = {
+var myObject01 = {
 	firstName: "Susan",
 	lastName: "Smith"
 };
 
-var myObject1 = {
+var myObject02 = {
 	firstName: "Susan",
 	lastName: "Smith",
 	fullName: function() { 'use strict';
-		return firstName + ' ' + lastName;
+		return this.firstName + ' ' + this.lastName;
 	}
 };
 
+var myObject03 = {
+	a: 1,
+	b: 2,
+	c: 'three',
+	addMe: function() { return this.a + this.b; }
+};
+
+console.log(myObject01.firstName);
+console.log(myObject01.lastName);
+console.log(myObject02.fullName());
+console.log(myObject03.addMe());
