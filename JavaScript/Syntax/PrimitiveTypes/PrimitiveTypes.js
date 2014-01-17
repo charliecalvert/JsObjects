844c5797dab2c19ff89565efc8a17c8903e8596d
Charles Calvert <java@elvenware.com>
2014-01-17 08:44:23 -0800

    Update Primitive Types

--- JavaScript/Syntax/PrimitiveTypes/PrimitiveTypes.js
+++ JavaScript/Syntax/PrimitiveTypes/PrimitiveTypes.js
@@ -5,11 +5,10 @@ var aNull = null;
 var aUndefined = undefined;
 var foo;
 
-
-console.log(typeof aNumber + ":\t\tvar aNumber = 3");
-console.log(typeof aString + ":\t\tvar aString = 'string'");
-console.log(typeof aBoolean + ":\tvar aBoolean = true");
-console.log(typeof aNull + ":\t\tvar aNull = null");
-console.log(typeof aUndefined + ":\tvar aUndefined = undefined");
-console.log(typeof foo + ":\tvar foo");
+console.log( typeof aNumber + ":\t\tvar aNumber = 3");
+console.log( typeof aString + ":\t\tvar aString = 'string'");
+console.log( typeof aBoolean + ":\tvar aBoolean = true");
+console.log( typeof aNull + ":\t\tvar aNull = null");
+console.log( typeof aUndefined + ":\tvar aUndefined = undefined");
+console.log( typeof foo + ":\tvar foo");
 
