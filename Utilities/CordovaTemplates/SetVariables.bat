466d1b474487075c270984cfc1a4fb07326a8127
Charles Calvert <java@elvenware.com>
2014-01-30 12:18:10 -0800

    Updated SanityCheck and SetVariables to include GIT_SSH

--- Utilities/CordovaTemplates/SetVariables.bat
+++ Utilities/CordovaTemplates/SetVariables.bat
@@ -2,3 +2,4 @@ SetX JAVA_HOME "C:\Program Files\Java\jdk1.7.0_51"
 SetX ANT_HOME C:\Src\Ant\apache-ant-1.9.3
 SetX PHONEGAP_HOME C:\Src\cordova-android
 SetX ANDROID_SDK_HOME C:\Src\Eclipse\sdk
+SetX GIT_SSH C:\Program Files (x86)\PuTTY\plink.exe
