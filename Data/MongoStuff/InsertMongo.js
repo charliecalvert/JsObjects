de42379e8e17b98a7c7d7cc90d53ea8d2ff35e69
Charlie bcuservb03 Calvert <charlie@elvenware.com>
2014-02-04 17:21:06 -0800

    Rushed

--- Data/MongoStuff/InsertMongo.js
+++ Data/MongoStuff/InsertMongo.js
@@ -4,11 +4,11 @@ var format = require('util').format;
 var url01 = 'mongodb://127.0.0.1:27017/test';
 var url02 = 'mongodb://192.168.2.19:27017/test';
 
-  MongoClient.connect(url02, function(err, db) {
+  MongoClient.connect(url01, function(err, db) {
     if(err) throw err;
 
     var collection = db.collection('test_insert');
-    collection.insert({c:4}, function(err, docs) {
+    collection.insert({"firstName": "John", "lastName": "Adams"}, function(err, docs) {
 
       collection.count(function(err, count) {
         console.log(format("count = %s", count));
