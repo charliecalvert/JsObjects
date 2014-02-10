186339277445ec640667a2800973725da356b218
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-02-09 16:10:21 -0800

    DropBox updated for 64 bit

---
+++ Utilities/DropBox/DropBoxOnLinuxServer_64Bit.sh
@@ -0,0 +1,19 @@
+# Script for downloading and installing DropBox on Linux server
+# and on other headless Linux boxes that have no GUI frontend.
+# You can use dropbox.py to help you manage dropbox.
+# I recommend running this script from $HOME/bin
+
+dropBoxName=dropbox.tar.gz
+wget -O dropbox.py "https://www.dropbox.com/download?dl=packages/dropbox.py"
+wget -O $dropBoxName "https://www.dropbox.com/download?plat=lnx.x86_64"
+tar xzf $dropBoxName
+if [ ! -e ~/.dropbox-dist/dropboxd ]
+then
+	mv .dropbox-dist ~/.
+else
+	echo "I see that /home/$USER/.dropbox-dist already exists."
+	echo "I did not expect that. I am going to exit and let you"
+	echo "handle this manually."
+	exit
+fi
+~/.dropbox-dist/dropboxd
