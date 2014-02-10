ab44b281ce8845ec3935c5bbb46ae18a06a7d030
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-02-09 20:51:39 -0800

    More complete example

--- JavaScript/UnitTests/JasmineHttpBackend/Source/index.js
+++ JavaScript/UnitTests/JasmineHttpBackend/Source/index.js
@@ -2,28 +2,32 @@
  * @author Charlie Calvert
  */
 
-
 var LoadJson = (function() {
-	
+
 	function LoadJson() {
-		$('#loadJson').click({ fileName: "BackEndData.json" }, this.loadJson);	
+		$('#loadJson').click({
+			fileName : "BackEndData.json"
+		}, this.loadJson);
 		// $('#loadJson02').click({ fileName: "Moredata.json" }, loadJson);
 	}
-	
+
+
 	LoadJson.prototype.loadJson = function(event) {
 		var fileName = event.data.fileName;
-		
-		var jqxhr = $.getJSON(fileName, function(data) {
+
+		$.getJSON(fileName, function(data) {
 			$('#showJson').html(data.name);
+		}).fail(function(jqxhr, textStatus, error) {
+			console.log("error: " + error);
 		});
-		
-	    /* jqxhr.fail(function(jqxhr, textStatus, error) {
-    		console.log( "error: " + error );
-  		}); */	
+
+		/* jqxhr.fail(function(jqxhr, textStatus, error) {
+		 console.log( "error: " + error );
+		 }); */
 	};
-	
+
 	return LoadJson;
-	
+
 })();
 
 $(document).ready(function() {
--- JavaScript/UnitTests/JasmineHttpBackend/Test/TestJsonLoader.js
+++ JavaScript/UnitTests/JasmineHttpBackend/Test/TestJsonLoader.js
@@ -6,7 +6,11 @@ describe("mycontrollertest", function() {'use strict';
 	var myController = new LoadJson();
 	
 	it("Tests load json", function() {
-		spyOn($, "getJSON");	
+		var data = {"name": "NPC01", 
+			"hitPoints": 1,
+			"damage": 2
+		};
+		spyOn($, "getJSON").andReturn({fail: function(c){c(data);}});	
 		myController.loadJson({ data: { fileName: "BackEndData.json" } });
 		expect($.getJSON).toHaveBeenCalledWith("BackEndData.json", jasmine.any(Function));
 	});
