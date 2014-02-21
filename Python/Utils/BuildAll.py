84418c1e56e9f2fab6441ee610fd0fabd857fa07
Charlie bcuservb03 Calvert <charlie@elvenware.com>
2014-02-20 19:26:33 -0800

    Checking in BuildAll.py for the first time

---
+++ Python/Utils/BuildAll.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3.3
+
+import os
+import sys
+import elfutils.elffiles as elffiles
+from elfutils.MarkdownToHtml import MarkdownToHtml
+
+# Site Root
+stackEditFrom=os.environ['HOME'] + '/Dropbox/StackEdit'
+bcTo='/var/www/bc'
+normalHtml = 0
+revealHtml = 1
+
+
+# process and Copy the files
+def makeItSo(markdown, folder, files, technique = normalHtml):
+	markdown.copyFrom = stackEditFrom
+	markdown.destination = bcTo
+	elffiles.ensureDir(markdown.destination)
+	if technique == revealHtml:
+		markdown.runReveal(files);
+	else:
+		markdown.runner(files, ['start.html', 'nav.html', 'footer.html', 'end.html']);
+
+
+
+# Prog270
+def prog280(markdown):
+	files = ["DropBox", "MongoMark"];
+	makeItSo(markdown, "", files);
+
+
+# Run Program
+markdown = MarkdownToHtml()
+prog280(markdown)
