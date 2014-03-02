b0dcb8e9495c271e890bc6e2eb79ae6703524e38
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-03-01 21:20:09 -0800

    Add Powershell, first script

--- Data/MongoTalk04/Library/QueryMongo.js
+++ Data/MongoTalk04/Library/QueryMongo.js
@@ -18,7 +18,7 @@ var QueryMongo = (function() {'use strict';
 			'mongodb://192.168.2.34:27017/test',
 			'mongodb://192.168.56.101:27017/test'];
 
-		url = urls[0];
+		url = urls[2];
 	}
 
 	var getDatabase = function(callback) {
