5478b9f1d069ffc10a6a3208190b24020fc75915
Charlie MongoVBbox Calvert <charlie@elvenware.com>
2014-03-03 16:23:28 -0800

    Adding jquery fixtures

--- Data/MongoTalk04/Tests/JQuerySpec.js
+++ Data/MongoTalk04/Tests/JQuerySpec.js
@@ -2,10 +2,34 @@
  * @author Charlie Calvert
  */
 
-describe("Jasmine-JQuery example suite", function() {
+function test() {
+	describe("Jasmine-JQuery example suite", function() {
+
+		jasmine.getFixtures().fixturesPath = '../Public/';
+				
+		it('proves jasmine-jquery suite is called and working', function() {
+			expect(true).toBe(true);
+		});
+
+		it('Manually finds if the word *talk* has been loaded into the page', function(done) {
+			$("#test").load("../Public/Pieces.html #introTemplate", function() {				
+				var test = $('#test');
+				var foo = test.text();
+				expect(foo).toContain('Talk');
+				test.empty();
+				done();
+			});			
+		}); 
+
+		it('Uses Jasmine-JQuery to check for the word *talk* using loadFixtures and toContainText', function() {
+			loadFixtures('Pieces.html');
+			expect($("#introTemplate")).toContainText("Talk");
+		});
+	});	
 	
-	it('proves jasmine-jquery suite is called and working', function() {
-		expect(true).toBe(true);
-	});
-	
+}
+
+
+$(document).ready(function() {
+	test();
 });
