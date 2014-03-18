abbf86262dae8d4781cd5757bf0bc96b887ec81d
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-03-18 10:23:16 -0700

    XMBC bash shell script

---
+++ Utilities/InstallScripts/Xmbc.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python-software-properties pkg-config
+sudo add-apt-repository -r ppa:whatever-ppa-you-have-now
+sudo add-apt-repository ppa:whatever-ppa-here
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install xbmc xbmc.bins
