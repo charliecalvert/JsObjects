9fd25a26c2b07feaee46ea6b167612930eedc3a9
Charlie MongoVBbox Calvert <charlie@elvenware.com>
2014-04-12 21:04:12 -0700

    ArrayAll first check in

---
+++ JavaScript/Syntax/ArrayAll/ArrayAll.js
@@ -0,0 +1,48 @@
+/**
+ * @author Charlie Calvert
+ */
+
+var app = {
+    arrayAll: null,
+
+    init: function() {
+        'use strict';
+        arrayAll = new ArrayAll();
+    }
+};
+
+var ArrayAll = (function() {
+    'use strict';
+
+    function ArrayAll() {
+
+    }
+
+    var emptyArray = function() {
+        var empty = [];
+        return empty.length;
+    };
+
+    var lettersArray = function() {
+        var empty = [];
+        var letters = ['a', 'b', 'c'];
+        return letters.length;
+    };
+
+    var arrayConstructor = function() {
+        var numbers = new Array(1, 2, 3, 4, 5);
+        return numbers;
+    };
+
+    var mixedArray = function() {
+        return ['a', 23, false, 'mixed types in one array'];
+    };
+
+    return ArrayAll;
+
+}());
+
+$(document).ready(function() {
+    'use strict';
+    app.init();
+});
---
+++ JavaScript/Syntax/ArrayAll/Tests/ArraySpec.js
@@ -0,0 +1,8 @@
+
+describe("An Array Suite", function() { 'use strict';
+
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  
+});
