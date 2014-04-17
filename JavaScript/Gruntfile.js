6c2b0627af493ebaa3b54d0b5214e141c41e32bf
Charles Calvert <java@elvenware.com>
2014-04-17 08:56:43 -0700

    Dubious Gruntfile

--- JavaScript/Gruntfile.js
+++ JavaScript/Gruntfile.js
@@ -25,9 +25,24 @@ module.exports = function(grunt) {
 				src : [ "**/node_modules/**", '*/barFooGoo/**'
 				]				
 			}
-		}
+		},
+		
+		jsbeautifier: {
+            files: ["**/*.js", '!**/node_modules/**',
+                '!**/coverage/**',
+                '!**/Tests/Jasmine-2.0.0/**'
+            ],
+            options: {
+                js: {
+                    jslintHappy: false
+                }
+            }
+        }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-jsbeautifier');
+	
+	grunt.registerTask('pretty', ['jsbeautifier']);
 };
