48b2403bf5381257db7b4e297f2d181a26b17a58
Charlie Ubuntu Calvert <charlie@elvenware.com>
2014-05-02 21:41:18 -0700

    Properties in Object Demos

--- JavaScript/Objects/ObjectDemo01/ObjectDemo01.js
+++ JavaScript/Objects/ObjectDemo01/ObjectDemo01.js
@@ -1,14 +1,37 @@
 var myObject = {
-    myProperty01: 12,
-    myProperty02: 4,
-    addProperties: function() {
-        'use strict';
+    myProperty01 : 12,
+    myProperty02 : 4,
+    addProperties : function() {'use strict';
         return this.myProperty01 + this.myProperty02;
     },
-    multiplyProperties: function() {
-        'use strict';
+    multiplyProperties : function() {'use strict';
         return this.myProperty01 * this.myProperty02;
     }
 };
 
-console.log(myObject.multiplyProperties());
+var display = function(value) {
+    console.log(value);
+    $("#debug").append('<li>' + value + '</li>');
+};
+
+var getPropertyIsEnumerable = function(propName) {
+    var isEnumerable = myObject.propertyIsEnumerable(propName) ? "true" : "false";
+    display("enumerable: " + isEnumerable);
+};
+
+var getPropertyDescriptor = function(propName) {
+    var descriptor = Object.getOwnPropertyDescriptor(myObject, propName);
+    var description = JSON.stringify(descriptor);
+    display(description);
+};
+
+$(document).ready(function() {
+    display(myObject.multiplyProperties());
+    var keys = Object.keys(myObject);
+    for (var i = 0; i < keys.length; i++) {
+        var propName = keys[i];
+        display(propName);
+        getPropertyDescriptor(propName);
+        getPropertyIsEnumerable(propName);
+    }
+});
