c246d84a4e4e533d9c197aa0ff487f6d5f8156d4
Charlie Ubuntu Calvert <charlie@elvenware.com>
2014-05-03 21:45:23 -0700

    It's the singleton and its starting to work!

--- JavaScript/Design/Singleton/public/javascripts/SingletonModule.js
+++ JavaScript/Design/Singleton/public/javascripts/SingletonModule.js
@@ -2,26 +2,44 @@
  * @author Charlie Calvert
  */
 
-Elf.SingletonModule = ( function() {
+var elf = {};
 
-		arguments.callee._singletonInstance = this;
+elf.SingletonModule = ( function() {
 
-		function SingletonModule() {
-			if (arguments.callee._singletonInstance) {
-				return arguments.callee._singletonInstance;
-			}
-		}
+        function SingletonModule() {
+            
+            
+            if (arguments.callee._singletonInstance) {
+                return arguments.callee._singletonInstance;
+            } else {
+                arguments.callee._singletonInstance = this;
+                return this;
+            }
+        }
 
+        SingletonModule.prototype.publicMethod = function() {
+            return "I'm the SingletonModule.publicMethod.";
+        };
 
-		SingletonModule.prototype.publicMethod = function() {
-			return "I'm the SingletonModule.publicMethod.";
-		};
+        SingletonModule.prototype.display = function(value) {
+            $('#debug').append('<li>' + value + '</li>');
+        };
 
-		return SingletonModule;
+        return SingletonModule;
 
-	}());
+    }());
 
-var a = new MySingletonClass()
-var b = MySingletonClass()
-Print(a === b);
-// prints: true
+$(document).ready(function() {
+    var a = new elf.SingletonModule();
+    var b = new elf.SingletonModule();
+    var c = new elf.SingletonModule();
+    var d = new elf.SingletonModule();
+    a.display(a === b);
+    a.display(a === c);
+    a.display(a === d);
+    a.display(b === c); 
+    a.display(c === d); 
+    // Sanity Check  
+    var e = new Array();    
+    a.display(a === e);
+});
