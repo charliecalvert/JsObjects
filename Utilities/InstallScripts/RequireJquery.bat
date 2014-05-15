46893d60043cfdbcd8e46407af07bd37c94679d7
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-05-15 10:15:16 -0700

    Add in RequireJquery scripts with wget

---
+++ Utilities/InstallScripts/RequireJquery.bat
@@ -0,0 +1,2 @@
+wget http://requirejs.org/docs/release/2.1.11/comments/require.js
+wget http://code.jquery.com/jquery-2.1.1.js
---
+++ Utilities/InstallScripts/RequireJquery.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+wget http://requirejs.org/docs/release/2.1.11/comments/require.js
+wget http://code.jquery.com/jquery-2.1.1.js
