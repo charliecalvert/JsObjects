ebf7873ab61c4f91cb11766a6e4668970fb6e3a2
Charlie Ubuntu Calvert <charlie@elvenware.com>
2014-05-19 12:27:20 -0700

    Final commit of CreateData

--- Data/MongoTalk04/CreateData.js
+++ Data/MongoTalk04/CreateData.js
@@ -7,97 +7,158 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
 var fs = require('fs');
+var assert = require('assert');
+var loadConfig = require('./Library/LoadConfig.js').loadConfig;
+var argv = require('minimist')(process.argv.slice(2));
 
-var QueryMongo = (function() {'use strict';
+var QueryMongo = (function() {
+	'use strict';
 
-	var url01 = 'mongodb://127.0.0.1:27017/test';
-	var url02 = 'mongodb://192.168.2.19:27017/test';
-	var url03 = 'mongodb://192.168.56.101:27017/test';
+	// var url = 'mongodb://127.0.0.1:27017/test';
+
+	var url = null;
+	var that = null;
+	var database = null;
+	var collectionName = 'test_insert';
 
 	function QueryMongo() {
-
+		console.log("Constructor called.")
+		that = this;
 	}
 
+	QueryMongo.prototype.run = function(option) {
+		loadConfig(function(urls) {
+			var mongoTalkJson = JSON.parse(urls);
+			url = mongoTalkJson.urls[mongoTalkJson.selectedUrl];
+			console.log("The Mongo URL: " + url);
+			that.getData(option);
+			console.log("Leaving loadConfig");
+		});
+	}
+
+	var getDatabase = function(callback) {
+		console.log('Called QueryMongo.getDatabase: ');
+		if (database !== null) {
+			console.log('database exists');
+			// showDatabase(database);
+			if (database.openCalled === false) {
+				console.log('calling open database');
+				database.open(function(err, database) {
+					console.log('In database open callback');
+					if (err) {
+						console.log("found err");
+						throw err;
+					}
+					callback(database);
+				});
+			} else {
+				callback(database);
+			}
+		} else {
+			console.log('Querying for database: ' + url);
+			MongoClient.connect(url, function(err, databaseResult) {
+				/*
+				 * if (err) { throw err; }
+				 */
+				assert.equal(null, err);
+				assert.ok(databaseResult !== null);
+				database = databaseResult;
+				// showDatabase(database);
+				callback(database);
+			});
+		}
+	};
 
 	QueryMongo.prototype.getData = function(option) {
 		console.log('Called getData');
 
-		// Open the test database that comes with MongoDb
-		MongoClient.connect(url01, function(err, database) {
-			if (err) {
-				throw err;
-			}
-			console.log('IngetDataCallback');
-			if (option === 'insert') {
-				insertCollection(database);
-			} else if (option === 'remove') {
-				removeCollection(database);
-			} else {
-				getCollection(database);
-			}
-		});
-
-	};
-
-	var insertData = function(newRecord) {
-		collection.insert(newRecord, function(err, docs) {
-			if (err) {
-				throw err;
-			} else {
-				console.log('Inserted: ' + newRecord);
-			}
-		});
+		if (option === 'insert') {
+			insertCollection(database);
+		} else if (option === 'remove') {
+			removeCollection(database);
+		} else {
+			getCollection(database);
+		}
+		console.log("Leaving get data");
 	};
 
 	var insertCollection = function(database) {
-		var collection = database.collection('test_insert');
-		for (var count = 10000; count < 10005; count++) {
-			var newRecord = {
-				firstName : "Abe" + count,
-				"lastName" : "Lincoln" + count,
-				"address" : count + " Green Street",
-				"city" : "Bellevue",
-				"state" : "WA",
-				"zip" : 98002
-			};
-			insertData(newRecord);
-		}
-		getCollection(database);
+
+		getDatabase(function(database) {
+			var count, collection = database.collection(collectionName);
+			var records = [];
+			
+			for (count = 10000; count < 10005; count++) {
+				var newRecord = {
+					firstName : "Abe" + count,
+					"lastName" : "Lincoln" + count,
+					"address" : count + " Green Street",
+					"city" : "Bellevue",
+					"state" : "WA",
+					"zip" : 98002
+				};
+				records.push(newRecord);
+			}
+			// insertData(collection, records);
+			collection.insert(records, function(err, docs) {
+				if (err) {
+					throw err;
+				} else {
+					console.log('Inserted: ' + JSON.stringify(docs));
+					database.close();
+				}
+			});
+			console.log("Leaving insert collection");
+		});
 	};
 
 	var getCollection = function(database) {
 		console.log('Get Collection Called');
-		var collection = database.collection('test_insert');
+		getDatabase(function(database) {
+			var collection = database.collection(collectionName);
 
-		// Count documents in the collection
-		//collection.count(function(err, count) {
-		//	console.log(format("count = %s", count));
-		//});
-
-		// Send the collection to the client.
-		collection.find().toArray(function(err, theArray) {
-			console.dir(theArray);
-			//console.log('Collection shown');
-			database.close();
-		});
+			// Send the collection to the client.
+			collection.find().toArray(function(err, theArray) {
+				console.dir(theArray);
+				database.close();
+			});
+		})
 
 	};
 
 	var removeCollection = function(database) {
-		var collection = database.collection('test_insert');
-		collection.remove(function(err) {
-			if (err) {
-				throw err;
-			}
-			database.close();
+		getDatabase(function(database) {
+			var collection = database.collection(collectionName);
+			collection.remove(function(err) {
+				if (err) {
+					throw err;
+				}
+				database.close();
+			});
 		});
 	};
 
 	return QueryMongo;
 
-})();
+}());
 
-var q = new QueryMongo();
-q.getData('insert');
-// q.getData('remove');
-// q.getData('show');
+function run(option) {
+	var q = new QueryMongo();
+	q.run(option);
+}
+
+if (argv.option) {
+	console.log(argv.option);
+	run(argv.option);
+} else {
+	console.log("Please pass in an option");
+	console.log();
+	console.log("  Usage: node CreateData.js --option=[OPTION]");
+	console.log("    Options are: insert, show, remove ");
+	console.log();
+	console.log("  Examples: ");
+	console.log("    node CreateData.js --option=show");
+	console.log("    node CreateData.js --option=insert");
+	console.log("    node CreateData.js --option=remove");
+}
+
