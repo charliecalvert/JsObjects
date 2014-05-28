f97838a919ce19294c01a3cfe87f62ac1347cb1b
Charlie Ubuntu Calvert <charlie@elvenware.com>
2014-05-28 15:02:40 -0700

    Start testing Singleton

---
+++ JavaScript/Design/Singleton/Tests/SingletonSpec.js
@@ -0,0 +1,15 @@
+/*globals describe:true, it:true, expect:true, define: true */
+
+define(function(require) {
+    'use strict';
+
+    describe("Singleton Suite", function() {
+
+        it("proves we can run a test", function() {
+            expect(true).toBe(true);
+        });
+
+    });
+
+});
+
---
+++ JavaScript/Design/Singleton/routes/UnitTests.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(request, response) {
+	'use strict';
+	console.log('UnitTests sent');
+	response.render('UnitTests', {
+		title : 'Singleton Tests'
+	});
+});
+
+module.exports = router;
