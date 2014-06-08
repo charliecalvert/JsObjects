3753209e54210655edbbe2606084fcf68375505a
Charlie Ubuntu Calvert <charlie@elvenware.com>
2014-06-08 16:42:18 -0700

    Needs jquery file

--- JavaScript/Syntax/CopyObject/index.js
+++ JavaScript/Syntax/CopyObject/index.js
@@ -1,5 +1,9 @@
-window.onload = function() {
+$(document).ready(function() {
 	console.log(myObject.add());
 	var newObject = copyObject(myObject);
 	console.log(newObject.add());
-}
+    // Shallow copy
+    var newObject1 = jQuery.extend({}, myObject);
+    // Deep copy
+    var newObject2 = jQuery.extend(true, {}, myObject);
+});
