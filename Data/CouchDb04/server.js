22fda22983899103d82319035530be5b6dc0daaa
Charlie at BC <charlie@elvenware.com>
2014-11-11 07:59:47 -0800

    Do server code in a callback

--- Data/CouchDb04/server.js
+++ Data/CouchDb04/server.js
@@ -3,16 +3,6 @@ var nano = require('nano')('http://localhost:5984');
 var docName = 'bigNames';
 var dbName = 'bc_data';
 
-function insert() {
-	nano.db.create(dbName);
-	var prog = nano.db.use(dbName);
-
-	prog.insert({ 'firstName': 'Suzie', 'lastName': 'Higgins'}, docName, function(err, body) {
-	  if (!err)
-		console.log(body);
-	});
-}
-
 var readIt = function() {
 	var prog = nano.db.use(dbName);
 	prog.get(docName, { revs_info: true }, function(err, body) {
@@ -21,5 +11,17 @@ var readIt = function() {
 	});
 }
 
+function insert() {
+	nano.db.create(dbName);
+	var prog = nano.db.use(dbName);
+
+	prog.insert({ 'firstName': 'Suzie', 'lastName': 'Higgins'}, docName, function(err, body) {
+	  if (!err)
+		console.log(body);
+		readIt();
+	});
+}
+
+
 insert();
-readIt();
+// readIt();
