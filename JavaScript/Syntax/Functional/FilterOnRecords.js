76458e0fe8149416a21e578c05fd7d32edba0182
Charlie MongoVBbox Calvert <charlie@elvenware.com>
2014-11-24 12:57:26 -0800

    Filter on Presidents

--- JavaScript/Syntax/Functional/FilterOnRecords.js
+++ JavaScript/Syntax/Functional/FilterOnRecords.js
@@ -1,45 +1,48 @@
 //From here: http://jhusain.github.io/learnrx/
 
 function go() {
-	var newReleases = [
-		{
-			"id": 70111470,
-			"title": "Die Hard",
-			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
-			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-			"rating": 4.0,
-			"bookmark": []
-		},
-		{
-			"id": 654356453,
-			"title": "Bad Boys",
-			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
-			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-			"rating": 5.0,
-			"bookmark": [{ id:432534, time:65876586 }]
-		},
-		{
-			"id": 65432445,
-			"title": "The Chamber",
-			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
-			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-			"rating": 4.0,
-			"bookmark": []
-		},
-		{
-			"id": 675465,
-			"title": "Fracture",
-			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
-			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-			"rating": 5.0,
-			"bookmark": [{ id:432534, time:65876586 }]
-		}
-	];
+	var presidents = [ {
+		"presidentName" : "George Washington",
+		"termStart" : "1789",
+		"termEnd" : "1797",
+		"born" : "1732",
+		"died" : "1799"
+	}, {
+		"presidentName" : "John Adams",
+		"termStart" : "1797",
+		"termEnd" : "1801",
+		"born" : "1735",
+		"died" : "1826"
+	}, {
+		"presidentName" : "Thomas Jefferson",
+		"termStart" : "1801",
+		"termEnd" : "1809",
+		"born" : "1743",
+		"died" : "1826"
+	}, {
+		"presidentName" : "James Madison",
+		"termStart" : "1809",
+		"termEnd" : "1817",
+		"born" : "1751",
+		"died" : "1836"
+	}, {
+		"presidentName" : "James Monroe",
+		"termStart" : "1817",
+		"termEnd" : "1825",
+		"born" : "1758",
+		"died" : "1831"
+	}, {
+		"presidentName" : "John Quincy Adams",
+		"termStart" : "1825",
+		"termEnd" : "1829",
+		"born" : "1767",
+		"died" : "1848"
+	}];
 
 	var videos = [];
-	newReleases.forEach(function(item) { 
-		console.log("iterate: " + item.rating);
-		if (item.rating >= 5.0) {
+	presidents.forEach(function(item) {
+		console.log("birth year: " + item.born);
+		if (item.born >= "1750") {
 			videos.push(item);
 		}
 	});
@@ -47,4 +50,4 @@ function go() {
 
 }
 
-console.log(go());
+console.log("Born later than 1750:\n", go());
