a2e51dd654c2bc5e78c98e0e50ac17a6d1d46f9c
Charlie Cygwin Home CedarIsle <charlie@elvenware.com>
2015-01-15 00:34:54 +0000

    DosAlias

---
+++ Utilities/SetupWindows/DosAlias.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+
+call doskey.exe gt=cd C:\Src\Git\Writing\Tech\Games\ThreePointerLock
+call doskey.exe jo=cd C:\Src\Git\JsObjects
+call doskey.exe jod=cd C:\Src\Git\JsObjects\Data
+call doskey.exe joj=cd C:\Src\Git\JsObjects\JavaScript
+call doskey.exe jou=cd C:\Src\Git\JsObjects\Utilities
+call doskey.exe jot=cd C:\Src\Git\JsObjects\JavaScript\UnitTests
+call doskey.exe batch=cd C:\Src\Batch
+call doskey.exe gitgo=cd C:\Src\Git
+call doskey.exe home=cd c:\Users\Charlie
+call doskey.exe hist=cd C:\Src\Git\historycode
+call doskey.exe sp=cd C:\Src\Git\Writing\Tech\Games\SpiritPath
+call doskey.exe isit=cd C:\Src\Git\Isit3202014
+call doskey.exe jackson=cd \Src\Git\Isit3202014\isit320_jackson
+call doskey.exe anderson=cd \Src\Git\Isit3202014\isit320_anderson
+call doskey.exe john=cd \Src\Git\Isit3202014\isit320_john
+call doskey.exe tania=cd \Src\Git\Isit3202014\isit320_vendrovska
+call doskey.exe waters=cd \Src\Git\Isit3202014\isit320_waters
+call doskey.exe wu=cd \Src\Git\Isit3202014\isit320_wu
