2e06e827722d0fdb1061570b294b801b7f3e38ce
Charlie CedarIsle Calvert <charlie@elvenware.com>
2015-01-23 09:10:25 -0800

    Script to pull key repos

---
+++ Utilities/SetupLinuxBox/GetCharlieRepos.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+git clone http://github.com/charliecalvert/Isit322-Assigments.git
+git clone http://github.com/charliecalvert/Prog272-Assigments.git
+git clone http://github.com/charliecalvert/JsObjects.git
