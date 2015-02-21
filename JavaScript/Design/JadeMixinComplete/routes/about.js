81bde6c62a3b6ed911d91a5ca08196f1eb8d797b
Charlie CedarIsle Calvert <charlie@elvenware.com>
2015-02-21 10:50:00 -0800

    Everthing

---
+++ JavaScript/Design/JadeMixinComplete/routes/about.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/', function(req, res) {
+  res.render('about', { title: 'about' });
+});
+
+module.exports = router;
