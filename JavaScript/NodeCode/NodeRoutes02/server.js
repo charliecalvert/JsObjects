9be3f6a6d07c57fd4663b74caf40acdf3bba0450
Charlie Ubuntu Calvert <charlie@elvenware.com>
2015-03-12 09:03:58 -0700

    NodeRoutes02 updated to work with latest express code

--- JavaScript/NodeCode/NodeRoutes02/server.js
+++ JavaScript/NodeCode/NodeRoutes02/server.js
@@ -1,62 +1,16 @@
 var express = require('express');
-var connect = require('connect');
 var app = express();
-var fs = require('fs');
-var getNine = require('./Library/GetNine');
-var addingMachine = require('./Library/AddingMachine');
 
-// Instead of bodyParser
-app.use(connect.urlencoded());
-app.use(connect.json());
+// For posts
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var port = process.env.PORT || 30025;
-
-app.get('/getNine', function(request, response) {
-    'use strict';
-    console.log('getNine called');
-    response.send({
-        "result": getNine.getNine()
-    });
-});
-
-// With a get, the parameters are passed in request.query
-app.get('/add', function(request, response) {
-    'use strict';
-    console.log('add called');
-    console.log(request.query);
-    var operandA = parseInt(request.query.operandA);
-    var operandB = parseInt(request.query.operandB);
-    var result = addingMachine.myObject.add(operandA, operandB);
-    response.send({
-        "result": result
-    });
-});
-
-/* To handle a post, we have to add express.bodyParser, shown above
-   Now our parameters come in on request.body */
-app.post('/add', function(request, response) {
-    'use strict';
-    console.log('add called');
-    console.log(request.body);
-    var operandA = parseInt(request.body.operandA);
-    var operandB = parseInt(request.body.operandB);
-    var result = addingMachine.myObject.add(operandA, operandB);
-    response.send({
-        "result": result
-    });
-});
-
-app.get('/', function(request, response) {
-    'use strict';
-    var html = fs.readFileSync(__dirname + '/Public/index.html');
-    response.writeHeader(200, {
-        "Content-Type": "text/html"
-    });
-    response.write(html);
-    response.end();
-});
-
+// Middleware. First getter the router from index.js
+var router = require('./Routes/index');
+// Then tell express about our middleware
+app.use('/', router);
 
 app.use("/", express.static(__dirname + '/Public'));
-app.listen(port);
-console.log('Listening on port :' + port);
+
+module.exports = app;
