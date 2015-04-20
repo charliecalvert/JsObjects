d74743751512701d74df90ef1b4c45df22957c0c
Charlie CedarIsle Calvert <charlie@elvenware.com>
2015-04-20 13:16:22 -0700

    My Changes

--- JavaScript/Design/Angular02/index.js
+++ JavaScript/Design/Angular02/index.js
@@ -3,43 +3,69 @@
  */
 
 (function() {
-	var app = angular.module('main', []);
+    
+    var app = angular.module('main', []);
 
-	app.controller('ListControl', function($scope) {
+    app.controller('ListControl', function($scope) {
 
-		'use strict';
+        'use strict';        
 
-		var listData = [ 
-		    { text : 'Attend class at BC', done : false }, 
-		    { text : 'Complete JavaScript programs', done : false } 
-		];
+        var listData = [ 
+            { text : 'Attend class at BC', done : false }, 
+            { text : 'Complete JavaScript programs', done : false } 
+        ];
+    
+        $scope.oldTodos = [];
+        $scope.todoList = listData;
+        $scope.archiveDone = [];
 
-		$scope.todoList = listData;
+        $scope.addTodo = function() {
+            $scope.todoList.push({
+                text : $scope.todoText,
+                done : false
+            });
+            $scope.todoText = '';
+        };
 
-		$scope.addTodo = function() {
-			$scope.todoList.push({
-				text : $scope.todoText,
-				done : false
-			});
-			$scope.todoText = '';
-		};
+        $scope.remaining = function() {
+            var count = 0;
+            angular.forEach($scope.todoList, function(todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
 
-		$scope.remaining = function() {
-			var count = 0;
-			angular.forEach($scope.todoList, function(todo) {
-				count += todo.done ? 0 : 1;
-			});
-			return count;
-		};
+        function arrayUnique(array) {
+            var a = array.concat();
+            for(var i=0; i<a.length; ++i) {
+                for(var j=i+1; j<a.length; ++j) {
+                    if(a[i] === a[j])
+                        a.splice(j--, 1);
+                }
+            }
 
-		$scope.archive = function() {
-			var oldTodos = $scope.todoList;
-			$scope.todoList = [];
-			angular.forEach(oldTodos, function(todo) {
-				if (!todo.done)
-					$scope.todoList.push(todo);
-			});
-		};
+            return a;
+        };
 
-	});
+        $scope.archive = function() {
+            $scope.oldTodos = $scope.oldTodos.concat($scope.todoList);
+            $scope.oldTodos = arrayUnique($scope.oldTodos);
+            $scope.todoList = [];
+            angular.forEach($scope.oldTodos, function(todo) {
+                if (!todo.done) {
+                    $scope.todoList.push(todo);
+                } else {
+                    $scope.archiveDone.push(todo);
+                    $scope.archiveDone = arrayUnique($scope.archiveDone);
+                }
+            });
+        };
+        
+        $scope.showTodos = function() {
+            angular.forEach($scope.oldTodos, function(todo) {
+                console.log(todo);
+            });
+        };
+
+    });
 })();
