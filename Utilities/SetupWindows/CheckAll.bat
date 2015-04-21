191009e764e03ca6b110eab55382557abf887574
Charlie at A134 <charlie@elvenware.com>
2015-04-21 07:55:31 -0700

    CheckAll

--- Utilities/SetupWindows/CheckAll.bat
+++ Utilities/SetupWindows/CheckAll.bat
@@ -1,21 +1,29 @@
 set BAR=c:\temp\bar.txt
 cls
+
 cd JsObjects 
 echo **** JsObjects **** > %BAR%
 git status >> %BAR%
-cd ..\Writing
+
+cd ..\writings
 echo **** Writing **** >> %BAR%
 git status  >> %BAR%
+
 cd ..\CloudNotes
 echo **** CloudNotes **** >> %BAR%
 git status  >> %BAR%
+
 cd ..\charliecalvert.github.io
 echo **** charliecalvert.github.io **** >> %BAR%
 git status >> %BAR%
-cd ..
+
 cd ..\elfsite
 echo **** elfsite **** >> %BAR%
 git status >> %BAR%
+
+cd ..\prog272-calvert
+echo **** prog272-calvert **** >> %BAR%
+git status >> %BAR%
 cd ..
 
-cat %BAR%
+type %BAR%
