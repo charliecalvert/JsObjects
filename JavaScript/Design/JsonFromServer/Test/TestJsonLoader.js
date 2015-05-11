644ba414f732a9e22910edce3837dade3771f3b8
Charlie Ubuntu Calvert <charlie@elvenware.com>
2015-05-11 07:05:03 -0700

    Status

--- JavaScript/Design/JsonFromServer/Test/TestJsonLoader.js
+++ JavaScript/Design/JsonFromServer/Test/TestJsonLoader.js
@@ -2,72 +2,82 @@
  * @author Charlie Calvert
  */
 
-describe("mycontrollertest", function() {'use strict';
-	var myController = null;
-	var $httpBackend = null;
+(function () {
 
-	beforeEach(function() {
-		module('jsonServerMod');	
-	});
-	
-	beforeEach(inject(function($rootScope, $controller) {
-		myController = $rootScope.$new();
-		$controller('myController', { $scope: myController });
-	}));
+    'use strict';
 
-	beforeEach(inject(function(_$httpBackend_) {
-		$httpBackend = _$httpBackend_;
-	}));
+    describe("mycontrollertest", function () {
 
-	afterEach(function() {
-		$httpBackend.verifyNoOutstandingExpectation();
-		$httpBackend.verifyNoOutstandingRequest();
-	});
+        var myScope;
+        var $httpBackend = null;
 
-	it("Test hint", function() {
-		expect(myController.hint).toEqual("<p>Start with a web server such as <strong>node server.js</strong> to retrieve JSON from Server</p>");
-	});
+        beforeEach(function () {
+            module('jsonServerMod');
+        });
 
-	it("Test load json name", function() {
-		$httpBackend.expectGET('data.json').respond({
-			"name": "NPC01",
-			"hitPoints": 1,
-			"damage": 2
-		});
-		myController.loadJson();
-		$httpBackend.flush();
-		expect(myController.data.name).toEqual("NPC01");
-	});
+        beforeEach(inject(function ($rootScope, $controller) {
+            myScope = $rootScope.$new();
+            $controller('myController', {
+	    	    $scope: myScope});
+		    }));
 
-	it("Test load json hitPoints", function() {
-		$httpBackend.expectGET('data.json').respond({
-			"name": "NPC01",
-			"hitPoints": 37,
-			"damage": 5});
-		myController.loadJson();
-		$httpBackend.flush();
-		expect(myController.data.hitPoints).toEqual(37);
-	});
+        beforeEach(inject(function (_$httpBackend_) {
+            $httpBackend = _$httpBackend_;
+        }));
 
-	it("Test load json damage", function() {
-		$httpBackend.expectGET('data.json').respond({
-			"name": "NPC01",
-			"hitPoints": 37,
-			"damage": 5});
-		myController.loadJson();
-		$httpBackend.flush();
-		expect(myController.data.damage).toEqual(5);
-	});
-	
-	it("Test Second load json name", function() {
-		$httpBackend.expectGET('moreData.json').respond({
-			"name": "NPC02",
-			"hitPoints": 1,
-			"damage": 2
-		});
-		myController.loadJson02('moreData.json');
-		$httpBackend.flush();
-		expect(myController.data2.name).toEqual("NPC02");
-	});
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
 
-});
+        it("Test hint", function () {
+            expect(myScope.hint).toEqual("<p>Start with a web server such as <strong>node server.js</strong> to retrieve JSON from Server</p>");
+        });
+
+        it("Test load json name", function () {
+            $httpBackend.expectGET('data.json').respond({
+                "name": "NPC01",
+                "hitPoints": 1,
+                "damage": 2
+            });
+            myScope.loadJson();
+            $httpBackend.flush();
+            expect(myScope.data.name).toEqual("NPC01");
+        });
+
+        it("Test load json hitPoints", function () {
+            $httpBackend.expectGET('data.json').respond({
+                "name": "NPC01",
+                "hitPoints": 37,
+                "damage": 5
+            });
+            myScope.loadJson();
+            $httpBackend.flush();
+            expect(myScope.data.hitPoints).toEqual(37);
+        });
+
+        it("Test load json damage", function () {
+            $httpBackend.expectGET('data.json').respond({
+                "name": "NPC01",
+                "hitPoints": 37,
+                "damage": 5
+            });
+            myScope.loadJson();
+            $httpBackend.flush();
+            expect(myScope.data.damage).toEqual(5);
+        });
+
+        it("Test Second load json name", function () {
+            $httpBackend.expectGET('moreData.json').respond({
+                "name": "NPC02",
+                "hitPoints": 1,
+                "damage": 2
+            });
+            myScope.loadJson02('moreData.json');
+            $httpBackend.flush();
+            expect(myScope.data2.name).toEqual("NPC02");
+        });
+
+    });
+
+})();
