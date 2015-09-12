688728c1053efa6652f856a6b6e96296f2d47e7c
Charlie CedarIsle Calvert <charlie@elvenware.com>
2015-09-12 11:45:45 -0700

    Working a bit better with support for two differnt kinds of walks

--- JavaScript/NodeCode/WalkJsObjects/app.js
+++ JavaScript/NodeCode/WalkJsObjects/app.js
@@ -1,64 +1,60 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
 var path = require('path');
-var walk = require('./public/javascripts/WalkJsObjects').walk;
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 30025);
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
-
-
-app.get('/walk', function(request, response) {
-    'use strict';
-    // If you run Node in Eclipse, to access JSOBJECTS, you made need 
-    // to choose Run | Run Configurations | Environment | Select
-    var dirToWalk = process.env.JSOBJECTS;
-    // var dirToWalk = getHomeDir + '/bin';
-    console.log("About to walk: " + dirToWalk);
-    walk(dirToWalk, ['karma.conf.js', 'Gruntfile.js'], ['node_modules', 'JavaScript'], function(err, data) {
-        if (err) {
-            console.log(err);
-            response.send({
-                result: "Error",
-                error: err
-            });
-        } else {
-            console.log(data);
-            response.send({
-                result: "Success",
-                files: data
-            });
-        }
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
     });
-
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function() {
-    'use strict';
-    console.log('Express server listening on port ' + app.get('port'));
-});
+module.exports = app;
