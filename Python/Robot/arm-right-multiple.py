a0697f08a904527be35c759f20f75b2cb2ca0849
Charlie on RohanElf <charlie@elvenware.com>
2015-09-15 21:44:33 -0700

    Walking and multiple arm moves

---
+++ Python/Robot/arm-right-multiple.py
@@ -0,0 +1,76 @@
+# -*- encoding: UTF-8 -*-
+
+'''Cartesian control: Arm trajectory example'''
+''' This example is only compatible with NAO '''
+
+import argparse
+import motion
+import almath
+from naoqi import ALProxy
+
+def moveArm(motionProxy, arm):
+    effector   = arm
+    frame      = motion.FRAME_TORSO
+    axisMask   = almath.AXIS_MASK_VEL # just control position
+    useSensorValues = False
+
+    path = []
+    currentTf = motionProxy.getTransform(effector, frame, useSensorValues)
+    targetTf  = almath.Transform(currentTf)
+    targetTf.r1_c4 += 0.03 # x
+    if arm=='LArm':
+        targetTf.r2_c4 += 0.03 # y
+    else:
+        targetTf.r2_c4 -= 0.03 # y
+
+    path.append(list(targetTf.toVector()))
+    path.append(currentTf)
+
+    # Go to the target and back again
+    times      = [2.0, 4.0] # seconds
+
+    motionProxy.transformInterpolations(effector, frame, path, axisMask, times)
+
+
+def main(robotIP, PORT=9559):
+    ''' Example showing a path of two positions
+    Warning: Needs a PoseInit before executing
+    '''
+
+    motionProxy  = ALProxy("ALMotion", robotIP, PORT)
+    postureProxy = ALProxy("ALRobotPosture", robotIP, PORT)
+
+    # Wake up robot
+    motionProxy.wakeUp()    
+
+    # Send robot to Stand Init
+    postureProxy.goToPosture("StandInit", 0.5)
+    
+    tts = ALProxy("ALTextToSpeech", "192.168.2.17", 9559)
+    
+    tts.say("I'm going to extend my left arm.")
+    moveArm(motionProxy, "LArm")
+    
+    tts.say("Now I'm going to extend my right arm 4 times.")    
+    moveArm(motionProxy, "RArm")
+    moveArm(motionProxy, "RArm")
+    moveArm(motionProxy, "RArm")
+    moveArm(motionProxy, "RArm")
+    
+    tts.say("I'm going to extend my left arm once more.")
+    moveArm(motionProxy, "LArm")
+    
+    tts.say("Now I will go back to the rest position!")
+
+    # Go to rest position
+    motionProxy.rest()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default="192.168.2.17",
+                        help="Robot ip address")
+    parser.add_argument("--port", type=int, default=9559,
+                        help="Robot port number")
+
+    args = parser.parse_args()
+    main(args.ip, args.port)
