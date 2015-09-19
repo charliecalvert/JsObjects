4054b0d64286d06eb813d203a5b8f9759414b212
Charlie CedarIsle Calvert <charlie@elvenware.com>
2015-09-18 21:10:47 -0700

    Node install alternative

--- Utilities/NodeInstall/NodeInstall.sh
+++ Utilities/NodeInstall/NodeInstall.sh
@@ -5,10 +5,17 @@ sudo apt-get install curl
 # Optionally uncomment the canonical setup script for Node.js v0.10
 # If you do this, then comment out the v0.12 script below
 # Details are here: https://nodesource.com/blog/nodejs-v012-iojs-and-the-nodesource-linux-repositories
+# Also see: https://github.com/nodesource/distributions
+
+# The setup script name for Node.js v0.10
 # curl -sL https://deb.nodesource.com/setup | sudo bash -
 
 # The new setup script name for Node.js v0.12
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+# curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+
+# The new setup script for Node.js v4.X
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+
 
 # Install node
 sudo apt-get install -y nodejs
