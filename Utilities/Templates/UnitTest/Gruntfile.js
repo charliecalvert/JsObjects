a7992be5e320e8c862d166ee278a757819489c22
Charlie Ubuntu Calvert <charlie@elvenware.com>
2015-10-08 11:33:24 -0700

    Updates to CreateExpressProject and adding code for UnitTesting

---
+++ Utilities/Templates/UnitTest/Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+	'use strict';
+
+	grunt.initConfig({
+
+		pkg : '<json:package.json>',
+
+		karma : {
+			karma : {
+				configFile : 'karma.conf.js'
+			}
+		},
+
+		jshint : {
+			files : [ '**/*.js' ],
+
+			options : {
+				ignores : [ 
+				     '**/node_modules/**', '**/components/**'
+				],
+				reporter : 'checkstyle',
+				reporterOutput : 'result.xml',
+				strict : true,
+				globals : {
+					describe : true,
+					afterEach : true,
+					beforeEach : true,
+					inject : true,
+					it : true,
+					jasmine : true,
+					expect : true
+				}
+			}
+		},
+
+	});
+
+	grunt.loadNpmTasks('grunt-karma');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.registerTask('test', [ 'jshint', 'karma' ]);
+
+};
