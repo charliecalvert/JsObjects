d273d0be4fb17f8ea2494fc016504318b7242c56
Charlie Ubuntu Calvert <charlie@elvenware.com>
2015-10-15 17:03:42 -0700

    Adding in delicious javascript links

---
+++ Utilities/Templates/WebServices/delicious-javascript-links.js
@@ -0,0 +1,141 @@
+/**
+ * Created by charlie on 10/13/15.
+ */
+
+var deliciousLinks = [
+	{
+		"a": "charliecalvert",
+		"d": "Elvenware JavaScript Home Page",
+		"n": "",
+		"u": "http://www.elvenware.com/charlie/development/web/JavaScript/",
+		"t": [
+			"javascript",
+			"elvenware"
+		],
+		"dt": "2015-10-08T19:33:33Z"
+	},
+	{
+		"a": "charliecalvert",
+		"d": "Speaking JavaScript",
+		"n": "",
+		"u": "http://speakingjs.com/es5/index.html",
+		"t": [
+			"javascript",
+			"books",
+			"ebook",
+			"learning"
+		],
+		"dt": "2015-10-04T04:16:39Z"
+	},
+	{
+		"a": "charliecalvert",
+		"d": "Read Understanding ECMAScript 6 | Leanpub",
+		"n": "",
+		"u": "https://leanpub.com/understandinges6/read/",
+		"t": [
+			"javascript",
+			"ebook",
+			"js",
+			"es6",
+			"ecmascript6",
+			"free",
+			"ecmascript"
+		],
+		"dt": "2015-10-04T04:10:18Z"
+	},
+	{
+		"a": "charliecalvert",
+		"d": "Turnkey Linux for NodeJs",
+		"n": "",
+		"u": "https://www.turnkeylinux.org/nodejs",
+		"t": [
+			"nodejs",
+			"node.js",
+			"javascript"
+		],
+		"dt": "2015-10-04T03:29:39Z"
+	},
+	{
+		"a": "charliecalvert",
+		"d": "Node.js Twitter Page",
+		"n": "",
+		"u": "https://twitter.com/nodejs",
+		"t": [
+			"nodejs",
+			"node.js",
+			"javascript"
+		],
+		"dt": "2015-10-04T16:27:04Z"
+	},
+	{
+		"a": "charliecalvert",
+		"d": "Twitter Bootstrap Github twbs/bootstrap",
+		"n": "",
+		"u": "https://github.com/twbs/bootstrap",
+		"t": [
+			"bootstrap",
+			"html",
+			"css",
+			"javascript"
+		],
+		"dt": "2015-10-04T03:12:48Z"
+	},
+	{
+		"a": "charliecalvert",
+		"d": "Node Js Home Page",
+		"n": "",
+		"u": "https://nodejs.org/",
+		"t": [
+			"javascript",
+			"node.js",
+			"library",
+			"framework",
+			"nodejs",
+			"node",
+			"development"
+		],
+		"dt": "2015-10-04T16:26:21Z"
+	},
+	{
+		"a": "charliecalvert",
+		"d": "Bootstrap",
+		"n": "",
+		"u": "http://getbootstrap.com/",
+		"t": [
+			"bootstrap",
+			"javascript",
+			"web",
+			"framework",
+			"css"
+		],
+		"dt": "2015-10-04T03:10:25Z"
+	},
+	{
+		"a": "charliecalvert",
+		"d": "Adventures in JavaScript Development",
+		"n": "",
+		"u": "http://rmurphey.com/",
+		"t": [
+			"javascript",
+			"developer",
+			"blog"
+		],
+		"dt": "2015-01-16T21:29:09Z"
+	},
+	{
+		"a": "charliecalvert",
+		"d": "WebPlatform.org — Your Web, documented",
+		"n": "",
+		"u": "http://www.webplatform.org/",
+		"t": [
+			"html",
+			"css",
+			"reference",
+			"html5",
+			"javascript",
+			"development",
+			"web"
+		],
+		"dt": "2012-10-08T22:39:50Z"
+	}
+];
