e3699894b98f975981d7d56724807097f0b52ab6
Charlie on rohan-gate at home <charlie@elvenware.com>
2015-10-21 15:33:01 -0700

    ElvenCode

--- JavaScript/Design/JadeMixinComplete/routes/index.js
+++ JavaScript/Design/JadeMixinComplete/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Jade Mixin Complete' });
 });
 
 module.exports = router;
---
+++ JavaScript/ElvenCode/index.js
@@ -0,0 +1,4 @@
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
+
