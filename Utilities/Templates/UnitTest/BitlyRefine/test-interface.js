2b023162ab6926c1b8e20a29eee250e53d46fd73
Charlie on rohan-gate at home <charlie@elvenware.com>
2015-11-14 13:44:24 -0800

    Updating the unit tests for help with fixtures

--- Utilities/Templates/UnitTest/BitlyRefine/test-interface.js
+++ Utilities/Templates/UnitTest/BitlyRefine/test-interface.js
@@ -2,44 +2,11 @@
  * Created by charlie on 11/11/15.
  */
 
-var utils = {
-    getAttributesFromElement: function(element) {
-        'use strict';
-        var atts = element.attributes;
-        var n = atts.length;
-        var arr = [];
-
-        for (var i = 0; i < n; i++) {
-            arr.push(atts[i].nodeName);
-        }
-
-        return arr;
-    },
-
-    showAttributes: function(element, arr) {
-        'use strict';
-        console.log(arr);
-        $.each(arr, function(index, value) {
-            console.log(value + ': ' + $(element).attr(value));
-        });
-    },
-
-    showHtmlElements: function(inputElements) {
-        'use strict';
-        console.log('Keys:', Object.keys(inputElements));
-        $.each(inputElements, function(index, inputElement) {
-            console.log(Object.keys($(inputElement)));
-            console.log($(inputElement).context);
-        });
-    }
-};
-
-describe('Test Jade Interface for Program Interface', function() {
+fdescribe('Test Jade Interface for Program Interface', function() {
     'use strict';
 
     beforeEach(function() {
-        jasmine.getFixtures().fixturesPath = 'base/spec/javascripts/fixtures/';
-        loadFixtures('dest.html');
+        elfFixtureUtil.loadFixture('fixture.html', 'test-interface.js');
     });
 
     it('shows we can get the attributes of keywordLink', function() {
@@ -50,8 +17,8 @@ describe('Test Jade Interface for Program Interface', function() {
 
     it('shows we can get all the attributes of the text input keywordLink', function() {
         var element = document.getElementById('keywordLink');
-        var arr = utils.getAttributesFromElement(element);
-        // utils.showAttributes(element, arr);
+        var arr = elfTestUtils.getAttributesFromElement(element);
+        // elfTestUtils.showAttributes(element, arr);
         expect(arr.length).toBe(6);
     });
 
@@ -63,13 +30,13 @@ describe('Test Jade Interface for Program Interface', function() {
 
     it('shows we can get at least 2 radio inputs', function() {
         var inputs = $('input:radio');
-        //utils.showHtmlElements(inputs);
+        //elfTestUtils.showHtmlElements(inputs);
         expect(inputs.length).toBeGreaterThan(1);
     });
 
     it('shows we can get at least 2 checkbox inputs', function() {
         var inputs = $('input:checkbox');
-        // utils.showHtmlElements(inputs);
+        // elfTestUtils.showHtmlElements(inputs);
         expect(inputs.length).toBeGreaterThan(1);
     });
 
@@ -86,21 +53,21 @@ describe('Test Jade Interface for Program Interface', function() {
     it('shows we have a div with an id of dataSource', function() {
         var dataSource = $('#dataSource');
         // console.log('length:', dataSource.length);
-        // utils.showHtmlElements(dataSource);
+        // elfTestUtils.showHtmlElements(dataSource);
         expect(dataSource.length).toBe(1);
     });
 
     it('shows a div of dataSource has radio with id of localData', function() {
         var dataSource = $('#dataSource #localData');
         // console.log('length:', dataSource.length);
-        // utils.showHtmlElements(dataSource);
+        // elfTestUtils.showHtmlElements(dataSource);
         expect(dataSource.length).toBe(1);
     });
 
     it('shows a div of dataSource has radio with id of cloudData', function() {
         var dataSource = $('#dataSource #cloudData');
         // console.log('length:', dataSource.length);
-        // utils.showHtmlElements(dataSource);
+        // elfTestUtils.showHtmlElements(dataSource);
         expect(dataSource.length).toBe(1);
     });
 });
--- Utilities/Templates/UnitTest/BitlyRefine/test-jade.js
+++ Utilities/Templates/UnitTest/BitlyRefine/test-jade.js
@@ -2,94 +2,45 @@
  * Created by charlie on 11/10/15.
  */
 
-describe('Test Jade', function() {
+fdescribe('Test Jasmine-JQuery Fixture Loading', function() {
     'use strict';
 
     beforeEach(function() {
-        jasmine.getFixtures().fixturesPath = 'base/spec/javascripts/fixtures/';
+        elfFixtureUtil.loadFixture('fixture.html', 'test-jade');
     });
 
     it('checks that we can run tests in Test Jade', function() {
-        //expect(true).toBeTruthy();
-        var foo = $('#foo');
-        //console.log(jasmine.getFixtures().fixturesPath);
-        //expect(foo).toBeEmpty();
+        var checkBoxPrivate = document.getElementById('checkBoxPrivate');
+        var name = $(checkBoxPrivate).attr('name');
+        var id = $(checkBoxPrivate).attr('id');
+        var type = $(checkBoxPrivate).attr('type');
+        expect(type).toBe('checkbox');
+        expect(name).toBe('checkBoxPrivate');
     });
 
-    it('shows we can load a fixture with jasmine-jquery now', function() {
-        jasmine.getFixtures().fixturesPath = '/base/spec/javascripts/fixtures/';
-        loadFixtures('temp01.html');
-        var foo = $('#my_fixture').html();
-        console.log(foo);
-        expect($('#my-fixture')).toBeTruthy();
-    });
-
-    it('shows we can load fixture temp01.html with jasmine-jquery', function() {
-        jasmine.getFixtures().fixturesPath = '/base/spec/javascripts/fixtures/';
-        loadFixtures('temp01.html');
-        var foo = $('#bar01').html();
-        console.log(foo);
-        expect(foo).toBe('foo1');
-    });
-
-    it('shows we can load a fixture temp.html with jasmine-jquery', function() {
-        jasmine.getFixtures().fixturesPath = '/base/spec/javascripts/fixtures/';
-        loadFixtures('temp.html');
-        var test01 = $('#test01');
-        expect(test01).toBeEmpty();
-    });
-
-    it('shows we can load fixture test02', function() {
-        jasmine.getFixtures().fixturesPath = '/base/spec/javascripts/fixtures/';
-        loadFixtures('temp.html');
-        var test01 = $('#test02').html();
-        expect(test01).toBe('test02');
-    });
-
-    it('shows we can get the attributes of keywordLink from dest.html', function() {
-        jasmine.getFixtures().fixturesPath = '/base/spec/javascripts/fixtures/';
-        loadFixtures('dest.html');
+    it('shows we can get the attributes of keywordLink from fixture.html', function() {
         var klink = $('#keywordLink');
         var name = $(klink).attr('name');
-
         expect(name).toBe('keywordLink');
     });
 
-    it('shows we can get all the attributes of keywordLink from dest.html', function() {
-        var el;
-        var arr = [];
-
-        jasmine.getFixtures().fixturesPath = '/base/spec/javascripts/fixtures/';
-        loadFixtures('dest.html');
-
-        el = document.getElementById('keywordLink');
-        var atts = el.attributes;
-        var n = atts.length;
-
-        for (var i = 0; i < n; i++) {
-            arr.push(atts[i].nodeName);
+    /*
+    it('shows we can get all the attributes of keywordLink from fixture.html', function() {
+        var el = document.getElementById('keywordLink');
+        var attributes = el.attributes;
+        console.log(attributes);
+        var props = Object.keys(attributes);
+        expect(props.length).toBe(7);
+        for (var i = 0; i < attributes.length; i++) {
+            console.log(attributes[i]);
         }
-        console.log(arr);
-        $.each(arr, function(index, value) {
-            console.log(value + ': ' + $(el).attr(value));
-        });
-        expect(arr.length).toBe(6);
-    });
+        var placeholder = $(el).attr('placeholder');
+        expect(placeholder).toBe('keywordLink');
+
+
+    }); */
 
     /*
-    it('shows we can load a fixture with html2js', function() {
-        //jasmine.getFixtures().fixturesPath = '/base/spec/javascripts/fixtures/';
-        //console.log(jasmine.getFixtures().fixturesPath);
-        var foo = window.__html__;
-        setFixtures(foo['base/javascripts/fixtures/temp.html']);
-        // $('#my-fixture').myTestedPlugin();
-        expect($('#my-fixture')).toBeTruthy();
-        var elfDiv = $('#test01');
-        console.log(elfDiv);
-        expect(elfDiv).toBeEmpty();
-    });
-
-
     it('shows we can load a fixture', function() {
         var foo = window.__html__;
         console.log(typeof foo);
