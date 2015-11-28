5627403f3196ce64ef82849269e8a8f50259f549
Charlie on forestpath <charlie@elvenware.com>
2015-11-28 11:56:56 -0800

    asdf

--- Utilities/Templates/UnitTest/NaoExplorer/test-fixture.js
+++ Utilities/Templates/UnitTest/NaoExplorer/test-fixture.js
@@ -2,8 +2,6 @@
  * Created by charlie on 11/23/15.
  */
 
-
-
 describe('Test Fixture Suite', function() {
 
     'use strict';
@@ -19,23 +17,49 @@ describe('Test Fixture Suite', function() {
     });
 
     it('expects a button with an id of #leftArm', function() {
-        var button = $('#leftArm');
-        // console.log(Object.getOwnPropertyNames(button).sort());
-        expect(button.selector).toBe('#leftArm');
+        var button = document.getElementById('leftArm')
+        var id = $(button).attr('id');
+        expect(id).toBe('leftArm');
+    });
+
+    it('expects a button with a caption of Left Arm', function() {
+        var button = document.getElementById('leftArm');
+        var caption = button.innerHTML
+        expect(caption).toBe('Left Arm');
+    });
+
+    it('expects a button with an caption of Get Behaviors', function() {
+        var button = $('#behaviors');
+        expect(button[0].innerHTML).toBe('Get Behaviors');
     });
 
     it('expects a button with an id of #behaviors', function() {
         var button = $('#behaviors');
-        expect(button.length).toBe(1);
+        expect($(button[0]).attr('id')).toBe('behaviors');
     });
 
     it('expects a button with an id of #parse', function() {
-        var button = $('#parse');
-        expect(button.length).toBe(1);
+        var button = document.getElementById('parse')
+        var id = $(button).attr('id');
+        expect(id).toBe('parse');
+    });
+
+    it('expects a button with a caption of Parse', function() {
+        var button = document.getElementById('parse');
+        var caption = button.innerHTML
+        expect(caption).toBe('Parse');
     });
 
     it('expects a button with an id of #network', function() {
-        var button = $('#network');
-        expect(button.length).toBe(1);
+        var button = document.getElementById('network')
+        var id = $(button).attr('id');
+        expect(id).toBe('network');
     });
+
+    it('expects a button with a caption of Network', function() {
+        var button = document.getElementById('network');
+        var caption = button.innerHTML
+        expect(caption).toBe('Network');
+    });
+
 });
