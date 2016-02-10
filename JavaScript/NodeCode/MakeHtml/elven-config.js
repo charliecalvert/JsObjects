af4fae194bf77615f0d3f180062a6d00e87dd448
Charlie on RohanElf <charlie@elvenware.com>
2016-02-09 22:10:49 -0800

    Updated MakeHtml for branch

---
+++ JavaScript/NodeCode/MakeHtml/elven-config.js
@@ -0,0 +1,63 @@
+/**
+ * @author Charlie
+ */
+
+var fs = require('fs');
+var os = require('os');
+var utils = require('isit322-calvert').utils;
+
+function getConfigName() { 'use strict';
+	var configFileName = "ElvenConfig.json";
+	var configName = '';
+	if (os.platform() === 'darwin') {
+		configName = process.env.HOME + '/.config/' + configFileName;
+	} else if (os.platform() === 'linux') {
+		configName = process.env.HOME + '/.config/' + configFileName;
+	} else if (os.platform() === 'win32') {		
+		configName = process.env.USERPROFILE + "\\Config\\" + configFileName;
+	}
+	return configName;
+}
+
+function reportError(err) { 'use strict';
+	console.log("*********************************");
+	console.log(err);
+	console.log("*********************************");
+	console.log("Error condition in elven-config.js!");
+	console.log("This program requires a config file");
+	console.log("Please put ElvenConfig.json somewhere we can find it.");
+	console.log("Contrl-C to abort");
+	console.log("Error condition!");
+	console.log("*********************************");
+}
+
+function elvenConfig() {
+
+}
+
+elvenConfig.configData = {};
+elvenConfig.loaded = false;
+
+elvenConfig.load = function(callback) {
+	'use strict';
+	var configName = getConfigName();
+	try {
+    	console.log(configName);
+        utils.readFile(configName, function(result) {
+			elvenConfig.loaded = true;
+            elvenConfig.configData = JSON.parse(result.result);
+			console.log('In load: ', JSON.stringify(elvenConfig.configData, null, 4));
+			if (callback) {
+				callback(elvenConfig.configData);
+			}
+        });
+	} catch(e) {
+		console.log(e);
+	}
+};
+
+elvenConfig.get = function(property) {
+	return elvenConfig.configData.calvert[property];
+};
+
+module.exports = elvenConfig;
--- JavaScript/NodeCode/MakeHtml/index.js
+++ JavaScript/NodeCode/MakeHtml/index.js
@@ -2,14 +2,57 @@
  * Created by charlie on 1/1/16.
  */
 
-var walker = require('./walker');
+var walker = require('elven-site-tools').walker;
+var fs = require('fs');
+var config = require('./elven-config');
 
-function walk() {
-    var directoryToWalk = process.env.HOME + '/Documents/AllTest';
-    walker.buildFileReport(directoryToWalk, '.md', function(report) {
-        var directories = walker.getDirectories(report);
-        walker.makePage(directoryToWalk, directories, report, null);
+function walk(directoryToWalk, destinationDir) {
+
+    fs.access(directoryToWalk, fs.F_OK | fs.R_OK, function(err) {
+        if (err) {
+            console.log('Could not find', directoryToWalk);
+            response.sendStatus(401);
+        } else {
+            console.log('start', directoryToWalk);
+            walker.buildFileReport(directoryToWalk, '.md', function(report) {
+                console.log('build');
+                var directories = walker.getDirectories(report);
+                try {
+                    walker.makePage(directoryToWalk, destinationDir, directories, report, function (masterListOfNames, htmlFilesWritten) {
+                        var report = {
+                            result: 'success',
+                            destinationDir: destinationDir,
+                            directories: directories,
+                            masterListOfNames: masterListOfNames,
+                            htmlFilesWritten: htmlFilesWritten
+                        };
+                        console.log(report);
+                    });
+                } catch(e) {
+                    console.log("The error:", e);
+                }
+            });
+        }
     });
 }
 
-walk();
+function run(configSummary) {
+    //var directoryToWalk = process.env.HOME + '/Documents/AllTest';
+    var directoryToWalk = configSummary.baseDir + configSummary.siteDirs[0];
+    var destinationDir = configSummary.destinationDirs[0];
+    walk(directoryToWalk, destinationDir);
+}
+
+function runConfig() {
+    config.load(function() {
+        var baseDir = config.get('base-dir');
+        var siteDirs = config.get('site-dirs');
+        var destinationDirs = config.get('destination-dirs');
+        var configSummary = {"baseDir": baseDir, 'siteDirs': siteDirs, "destinationDirs": destinationDirs};
+        console.log("ConfigSummary:", configSummary);
+        run(configSummary);
+    });
+}
+
+
+runConfig();
