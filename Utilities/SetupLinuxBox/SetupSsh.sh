f89f257251ec2afaccefd3055d08977b99fdb53c
Charlie on RohanElf <charlie@elvenware.com>
2016-03-10 21:28:11 -0800

    SetupSsh

--- Utilities/SetupLinuxBox/SetupSsh.sh
+++ Utilities/SetupLinuxBox/SetupSsh.sh
@@ -1,5 +1,31 @@
 #! /bin/bash
 
-eval `ssh-agent`
-ssh-add ~/.ssh/YOUR SSH KEY HERE
-ssh-add ~/.ssh/YOUR SSH KEY HERE
+KEY_NAME=Test-Key
+SSH_DIR=$HOME/.ssh
+FULL_PATH=$SSH_DIR/$KEY_NAME
+PUBLIC_KEY=$FULL_PATH.pub
+AUTHORIZED_KEY_FILE=$SSH_DIR/authorized_keys
+
+cd $HOME
+pwd
+echo $FULL_PATH
+
+function setupKey() {
+	# -t: Type of key
+	# -P: Passphrase which is initially empty
+	# -f: The file name of the key to create
+	ssh-keygen -v -t rsa -P '' -f $FULL_PATH 
+	cat $PUBLIC_KEY >> $AUTHORIZED_KEY_FILE
+
+	# Change the passphrase
+	ssh-keygen -p -f $FULL_PATH
+}
+
+function removeKey() {
+	rm -v $FULL_PATH
+	rm -v $PUBLIC_KEY
+	rm -v $AUTHORIZED_KEY_FILE
+}
+
+# removeKey
+setupKey
