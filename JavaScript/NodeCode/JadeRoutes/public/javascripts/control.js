a7d837675344870221018f66542b566249fcc30d
Charlie on RohanElf <charlie@elvenware.com>
2016-06-15 15:38:19 -0700

    Loading Jade

--- JavaScript/NodeCode/JadeRoutes/public/javascripts/control.js
+++ JavaScript/NodeCode/JadeRoutes/public/javascripts/control.js
@@ -1,10 +1,50 @@
 $(document).ready(function() { 'use strict';
 
+    /**********************************
+     * LOADING JADE
+     **********************************/
     $('#routeOne').click(function() {
         $('#elf-view').load('/main-page');
     });
 
     $('#routeTwo').click(function() {
         $('#elf-view').load('/renewables/main-page');
-    })
+    });
+
+    /**********************************
+     * CALLING SERVER SIDE FUNCTIONS
+     **********************************/
+
+    $('#routeThree').click(function() {
+        $.getJSON('/renewables/', function (response) {
+            $('#debug').html(JSON.stringify(response, null, 4));
+        })
+            .fail(function (a, b, c) {
+                console.log('Error', a, b, c);
+                $('#debug').html('Error occured: ', a.status);
+            })
+            .done(function () {
+                console.log('second success');
+            })
+            .always(function () {
+                console.log('complete');
+            });
+    });
+
+    $('#routeFour').click(function() {
+        $.getJSON('/renewables/some-function', function(response) {
+            $('#debug').html(JSON.stringify(response, null, 4));
+        })
+            .fail(function(a, b, c) {
+                console.log('Error', a, b, c);
+                $('#debug').html('Error occured: ', a.status);
+            })
+            .done(function() {
+                console.log('second success');
+            })
+            .always(function() {
+                console.log('complete');
+            });
+    });
+
 });
--- JavaScript/NodeCode/JadeRoutes/routes/renewables.js
+++ JavaScript/NodeCode/JadeRoutes/routes/renewables.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(request, response) {
+    response.send({result: 'succecss', description: 'home route'})
+});
+
+router.get('/some-function', function(request, response) {
+    response.send({result: 'succecss', description: 'some function'})
+});
+
 router.get('/:id', function (request, response) {
     'use strict';
     response.render('renewables/' + request.params.id, {
