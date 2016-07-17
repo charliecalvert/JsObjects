a4740b88226f99458d9168246702654abf0d7bba
Charlie on RohanElf <charlie@elvenware.com>
2016-07-17 16:43:15 -0700

    update index to match isit322

--- JavaScript/ElvenCode/index.js
+++ JavaScript/ElvenCode/index.js
@@ -1,12 +1,13 @@
+exports.elfUtils = require('./elf-utils');
+exports.elfLog = require('./elf-log');
+exports.elfConfig = require('./elf-config');
+exports.walker = require('./walker');
+
+
+
+
+
 exports.printMsg = function() {
     'use strict';
     console.log('ElvenCode');
 };
-
-exports.elfUtils = require('./elf-utils');
-
-exports.walker = require('./walker');
-
-exports.elfLog = require('./elf-log');
-
-exports.elfConfig = require('./elf-config');
