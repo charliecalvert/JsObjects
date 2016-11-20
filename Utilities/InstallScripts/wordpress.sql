74c4decbecb3ec37c95d890bd2e4bbbfd9d5a2c5
Charlie <you@example.com>
2016-11-20 12:41:14 -0800

    Wordpress

---
+++ Utilities/InstallScripts/wordpress.sql
@@ -0,0 +1,5 @@
+create database if not exists wordpress;
+use wordpress
+GRANT ALL PRIVILEGES ON wordpress.* TO "root"@"localhost" IDENTIFIED BY "foobar";
+FLUSH PRIVILEGES;
+show grants for 'root'@'localhost';
