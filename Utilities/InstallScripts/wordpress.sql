3aaaee1bedf829fe20f6e4a80a67c1ba4904c873
Charlie on RohanElf <charlie@elvenware.com>
2016-12-13 12:23:10 -0800

    wordpress

--- Utilities/InstallScripts/wordpress.sql
+++ Utilities/InstallScripts/wordpress.sql
@@ -1,23 +1,6 @@
-#! /bin/bash
+create database if not exists wordpress;
+use wordpress
 
-if [ -z "$WORDPRESS_DIR" ]; then
-        echo "WORDPRESS_DIR is unset";
-        exit 1
-else
-        echo "WORDPRESS_DIR is set to '$WORDPRESS_DIR'";
-fi
-
-if [ -z "$WORDPRESS_DATA_PASSWORD" ]; then
-        echo "WORDPRESS_DATA_PASSWORD is unset."
-        exit 1
-else
-        echo "WORDPRESS_DATA_PASSWORD is set.";
-fi
-
-create database if not exists "$WORDPRESS_DIR";
-use "$WORDPRESS_DIR"
-
-
-GRANT ALL PRIVILEGES ON wordpress.* TO "root"@"localhost" IDENTIFIED BY "$WORDPRESS_DATA_PASSWORD";
+GRANT ALL PRIVILEGES ON wordpress.* TO "root"@"localhost" IDENTIFIED BY "foobar";
 FLUSH PRIVILEGES;
 show grants for 'root'@'localhost';
