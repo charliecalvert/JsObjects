bfbaef352ff754ff4efe3d41a2a049b3de1b0cc1
Charlie on RohanElf <charlie@elvenware.com>
2017-03-08 20:25:01 -0800

    update ElvenCode on NPM to have latest utils

--- JavaScript/ElvenCode/jasmine-runner.js
+++ JavaScript/ElvenCode/jasmine-runner.js
@@ -3,13 +3,17 @@
  */
 
 var Jasmine = require('jasmine');
-var SpecReporter = require('jasmine-spec-reporter');
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 var noop = function() {};
 
 var jrunner = new Jasmine();
 jrunner.configureDefaultReporter({
     print: noop
 }); // remove default reporter logs
-jasmine.getEnv().addReporter(new SpecReporter()); // add jasmine-spec-reporter
+jasmine.getEnv().addReporter(new SpecReporter({  // add jasmine-spec-reporter
+  spec: {
+    displayPending: true
+  }
+})); // add jasmine-spec-reporter
 jrunner.loadConfigFile(); // load jasmine.json configuration
 jrunner.execute();
