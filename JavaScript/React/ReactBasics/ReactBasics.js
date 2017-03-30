9f697ec65525bf24e7f5703d80794d3bdb12feab
Charlie on RohanElf <charlie@elvenware.com>
2017-03-30 10:04:21 -0700

    ReactBasics

---
+++ JavaScript/React/ReactBasics/ReactBasics.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class ReactBasics extends React.Component {
+    render() {
+        return <h1>An H1 element in a React Component</h1>
+    }
+}
+
+
---
+++ JavaScript/React/ReactBasics/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ReactBasics } from './ReactBasics.js';
+//import { ReactBasics } from './ReactBasicsStatelessFunctional.js';
+
+ReactDOM.render(<ReactBasics/>, document.getElementById('root'));
