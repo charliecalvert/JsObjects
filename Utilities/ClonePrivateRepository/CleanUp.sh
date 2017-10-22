ec7b75e20fbccf624a02865a5436305f965cf161
Charlie on RohanElf <charlie@elvenware.com>
2017-10-22 07:29:17 -0700

    Simple View Cleanup

--- Utilities/ClonePrivateRepository/CleanUp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# If you modified this script, you could use it
-# during debug sessions when you are planning your
-# deployment.
-cd ~/
-rm -rf .ssh/CharlieMainKey.pem
-rm -rf Git/test03-calvert
-rm bin/PullGitRepository.sh
--- Utilities/ClonePrivateRepository/CreateSshDir.sh
+++
@@ -1,7 +0,0 @@
-SSH_DIR=~/.ssh
-
-
-if [ ! -d "$BIN_DIR" ]; then
-	/bin/mkdir $SSH_DIR
-	/bin/chmod 700 $SSH_DIR
-fi
