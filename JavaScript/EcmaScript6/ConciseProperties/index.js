4755474b2899f8f1c3e00c4b455ae8fe92b4b579
Charlie on Mint Home <charlie@elvenware.com>
2017-11-05 16:49:32 -0800

    ConciseProperties

---
+++ JavaScript/EcmaScript6/ConciseProperties/index.js
@@ -0,0 +1,20 @@
+const m = 3;
+const n = 2;
+
+const myObject = {
+	m: m,
+	n: n
+};
+
+console.log(myObject);
+
+const x = 5;
+const y = 7;
+
+const myConciseObject = {
+	x,
+	y
+}
+
+console.log(myConciseObject);
+
