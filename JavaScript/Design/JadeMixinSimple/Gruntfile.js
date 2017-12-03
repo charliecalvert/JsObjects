6d09f77fca48be5776a28651cfe2d3393751a880
Charlie on Mint Home <charlie@elvenware.com>
2017-12-03 10:27:23 -0800

    Updated JadeMixinSimple

--- JavaScript/Design/JadeMixinSimple/Gruntfile.js
+++ JavaScript/Design/JadeMixinSimple/Gruntfile.js
@@ -31,27 +31,10 @@ module.exports = function(grunt) {
             }
         },
 
-        'jsbeautifier': {
-            files: ['**/*.js', '!**/node_modules/**', '!**/components/**'],
-            options: {
-                'indentSize': 4
-            }
-        },
-
-        karma: {
-            karma: {
-                configFile: 'karma.conf.js'
-            }
-        }
-
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-jscs');
-    grunt.loadNpmTasks('grunt-jsbeautifier');
-    grunt.loadNpmTasks('grunt-karma');
-    grunt.registerTask('beautify', ['jsbeautifier']);
-    grunt.registerTask('check', ['beautify', 'jscs', 'jshint']);
-    grunt.registerTask('test', ['jshint', 'karma']);
+    grunt.registerTask('check', ['jscs', 'jshint']);
 };
