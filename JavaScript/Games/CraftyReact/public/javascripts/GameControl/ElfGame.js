4a0cfc54d04af235653e33317601c7a079610e46
Charlie on Mint Home <charlie@elvenware.com>
2018-02-18 16:31:55 -0800

    Angular is completely removed from the project

--- JavaScript/Games/CraftyReact/public/javascripts/GameControl/ElfGame.js
+++ JavaScript/Games/CraftyReact/public/javascripts/GameControl/ElfGame.js
@@ -1,80 +1,93 @@
 /* jshint browser: true */
 
-angular.module('elfGameMod', [])
-    .factory('elfGameService',
-        function() {
-            'use strict';
-            // Initialize and start our game
+const ElfGame = (function() {
+    'use strict';
+    // Initialize and start our game
 
-            const people = new People();
-            const gameEventService = new GameEvent();
 
-            return {
+    let people = null;
+    let gameEventService = null;
 
-                map_grid: null,
+    function ElfGame() {
+        people = new People();
+        gameEventService = new GameEvent();
 
-                misses: 0,
+    }
 
-                bushHits: 0,
+//const people = new People();
+//const gameEventService = new GameEvent();
 
-                defaultMapGrid: {
-                    width: 18,
-                    height: 12,
-                    tile: {
-                        width: 32,
-                        height: 32
-                    }
-                },
+//const elfGameService = {
 
-                villages: [],
+    ElfGame.prototype.getService = function() {
+        return {
+            map_grid: null,
 
-                encounters: new Encounters(gameEventService),
-                gameMessages: new GameMessages(gameEventService),
+            misses: 0,
 
-                newVillage: function(village) {
-                    village.tower = people.tower();
-                    return this.villages.push(village);
-                },
+            bushHits: 0,
 
-                goLeft: function() {
-                    Crafty.trigger('goLeft', Crafty);
-                    return true;
-                },
-
-                stopMove: function() {
-                    Crafty.trigger('stopMove', Crafty);
-                    return true;
-                },
-
-                // Get width of the game screen in pixels
-                width: function() {
-                    return this.map_grid.width * this.map_grid.tile.width;
-                },
-
-                // Get height of the game screen in pixels
-                height: function() {
-                    return this.map_grid.height * this.map_grid.tile.height;
-                },
-
-                initMapGrid: function(mapGrid) {
-                    this.map_grid = mapGrid;
-                },
-
-                start: function(mapGrid) {
-                    //utility.addStringFormat();
-                    // Start crafty
-                    const gameDiv = document.getElementById("gameBoard");
-                    if (mapGrid) {
-                        this.map_grid = mapGrid;
-                    } else {
-                        this.map_grid = this.defaultMapGrid;
-                    }
-                    gameWrap.startGame(gameDiv, this);
+            defaultMapGrid: {
+                width: 18,
+                height: 12,
+                tile: {
+                    width: 32,
+                    height: 32
                 }
-            };
-        })
-    .controller('ElfController', function($scope, elfGameService) {
-        'use strict';
-        elfGameService.start();
-    });
+            },
+
+            villages: [],
+
+            encounters: new Encounters(gameEventService),
+            gameMessages: new GameMessages(gameEventService),
+
+            newVillage: function(village) {
+                village.tower = people.tower();
+                return this.villages.push(village);
+            },
+
+            goLeft: function() {
+                Crafty.trigger('goLeft', Crafty);
+                return true;
+            },
+
+            stopMove: function() {
+                Crafty.trigger('stopMove', Crafty);
+                return true;
+            },
+
+            // Get width of the game screen in pixels
+            width: function() {
+                return this.map_grid.width * this.map_grid.tile.width;
+            },
+
+            // Get height of the game screen in pixels
+            height: function() {
+                return this.map_grid.height * this.map_grid.tile.height;
+            },
+
+            initMapGrid: function(mapGrid) {
+                this.map_grid = mapGrid;
+            },
+
+            start: function(mapGrid) {
+                //utility.addStringFormat();
+                // Start crafty
+                const gameDiv = document.getElementById("gameBoard");
+                if (mapGrid) {
+                    this.map_grid = mapGrid;
+                } else {
+                    this.map_grid = this.defaultMapGrid;
+                }
+                gameWrap.startGame(gameDiv, this);
+            }
+        };
+    }
+
+    return ElfGame;
+})();
+
+const elfGame = new ElfGame();
+elfGame.getService().start();
+
 
