a38e1be5499ba9662ce99b0307c7b63a7714b9cd
Charlie on Mint Home <charlie@elvenware.com>
2018-03-03 10:32:41 -0800

    No more jquery in MongoTalk02

--- Data/MongoTalk02/public/index.js
+++ Data/MongoTalk02/public/index.js
@@ -1,13 +1,9 @@
 
-function showDataOld() {
-
-	$.getJSON('/read', function(data) {
-		console.log(data);
-		for (var i = 0; i < data.length; i++) {
-			$("#mongoData").append('<li>' + JSON.stringify(data[i]) + '</li>');
-		}
-	});
-
+function appendToList(text) {
+	var ul = document.getElementById("mongoData");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(text));
+  ul.appendChild(li);
 }
 
 function showData() {
@@ -15,13 +11,13 @@ function showData() {
 		.then(function(response){ return response.json() })
 		.then(function(json) {
 			for (var i = 0; i < json.length; i++) {
-				$("#mongoData").append('<li>' + JSON.stringify(json[i]) + '</li>');
+				appendToList(JSON.stringify(json[i]));
+				// $("#mongoData").append('<li>' + JSON.stringify(json[i]) + '</li>');
 			}
 		})
 
 }
 
-
-$(document).ready(function() {
+window.onload = function() {
 	showData();
-});
+};
