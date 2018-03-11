11d0f9e692ef557aec7e2d383e837712d67e0e6d
Charlie on Mint Home <charlie@elvenware.com>
2018-03-11 10:10:35 -0700

    MongoHttp01 now working

--- Data/MongoHttp01/index.js
+++ Data/MongoHttp01/index.js
@@ -1,37 +1,42 @@
 angular.module('customResourceDemo', ['mongoMod'])
-.constant('CONFIG', {
-    DB_NAME: 'elvenlab01',
-    API_KEY: 'PUT YOUR API KEY HERE'
-})
-.factory('presidents', function (mongolabResource) {
-    return mongolabResource('Presidents');
-})
-.controller('CustomResourceCtrl', function ($scope, presidents) {
+    .constant('CONFIG', {
+        DB_NAME: 'elvenlab01',
+        MONGO_URL: 'https://api.mongolab.com/api/1/databases/',
+        API_KEY: 'duUXXd0giOpJM-Fqyk9hP2_PDeQV7SRT'
+    })
+    .factory('presidents', function(mongolabResource) {
+        return mongolabResource('Presidents');
+    })
+    .controller('CustomResourceCtrl', function($scope, presidents) {
 
-    presidents.query().then(function(presidentData){
-      $scope.presidents = presidentData;
+        function showPresidents() {
+            presidents.query().then(function(presidentData) {
+                $scope.presidents = presidentData;
+            });
+        }
+
+        $scope.addPresident = function() {
+            var pres = {
+                presidentName: 'John Quincy Adames',
+                termStart: 1825,
+                termEnd: 1829
+            };
+            new presidents(pres)
+                .save(function(president, $scope) {
+                    showPresidents();
+                })
+        };
+
+        $scope.deletePresident = function() {
+            var indexOfItemToDelete = 5;
+            $scope.presidents[indexOfItemToDelete].remove(function(deletedObject, headers) {
+                showPresidents();
+            }, function(err) {
+                console.log("error: " + err.data.message);
+            });
+
+        };
+
+        showPresidents();
     });
-
-    $scope.addPresident = function () {
-      var pres = { 
-        presidentName: 'John Quincy Adames', 
-        termStart: 1825, 
-        termEnd: 1829 };
-      new presidents(pres)
-          .$save(function(president, $scope) {
-              //this.$apply();
-          })
-    };
-    
-    $scope.deletePresident = function() {
-        var indexOfItemToDelete = 5;
-        // if (indexOfItemToDelete < $scope.presidents.length) {}
-        $scope.presidents[indexOfItemToDelete].$remove(function(deletedObject, headers) {
-            $scope.presidents.splice(indexOfItemToDelete, 1);
-            $scope.presidentsLength = $scope.presidents.length;
-        }, function(err) {
-            console.log("error: " + err.data.message);  
-        });
-    };
-  });
 
