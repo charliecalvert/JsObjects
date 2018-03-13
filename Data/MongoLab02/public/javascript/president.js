dede70041ffb173d51e23c0b42523b8cdea8ccdf
Charlie on Mint Home <charlie@elvenware.com>
2018-03-13 08:15:17 -0700

    In the midst on MongoLab02

---
+++ Data/MongoLab02/public/javascript/president.js
@@ -0,0 +1,47 @@
+angular.module('pres', ['ngResource'])
+    .factory('presidents', function($resource, mlabSettings) {
+        console.log('Presidents factory called');
+
+
+        return new Promise(function(resolve, reject) {
+            mlabSettings
+                .then(function(response) {
+
+                    var Presidents = $resource(mlabSettings.url, mlabSettings.apiKey,
+                        {
+                            update: {method: 'PUT'}
+                        });
+
+                    Presidents.prototype.updateMe = function(callback, errorCallback) {
+                        console.log("update called");
+                        return Presidents.update(
+                            {id: this._id.$oid},
+                            angular.extend({}, this, {_id: undefined}),
+                            callback,
+                            errorCallback);
+                    };
+
+                    Presidents.prototype.getPresidentName = function() {
+                        return this.presidentName;
+                    };
+
+                    Presidents.prototype.getTermStart = function() {
+                        return this.termStart;
+                    };
+
+                    Presidents.prototype.getTermEnd = function() {
+                        return this.termEnd;
+                    };
+
+                    Presidents.prototype.remove = function(cb, errorcb) {
+                        return Presidents.remove({id: this._id.$oid}, cb, errorcb);
+                    };
+
+                    Presidents.prototype['delete'] = function(cb, errorcb) {
+                        return this.remove(cb, errorcb);
+                    };
+
+                    return Presidents;
+                })
+        });
+    });
