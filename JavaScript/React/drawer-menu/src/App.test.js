d5df11b951b3f02c641e6c32773f8ffca55f8401
Charlie on Mint Home <charlie@elvenware.com>
2018-03-20 10:49:49 -0700

    React tests

--- JavaScript/React/drawer-menu/src/App.test.js
+++ JavaScript/React/drawer-menu/src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Bar from './Bar';
+import Foo from './Foo';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider><App/></MuiThemeProvider>, div);
+});
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Bar/>, div);
+});
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Foo/>, div);
 });
