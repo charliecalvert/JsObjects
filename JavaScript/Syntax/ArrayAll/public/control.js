44413215d7cbaebb43091271b7fb5901b58dc572
Charlie on Mint Home <charlie@elvenware.com>
2018-03-22 15:29:11 -0700

    Adding readme

--- JavaScript/Syntax/ArrayAll/public/control.js
+++ JavaScript/Syntax/ArrayAll/public/control.js
@@ -2,8 +2,8 @@
 const app = require('./app');
 
 function appendToList(text, method) {
-    var ul = document.getElementById("appList");
-    var li = document.createElement("li");
+    const ul = document.getElementById("appList");
+    const li = document.createElement("li");
     li.appendChild(document.createTextNode(method + ": " + text));
     ul.appendChild(li);
 }
@@ -15,16 +15,3 @@ window.onload = function() {
     appendToList(app.arrayAll.mixedArray(), "mixedArray");
     appendToList(app.arrayAll.convertTextToArray("This is some text", "convertTextToArray"));
 };
-
-
-
-/*
-$(document).ready(function() {
-    'use strict';
-    //app.init();
-    app.init();
-    appendToList(app.arrayAll.arrayConstructor(), "arrayConstructor");
-    appendToList(app.arrayAll.mixedArray(), "mixedArray");
-    appendToList(app.arrayAll.convertTextToArray("This is some text", "convertTextToArray"));
-});
-*/
