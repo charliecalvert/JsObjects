20c2f5d241e1dceeec6f4e9eb41f24db7e769256
Charlie on Mint Home <charlie@elvenware.com>
2018-04-04 22:17:24 -0700

    app old

--- JavaScript/React/ElfExpressServer/client/tests/App.old.js
+++ JavaScript/React/ElfExpressServer/client/tests/App.old.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 
 import React from 'react';
-import { configure } from 'enzyme';
+import App from '../src/App';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
-import { shallow } from 'enzyme';
-import App from '../src/App';
+
 
 describe('App', () => {
   let wrapper;
