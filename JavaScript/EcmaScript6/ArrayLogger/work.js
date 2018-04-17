cd4d785d82fa7beec2271c733fa3dfdd35b2e987
Charlie on Mint Home <charlie@elvenware.com>
2018-04-16 21:14:17 -0700

    ArrayLogger initial commit

---
+++ JavaScript/EcmaScript6/ArrayLogger/work.js
@@ -0,0 +1,53 @@
+'use strict';
+
+function addEs5(operanda, operandb) {
+    return operanda + operandb;
+}
+
+const addEs6 = (operanda, operandb) => {
+    return operanda + operandb;
+};
+
+const addConcise = (a, b) => a + b;
+
+console.log(addEs5(2, 3));
+console.log(addEs6(5, 7));
+console.log(addConcise(12, 14));
+
+((numbers) => {
+    numbers.forEach((number) => {
+        console.log(number);
+    });
+})(['one', 'two', 'three']);
+
+((numbers) => {
+    for (let number of numbers) {
+        console.log(number);
+    }
+})(['four', 'five', 'six']);
+
+const logger = (numbers) => {
+    numbers.forEach((number) => {
+        console.log(number);
+    });
+};
+
+const loggerForOf = (numbers) => {
+    for(let number of numbers) {
+        console.log(number);
+    }
+};
+
+const myObject = { first: 'alpha', last: 'omega' };
+
+const loggerForIn = (myObject) => {
+    for (let property in myObject) {
+        if (myObject.hasOwnProperty(property)) {
+            console.log(property);
+        }
+    }
+};
+
+logger(['four', 'five', 'six']);
+loggerForOf(['seven', 'eight', 'nine']);
+loggerForIn(myObject);
