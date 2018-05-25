f9adcc196eb382db4e63d4d4112cf9beb5f81dbd
Charlie on Mint Home <charlie@elvenware.com>
2018-05-25 10:49:07 -0700

    Logger

--- JavaScript/React/ReactNativeTesting/EnzymeBasics/App.js
+++ JavaScript/React/ReactNativeTesting/EnzymeBasics/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 import Address from "./Address";
 import elfStyles from './elf-styles';
 
@@ -7,21 +7,48 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            foobar: 'barfoo'
-        }
+            test: 'unknown',
+            result: 'unknown'
+        };
+        this.fetchMicro = this.fetchMicro.bind(this);
     }
 
-    clickFoobar = () => {
-        this.setState({foobar: 'foobar1'});
+    clickTest = () => {
+        this.setState({test: 'foo is known'});
+    };
+
+    fetchMicro() {
+        const that = this;
+        const ips = ['ccalvert.com', '168.156.43.39'];
+        return fetch('http://' + ips[0] + ':30027/you-rang')
+            .then((response) => response.json())
+            .then(function(result) {
+                console.log(result);
+                that.setState({
+                    result: result.result
+                });
+            }).catch(function(ex) {
+            that.setState({foo: 'qux error'});
+        });
     };
 
     render() {
         return (
             <View style={elfStyles.container}>
-                <Text id="testClosed">{this.state.foobar}</Text>
-                <button id="textOpen" onClick={this.clickFoobar}>
+                <Text id="testClosed">Test: {this.state.test}</Text>
+                <Text>You Rang: {this.state.result}</Text>
+                <Button
+                    id="clickTest"
+                    title="Test"
+                    onPress={this.clickTest}>
                     Foobar
-                </button>
+                </Button>
+                <Button
+                    id="fetchMicro"
+                    title="Fetch Micro"
+                    onPress={this.fetchMicro}>
+                    Foobar
+                </Button>
                 <Address/>
             </View>
         );
--- JavaScript/React/ReactNativeTesting/EnzymeBasics/App.test.js
+++ JavaScript/React/ReactNativeTesting/EnzymeBasics/App.test.js
@@ -2,13 +2,32 @@ import React from 'react';
 import App from './App';
 import renderer from 'react-test-renderer';
 import {configure, shallow} from 'enzyme';
+import {Text} from 'react-native';
 import Adapter from 'enzyme-adapter-react-16';
+import elfLogger from './elf-logger';
 
 configure({adapter: new Adapter()});
 
 describe('App tests', function() {
 
-    const debug = false;
+    elfLogger.off();
+
+    beforeEach(() => {
+        global.fetch = jest.fn().mockImplementation(() => {
+            const promise = new Promise((resolve, reject) => {
+                resolve({
+                    ok: true,
+                    json: function() {
+                        return {
+                            test: 'status did go here',
+                            result: 'hello'
+                        };
+                    }
+                });
+            });
+            return promise;
+        });
+    });
 
     it('renders without crashing', () => {
         const rendered = renderer.create(<App/>).toJSON();
@@ -22,25 +41,45 @@ describe('App tests', function() {
 
     it('handles click on foobar with react', () => {
         const after = renderer.create(<App/>);
-        after.getInstance().clickFoobar();
+        after.getInstance().clickTest();
         expect(after.toJSON()).toMatchSnapshot();
     });
 
     it('renders default output with enzyme shallow', () => {
         const wrapper = shallow(<App/>);
-        if (debug) {
-            console.log(wrapper.debug());
-        }
+        console.log(wrapper.debug());
         expect(wrapper).toMatchSnapshot();
 
     });
 
     it('handles click on foobar with enzyme', () => {
         const wrapper = shallow(<App/>);
-        if (debug) {
-            console.log(wrapper.debug());
-        }
-        wrapper.find('#textOpen').simulate('click');
+        console.log(wrapper.debug());
+        wrapper.find('#clickTest').simulate('click');
         expect(wrapper).toMatchSnapshot();
     });
+
+    it('renders state of File paragraph after button click setimeout', () => {
+        const wrapper = shallow(<App/>);
+        const statusParagraph = (
+            <Text>You Rang: hello</Text>
+        );
+
+        return wrapper.instance().fetchMicro()
+            .then(() => {
+                wrapper.update();
+                console.log(wrapper.debug());
+                expect(wrapper.contains(statusParagraph)).toBe(true);
+            });
+
+    });
+
+    it('calls queryServer button click', () => {
+        const spy = jest.spyOn(App.prototype, 'fetchMicro');
+        const wrapper = shallow(<App/>);
+        wrapper.find('#fetchMicro').props().onPress();
+        console.log(wrapper.debug());
+        expect(spy).toHaveBeenCalled();
+    });
+
 });
