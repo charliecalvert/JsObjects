1d1f69aa56be0bba4903833f50f388d780fd2223
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-09-07 17:13:33 -0700

    update due to github warning

--- Data/CouchView02/routes/Couch.js
+++ Data/CouchView02/routes/Couch.js
@@ -7,7 +7,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-var servers = ['http://127.0.0.1:5984', 'http://192.168.2.19:5984'];
+var servers = ['http://127.0.0.1:5984', 'http://192.168.2.40:5984'];
 var serverIndex = 1;
 var nano = require('nano')(servers[serverIndex]);
 
