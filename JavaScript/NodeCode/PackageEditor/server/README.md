2d0f5dfcada704b6d867d416c6e82e5d127a514c
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-10-16 11:02:55 -0700

    Package editor starts

---
+++ JavaScript/NodeCode/PackageEditor/server/README.md
@@ -0,0 +1,3 @@
+# server
+
+by Charlie Calvert
---
+++ JavaScript/NodeCode/PackageEditor/server/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) { 'use strict';
+  res.render('index', { title: 'server' });
+});
+
+module.exports = router;
