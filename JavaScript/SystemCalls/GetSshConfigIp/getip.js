16d94ea9665c4c32483f3c354f3b048437090189
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-11-09 21:04:39 -0800

    GetSshConfigIp

---
+++ JavaScript/SystemCalls/GetSshConfigIp/getip.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const elfUtils = require('elven-code').elfUtils;
+
+/*
+elfUtils.readFile((process.env.HOME + '/.ssh/config'), (content) => {
+    var pattern= new RegExp('Host ec2-bc[\\s\\S]\\s*HostName\\s*(.*)');
+    const find = content.result.match(pattern);
+    console.log(find[1]);
+});
+*/
+
+
+const getSshIp = () => {
+    return new Promise(function (resolve, reject) {
+        elfUtils.readFile(process.env.HOME + '/.ssh/config')
+            .then((content) => {
+                var pattern = new RegExp('Host ec2-bc[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)');
+
+                const result = {};
+                const match = content.result.match(pattern);
+                for (let i = 1; i < 5; i++) {
+                    if (match[i].startsWith('HostName')) {
+                        var hostPattern = new RegExp('HostName\\s(.*)');
+                        result.hostName = match[i].match(hostPattern)[1];
+                    }
+                    if (match[i].startsWith('IdentityFile')) {
+                        const idPattern = new RegExp('IdentityFile\\s(.*)');
+                        const path = match[i].match(idPattern)[1];
+                        result.identityFile = path.substring(path.lastIndexOf('/') + 1, path.length)
+                    }
+                }
+                resolve(result);
+            })
+            .catch(reject);
+    });
+};
+
+getSshIp()
+    .then(result => {
+        console.log(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
