fbe38aa148877d934eeb621a0c269f5b9bbea3df
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-12-06 15:32:17 -0800

    packageeditor

--- JavaScript/NodeCode/PackageEditor/server/routes/index.js
+++ JavaScript/NodeCode/PackageEditor/server/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) { 'use strict';
-  res.render('index', { title: 'server' });
+router.get('/', function (req, res, next) {
+    'use strict';
+    res.render('index', {
+        title: 'Package Editor Server',
+        author: 'Charlie Calvert',
+        description: 'package editor JsObjects',
+        status: "incomplete"
+    });
 });
 
 module.exports = router;
