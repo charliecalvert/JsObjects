e95b9bf0164a88b0b5bdbe5c34bba43a02561bd2
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-12-27 18:22:37 -0800

    stuff

--- Data/CouchDb02/server.js
+++ Data/CouchDb02/server.js
@@ -1,31 +1,51 @@
+var setServer = require('../set-server');
 var request = require('request');
 
-var server = ['http://127.0.0.1:5984/', 'http://192.168.2.30:5984/'];
-var index = 0;
+//var server = ['http://127.0.0.1:5984/', 'http://192.168.2.30:5984/'];
+// var index = 0;
+var server = setServer.serverUrl + '/';
 
-console.log('Using: ' + server[index] + '\n');
+console.log('CouchDb02 Using: ' + server + '\n');
 
-request(server[index], function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body); 
-  }
-})
+function basics() {
+    request(server, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log("BODY:", body); 
+      } else {
+          console.log(error);
+      }
+    })
+}
 
-console.log('Available databases:\n');
 
-request(server[index] + '_all_dbs', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-})
 
-console.log('stats');
 
-request(server[index] + '_stats', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-      console.log(typeof body);
-      var data = JSON.parse(body);
-      console.log(JSON.stringify(data, null, 4));
-  }
-})
+const available_databases = true;
+if (available_databases) {
+    console.log('Available databases:\n');
 
+    request(server + '_all_dbs', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      } else {
+          console.log(error);
+      }
+    })
+
+}
+
+
+function stats() {
+    console.log('stats');
+
+    request(server + '_stats', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          console.log(typeof body);
+          var data = JSON.parse(body);
+          console.log(JSON.stringify(data, null, 4));
+      } else {
+          console.log(error);
+      }
+    })
+
+}
--- Data/set-server.js
+++ Data/set-server.js
@@ -3,13 +3,13 @@
  */
 
 var servers = ['http://127.0.0.1:5984',
-    'http://192.168.2.19:5984',
+    'http://192.168.86.115:5984',
     'http://192.168.2.20:5984',
     'http://168.156.41.96:5984'
 ];
 
 var dbName = 'game_data_calvert';
-var serverIndex = 2;
+var serverIndex = 1;
 var serverUrl = servers[serverIndex];
 console.log('Middleware attaching to database on: ', serverUrl);
 console.log('Middleware defaulting to database: ', dbName);
