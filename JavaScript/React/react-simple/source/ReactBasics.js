304763ada19c53aa5148d97ecf1c3ab818730887
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-04-05 10:18:28 -0700

    Scripts and ReactSimple CheckAll: 04-05-2019

---
+++ JavaScript/React/react-simple/source/ReactBasics.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+//process.env.NODE_ENV = 'development';
+
+export class ReactBasics extends React.Component {
+
+    getTwo(x) {
+        console.log(x);
+        return 2 * x;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>An H1 element in a React Component</h1>
+                <p>{this.getTwo(3)}</p>
+            </div>
+        );
+    }
+}
+
+
---
+++ JavaScript/React/react-simple/source/control.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ReactBasics } from './ReactBasics.js';
+
+ReactDOM.render(
+    <ReactBasics/>,
+    document.getElementById('root')
+);
