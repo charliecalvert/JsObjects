284db3e5fe2f56d9a0031e4f882373ea38c4440e
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-04-10 10:53:58 -0700

    do browserify

--- JavaScript/React/MaterialBrowserify/app.js
+++ JavaScript/React/MaterialBrowserify/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+//var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -22,12 +22,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+/*
 app.use(sassMiddleware({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public'),
     indentedSyntax: true, // true = .sass and false = .scss
     sourceMap: true
 }));
+*/
 
 
 app.use('/', index);
