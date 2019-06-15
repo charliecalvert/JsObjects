0de397e2ec2d84db0a238acb3770cdd661010343
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-06-15 11:31:23 -0700

    Add admin.apps.length duplicate app check

--- JavaScript/Firebase/ElfExpressFirestore/routes/verify-db.js
+++ JavaScript/Firebase/ElfExpressFirestore/routes/verify-db.js
@@ -5,32 +5,33 @@ let loggedIn = false;
 //'firebase-adminsdk-2p1h1@prog270-calvert.iam.gserviceaccount.com';
 function init() {
     loggedIn = true;
-    const firebaseApp = admin.initializeApp({
-        credential: admin.credential.applicationDefault(),
-    });
-    console.log('INITIALIZE FIREBASE ADMIN', firebaseApp);
+    if (admin.apps.length === 0) {
+        admin.initializeApp({
+            credential: admin.credential.applicationDefault(),
+        });
+    }
     return admin.firestore();
 }
 
 function verifyToken(token, url) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         if (!loggedIn) {
             init(url);
         }
         admin
             .auth()
             .verifyIdToken(token)
-            .then(function(decodedToken) {
+            .then(function (decodedToken) {
                 console.log('UID', JSON.stringify(decodedToken, null, 4));
                 console.log('MAIN SERVER QUX YOU RANG CALLED');
                 resolve(decodedToken);
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log(error);
                 reject(error);
             });
     });
 }
 
-module.exports.verifyToken=verifyToken;
-module.exports.init=init;
+module.exports.verifyToken = verifyToken;
+module.exports.init = init;
