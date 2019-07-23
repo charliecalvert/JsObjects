289838d928a19a3b54adccce272e4f4ba270fb50
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-07-23 11:32:52 -0700

    Canvas working

--- JavaScript/Graphics/CanvasSimple02/Canvas01.js
+++ JavaScript/Graphics/CanvasSimple02/Canvas01.js
@@ -2,39 +2,46 @@ var context;
 var context02;
 var image;
 
-function loadImage(callback)
-{
+function loadImage(callback) {
     'use strict';
 	console.log('loadImage called');
 	image = new Image();
 	image.onload = function() {
 		callback(image);
-	}
-	image.src = "images/cscGarden.png";
+	};
+	image.src = "./cscGarden.png";
 }
 
-function doLoad()
-{
+
+
+function doLoad() {
     'use strict';
 	console.log('doLoad called');
 	loadImage(function(image) {
-		$("#blitButton").prop('disabled', false);
-		$("#drawButton").prop('disabled', false);
-		$("#loadButton").prop('disabled', true);
+		const blitButton = document.getElementById('blitButton');
+		blitButton.disabled = false;
+		document.getElementById("drawButton").disabled = false;
+		document.getElementById("loadButton").disabled = true;
 	});
 
 	doLoader();
 }
 
-function doLoader()
-{
+function drawMe(image, x, y) {
+	const rectSize = 25;
+	context.drawImage(image, rectSize, 0, rectSize, rectSize,
+		x, y, rectSize, rectSize);
+	blitButton.disabled = false;
+}
+function doLoader() {
     'use strict';
 	console.log('doLoader called');
+
 	image = new Image();
-	image.src = "images/cscGarden.png";
-	$(image).load(function() {
-		$("#blitButton").prop('disabled', false);
-	});
+	image.onload = () => {
+		drawMe(image, 0, 0)
+	};
+	image.src = "cscGarden.png";
 }
 
 function draw() {
@@ -43,32 +50,31 @@ function draw() {
 	context02.drawImage(image, 0, 0);
 }
 
-function blitTest()
-{
+function blitTest() {
     'use strict';
 	console.log('blitTest called');
 	// var imageData = context.getImageData(0, 0, 25, 25);
-	for (var j = 0; j < 6; j++)
-		for (var i = 0; i < 12; i++)
+	for (let j = 0; j < 6; j++)
+		for (let i = 0; i < 12; i++)
 		{
 			context.drawImage(image, 0, 50, 25, 25, i * 25, j * 25, 25, 25);
 			context02.drawImage(image, 0, 25, 25, 25, i * 25, j * 25, 25, 25);
 		}
 }
 
-$('document').ready(function () {
+window.onload = () => {
     'use strict';
 
 	console.log('Document Ready called');
-	
+
 	// Without jQuery
 	var canvas01 = document.getElementById('canvas01');
 	canvas01.width = 250;
 	context = canvas01.getContext('2d');
-	
-	// With jQuery
-	var canvas02 = $('#canvas02');
-	canvas02.get(0).width = 250;
-	context02 = canvas02.get(0).getContext('2d');
-});
+
+	// Without jQuery
+	var canvas02 = document.getElementById('canvas02');;
+	canvas02.width = 250;
+	context02 = canvas02.getContext('2d');
+};
 
