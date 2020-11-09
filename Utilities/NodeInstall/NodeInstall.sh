c9334cd96ead955964d9d54f61eb7627efefba94
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2020-11-09 13:43:57 -0800

    Node Install

--- Utilities/NodeInstall/NodeInstall.sh
+++ Utilities/NodeInstall/NodeInstall.sh
@@ -41,7 +41,10 @@ sudo apt-get install curl
 # curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
 # The new setup script for Node.js v13.X
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+# curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+
+# The new setup script for Node.js v14.X
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 # Install node
 sudo apt-get install -y nodejs
