7ad420a5a5a1d1810400d913c4da3d0d5e896e8c
charlie on WSL HP-Desktop <charlie@elvenware.com>
2022-03-13 13:58:29 -0700

    Clean up some dead code

--- JavaScript/React/ReactRouterDomBasics/source/index.js
+++ JavaScript/React/ReactRouterDomBasics/source/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
 import Routing from './Routing';
 import { BrowserRouter } from "react-router-dom";
 
