ad8945205cf2887f0dcac68993388f76c21c10fe
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-10 16:57:29 -0700

    update copy

--- JavaScript/NodeCode/AwsBasicS3/copy-to-chaio.sh
+++ JavaScript/NodeCode/AwsBasicS3/copy-to-chaio.sh
@@ -4,7 +4,7 @@ S3_CLOUD_IMAGES=s3://s3bucket01.elvenware.com/dev-images/cloud-images/
 DESTINATION=${CHARLIE_IO}/assets/images/.
 
 # FLAGS=--recursive --exclude "*" --include "Node0*"
-FLAGS='--recursive --exclude "*" --include "Azure*"'
+FLAGS='--recursive --include "Azure*" --exclude "*"'
 COMMAND="aws s3 cp ${S3_CLOUD_IMAGES} ${DESTINATION} ${FLAGS}"
 
 echo ${COMMAND}
