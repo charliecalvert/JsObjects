31aec71f50fb6b75ace0710878ac9ff546e9852b
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-28 09:48:28 -0700

    Adding snippets

--- JavaScript/clean-all.js
+++ JavaScript/clean-all.js
@@ -2,7 +2,20 @@
 // except node_modules and .git directories.
 // Usage: node clean-all.js
 
+import { execSync } from 'child_process';
+import fs from 'node:fs';
+import fs, { readdirSync, statSync } from 'node:fs';
+import { readFileSync } from 'node:fs';
+
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
+const startDir = process.cwd();
+const command = 'npm run clean';
+const dirs = fs.readdirSync(__dirname).filter((file) => {
+    return fs.statSync(path.join(__dirname, file)).isDirectory() &&
+        file !== 'node_modules' &&
+        file !== '.git';
+}
+);
