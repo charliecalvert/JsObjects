69aedebfa96058253e061586df3d0a6c3f22049f
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-29 15:15:03 -0700

    cleaning up packages

--- JavaScript/clean-package-json.sh
+++ JavaScript/clean-package-json.sh
@@ -6,4 +6,31 @@ grep -roh --include="*.json" \
     --exclude-dir=bower_components \
     --exclude-dir=.config --exclude-dir=.mozilla \
     --exclude-dir=.metadata "\"karma-*[^\"]*." \
-    | xargs npm rm
+    | xargs -r npm rm
+
+grep -roh --include="*.json" \
+    --exclude=bundle.js --exclude=package-lock.json \
+    --exclude-dir=node_modules --exclude-dir=static \
+    --exclude-dir=bower_components \
+    --exclude-dir=.config --exclude-dir=.mozilla \
+    --exclude-dir=.metadata "\"jasmine-*[^\"]*." \
+    | xargs -r npm rm && npm i jest --save-dev \
+    && npm i jest-cli --save-dev \
+    && mkdir -p __tests__ \
+    && cp ${ELF_TEMPLATES}/UnitTest/test-basic-module.js __tests__/elven-basic.test.js
+
+# grep -roh --include="*.json" \
+#     --exclude=bundle.js --exclude=package-lock.json \
+#     --exclude-dir=node_modules --exclude-dir=static \
+#     --exclude-dir=bower_components \
+#     --exclude-dir=.config --exclude-dir=.mozilla \
+#     --exclude-dir=.metadata "\"grunt-*[^\"]*." \
+#     | xargs npm rm
+
+grep -roh --include="*.json" \
+    --exclude=bundle.js --exclude=package-lock.json \
+    --exclude-dir=node_modules --exclude-dir=static \
+    --exclude-dir=bower_components \
+    --exclude-dir=.config --exclude-dir=.mozilla \
+    --exclude-dir=.metadata "\"jade-*[^\"]*." \
+    | xargs -r npm rm && npm i pug
--- Utilities/Templates/JavaScript/clean-package-json.sh
+++ Utilities/Templates/JavaScript/clean-package-json.sh
@@ -6,7 +6,7 @@ grep -roh --include="*.json" \
     --exclude-dir=bower_components \
     --exclude-dir=.config --exclude-dir=.mozilla \
     --exclude-dir=.metadata "\"karma-*[^\"]*." \
-    | xargs npm rm
+    | xargs -r npm rm
 
 grep -roh --include="*.json" \
     --exclude=bundle.js --exclude=package-lock.json \
@@ -14,10 +14,10 @@ grep -roh --include="*.json" \
     --exclude-dir=bower_components \
     --exclude-dir=.config --exclude-dir=.mozilla \
     --exclude-dir=.metadata "\"jasmine-*[^\"]*." \
-    | -q xargs npm rm && npm i jest --save-dev \
+    | xargs -r npm rm && npm i jest --save-dev \
     && npm i jest-cli --save-dev \
     && mkdir -p __tests__ \
-    && mv ${ELF_TEMPLATES}/UnitTest/test-basic-module.js __tests__/elven-basic.test.js
+    && cp ${ELF_TEMPLATES}/UnitTest/test-basic-module.js __tests__/elven-basic.test.js
 
 # grep -roh --include="*.json" \
 #     --exclude=bundle.js --exclude=package-lock.json \
@@ -33,4 +33,12 @@ grep -roh --include="*.json" \
     --exclude-dir=bower_components \
     --exclude-dir=.config --exclude-dir=.mozilla \
     --exclude-dir=.metadata "\"jade-*[^\"]*." \
-    | xargs npm rm && npm i pug
+    | xargs -r npm rm && npm i pug
+
+grep -roh --include="*.json" \
+    --exclude=bundle.js --exclude=package-lock.json \
+    --exclude-dir=node_modules --exclude-dir=static \
+    --exclude-dir=bower_components \
+    --exclude-dir=.config --exclude-dir=.mozilla \
+    --exclude-dir=.metadata "\"grunt-jscs*[^\"]*." \
+    | xargs -r npm rm
