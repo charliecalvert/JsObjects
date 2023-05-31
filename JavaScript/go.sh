7ed735fe95bd665e39e4f1bb4bbb5439b9658335
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-31 14:34:14 -0700

    Cleaned up EcmaScript6 packages.

--- JavaScript/go.sh
+++ JavaScript/go.sh
@@ -1,2 +1,2 @@
-cd /home/ubuntu/Git/JsObjects/JavaScript/NodeCode/NodeRoutesAngular
+cd /home/ubuntu/Git/JsObjects/JavaScript/EcmaScript6/ArrayLogger
 sh /home/ubuntu/Git/JsObjects/Utilities/Templates/JavaScript/clean-package-json.sh
