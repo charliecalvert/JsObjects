e8671c822a5633283a22db973762c7200e389b5c
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-06-08 14:43:28 -0700

    Git Heroku

---
+++ Utilities/Templates/GitScript/get-heroku.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+git clone https://git.heroku.com/calvert11.git
+git clone https://git.heroku.com/heroku-test-calvert.git
+git clone https://git.heroku.com/heroku-test-calvert-heroku-22.git
+git clone https://git.heroku.com/micro-first.git
+git clone https://git.heroku.com/micro-second.git
