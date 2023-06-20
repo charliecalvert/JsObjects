3b87c1ccae2583984c98561317caa5e4e7c7350e
charlie on us25 hyper-v <charlie@elvenware.com>
2023-06-20 23:23:24 +0000

    rolling, rolling, rolling rawhide!

--- Utilities/SetupLinuxBox/get-nginx.sh
+++ Utilities/SetupLinuxBox/get-nginx.sh
@@ -3,14 +3,14 @@
 sudo apt update
 sudo apt install nginx
 cd /etc/nginx/sites-available/
-cp ${ELF_TEMPLATES}/HtmlJson/ec2-nginx-simple .
+sudo cp -v ${ELF_TEMPLATES}/HtmlJson/ec2-nginx-simple .
 cd ../sites-enabled/
 sudo rm default
 sudo ln -s /etc/nginx/sites-available/ec2-nginx-simple default
 cd /var/www
 sudo mkdir elves
 cd elves
-cp ${ELF_TEMPLATES}/HtmlJson/default-index.html /var/www/tutorial/index.html
-sudo nano index.html
+sudo cp -v ${ELF_TEMPLATES}/HtmlJson/default-index.html /var/www/elves/index.html
+# sudo nano index.html
 sudo service nginx restart
 sudo service nginx status
