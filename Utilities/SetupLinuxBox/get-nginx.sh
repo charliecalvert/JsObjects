ee3f4de228522c57a118ec8a350a10e2e4af73d1
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-06-19 17:45:00 -0700

    update get-nginx.sh

--- Utilities/SetupLinuxBox/get-nginx.sh
+++ Utilities/SetupLinuxBox/get-nginx.sh
@@ -3,10 +3,10 @@
 sudo apt update
 sudo apt install nginx
 cd /etc/nginx/sites-available/
-sudo nano tutorial
+cp ${ELF_TEMPLATES}/HtmlJson/ec2-nginx-simple .
 cd ../sites-enabled/
 sudo rm default
-sudo ln -s /etc/nginx/sites-available/tutorial default
+sudo ln -s /etc/nginx/sites-available/ec2-nginx-simple default
 cd /var/www
 sudo mkdir elves
 cd elves
