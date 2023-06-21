a4bf04db39b951c737e21687d4a0e61e84ad3a05
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-06-21 07:59:37 -0700

    my bash aliases symbolic link added

--- Utilities/SetupLinuxBox/get-started-copies.sh
+++ Utilities/SetupLinuxBox/get-started-copies.sh
@@ -3,4 +3,5 @@
 scp ${ELF_UTILS}/SetupLinuxBox/GetStarted aws-spot:/home/ubuntu/.
 scp ${ELF_TEMPLATES}/GitScript/big-git-install-2023-05-08.sh aws-spot:/home/ubuntu/.
 scp /mnt/g/Shared/AwsConfigAll.zip aws-spot:/home/ubuntu/.ssh/.
+ln -s ~/Git/JsObjects/Utilities/SetupLinuxBox/.my_bash_aliases ~/.my_bash_aliases
 
