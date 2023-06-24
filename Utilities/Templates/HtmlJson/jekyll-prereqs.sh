f375d3e19cf86f9f3ad8948be0b970a7b6d77575
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-06-24 12:51:33 -0700

    JEKYLL prereqs

--- Utilities/Templates/HtmlJson/jekyll-prereqs.sh
+++ Utilities/Templates/HtmlJson/jekyll-prereqs.sh
@@ -25,6 +25,7 @@ sudo apt-get install ruby-full build-essential zlib1g-dev
 echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
-# source ~/.bashrc
+source ~/.bashrc
+sudo gem install jekyll bundler
 
 echo -e 'You should do this also:\n'${BIGreen}'source ~/.bashrc${Color_Off}'
