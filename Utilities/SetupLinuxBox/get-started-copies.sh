05488d262331cae408602876421247ebf3bb77a0
charlie on CascadeTrails <charlie@elvenwaree.com>
2024-08-03 13:20:22 -0700

    ncu updates all works

--- Utilities/SetupLinuxBox/get-started-copies.sh
+++ Utilities/SetupLinuxBox/get-started-copies.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+AWS=aws2403
+
 GIT_HOME=$HOME/Git
 JSOBJECTS=$GIT_HOME/JsObjects
 ELF_UTILS=$JSOBJECTS/Utilities
@@ -12,11 +14,11 @@ if [ -f ${ELF_SCRIPTS}/Colors.sh ]; then
     . ${ELF_SCRIPTS}/Colors.sh
 fi
 
-scp ${SETUP_LINUXBOX}/GetStarted aws-spot:/home/ubuntu/.
-scp ${ELF_TEMPLATES}/GitScript/big-git-install-2023-05-08.sh aws-spot:/home/ubuntu/.
-scp /mnt/g/Shared/AwsConfigAll.zip aws-spot:/home/ubuntu/.ssh/.
-ln -s ${SETUP_LINUXBOX}/.my_bash_aliases ~/.my_bash_aliases
+scp ${SETUP_LINUXBOX}/GetStarted ${AWS}:/home/ubuntu/.
+scp ${ELF_TEMPLATES}/GitScript/big-git-install-2023-05-08.sh ${AWS}:/home/ubuntu/.
+scp /mnt/g/Shared/AwsConfigAll.zip ${AWS}:/home/ubuntu/.ssh/.
+# ssh ${AWS} "ln -s ${SETUP_LINUXBOX}/.my_bash_aliases ~/.my_bash_aliases"
 
-if [ -f ${JEKYLL_PREREQS} ]; then
-    ./${JEKYLL_PREREQS}
-fi
+# if [ -f ${JEKYLL_PREREQS} ]; then
+#    ./${JEKYLL_PREREQS}
+# fi
