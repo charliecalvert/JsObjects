d31c8833cc1ba3a8dd22e6078cd726aa5817c95a
charlie on CascadeTrails <charlie@elvenwaree.com>
2024-08-11 17:05:21 -0700

    not working

--- JavaScript/React/simple-radio-buttons/src/App.js
+++ JavaScript/React/simple-radio-buttons/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import './App.css';
 
 class App extends Component {
---
+++ JavaScript/React/simple-radio-buttons/webpack.config.js
@@ -0,0 +1,34 @@
+//var path = require('path');
+//var webpack = require('webpack');
+console.log(__dirname);
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-react'
+                            ]
+                        }
+                    }
+                ]
+            },
+            { test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    }
+};
