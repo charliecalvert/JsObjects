aaec8f8d35abaa0b26886ed7b56964e43f6f7bdd
charlie on CascadeTrails <charlie@elvenwaree.com>
2024-08-14 17:06:09 -0700

    chore: Update npm dependencies and add Babel ESLint parser

--- JavaScript/React/simple-inputs/source/control.js
+++ JavaScript/React/simple-inputs/source/control.js
@@ -2,15 +2,15 @@
  * Created by charlie on 11/5/16.
  */
 
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './App';
 
 window.onload = function() {
-    ReactDOM.render(
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
         <div>
             <App />
-        </div>,
-        document.getElementById('root')
+        </div>
     );
 };
