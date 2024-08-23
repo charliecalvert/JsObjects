a9b3f23e2a6e549455c8d9ab96bc16c380ee6568
charlie on CascadeTrails <charlie@elvenwaree.com>
2024-08-23 10:07:33 -0700

    simple-express-jest

---
+++ JavaScript/React/simple-express-jest/app.js
@@ -0,0 +1,9 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = app;
---
+++ JavaScript/React/simple-express-jest/app.test.js
@@ -0,0 +1,11 @@
+// app.test.js
+const supertest = require('supertest');
+const app = require('./app');
+
+describe('GET /', () => {
+  it('should respond with "Hello World!"', async () => {
+    const response = await supertest(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello World!');
+  });
+});
