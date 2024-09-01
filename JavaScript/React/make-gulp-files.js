7fa8748ad53d050578d370827078fd6dd3afc8f1
charlie on CascadeTrails <charlie@elvenwaree.com>
2024-09-01 13:23:52 -0700

    Got to keep moving forward.  No matter what.

--- JavaScript/React/make-gulp-files.js
+++ JavaScript/React/make-gulp-files.js
@@ -2,7 +2,11 @@
 
 const gulp = require('gulp');
 const { exec } = require('child_process');
-const { logger } = require('./logger');
+const { log } = require('./logger');
+
+if (!log) {
+    throw new Error('Logger not found');
+}
 
 // List of folder names
 const folders = [
@@ -39,8 +43,8 @@ const folders = [
 folders.forEach((folder) => {
     gulp.task(folder, (done) => {
         exec(`cd ${folder} && npm run build`, (err, stdout, stderr) => {
-            logger(stdout);
-            logger(stderr);
+            log(stdout);
+            log(stderr);
             done(err);
         });
     });
@@ -48,3 +52,4 @@ folders.forEach((folder) => {
 
 // Default task to run all build tasks sequentially
 gulp.task('build-all', gulp.series(...folders));
+exports.default = gulp.series('build-all');
