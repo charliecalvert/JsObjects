987d812e14d193467b51165526e5fa03f85cb10b
charlie on CascadeTrails <charlie@elvenwaree.com>
2024-09-18 08:18:37 -0700

    Save your changes for this work in progress commit.

---
+++ JavaScript/React/simple-radio-buttons/dir-compare.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Check if at least two arguments are provided
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <directory1> <directory2> [exclude_pattern]"
+    exit 1
+fi
+
+DIR1=$1
+DIR2=$2
+EXCLUDE_PATTERN=$3
+# OUTPUT_FILE=$4
+
+# Check if the provided arguments are directories
+if [ ! -d "$DIR1" ]; then
+    echo "Error: $DIR1 is not a directory."
+    exit 1
+fi
+
+if [ ! -d "$DIR2" ]; then
+    echo "Error: $DIR2 is not a directory."
+    exit 1
+fi
+# Compare the directories, excluding specified file types if provided
+if [ -n "$EXCLUDE_PATTERN" ]; then
+    diff -rq --exclude=".git" --exclude="$EXCLUDE_PATTERN" "$DIR1" "$DIR2"
+else
+    echo "====================="
+    echo "No exclude pattern provided using elven default"
+    echo "====================="
+    diff -rq --exclude=".git" --exclude "node_modules" --exclude="*.save" --exclude="*.old" --exclude="*.sh" --exclude="README.md"  "$DIR1" "$DIR2"
+fi
+
+# Compare the directories, excluding specified file types if provided
+# if [ -n "$OUTPUT_FILE" ]; then
+#     eval diff -rq $EXCLUDE_OPTIONS "$DIR1" "$DIR2" > "$OUTPUT_FILE"
+#     DIFF_COUNT=$(wc -l < "$OUTPUT_FILE")
+# else
+#     DIFF_OUTPUT=$(eval diff -rq $EXCLUDE_OPTIONS "$DIR1" "$DIR2") > "output.txt"
+#     DIFF_COUNT=$(echo "$DIFF_OUTPUT" | wc -l)
+#     echo "$DIFF_OUTPUT"
+# fi
+
+# # Print the summary
+# echo "Summary: $DIFF_COUNT differences found."
+
+# Exit with the status of the diff command
+exit $?
--- JavaScript/React/simple-radio-buttons/webpack.config.js
+++ JavaScript/React/simple-radio-buttons/webpack.config.js
@@ -1,37 +1,38 @@
-//var path = require('path');
-//var webpack = require('webpack')
-// import HtmlWebpackPlugin from 'html-webpack-plugin';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-console.log(__dirname);
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/public/',
-        filename: 'bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /.(js)?$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react'
-                        ]
-                    }
-                },
-                exclude: /(node_modules|bower_components)/,
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader']
-            },
-        ],
-    }
+	entry: 'index.jsx',
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'index_bundle.js',
+	},
+	target: 'web',
+	devServer: {
+		port: '5000',
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		open: true,
+		hot: true,
+		liveReload: true,
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'public', 'index.html'),
+		}),
+	],
 };
