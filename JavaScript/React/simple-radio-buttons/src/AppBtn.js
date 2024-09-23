4c0b8c1f4eb612ae8a916651a79e79554fc06a76
charlie on CascadeTrails <charlie@elvenwaree.com>
2024-09-23 13:09:49 -0700

    wip

--- JavaScript/React/simple-radio-buttons/src/AppBtn.js
+++ JavaScript/React/simple-radio-buttons/src/AppBtn.js
@@ -1,81 +1,8 @@
-import React, { useState, Fragment } from 'react';
-// import ReactDOM from 'react-dom'
-import { log } from './logger.js';
-import './AppBtn.css';
+import React from 'react';
+import center from './AppBtn.css';
 
-log('AppBtn component loaded');
-
-// class AppBtn extends Component {
 function AppBtn() {
-    log('AppBtn component loaded');
-    // log('AppBtn component loaded', props
-    /*     constructor(props) {
-            super(props);
-            log('AppBtn component constructor', props);
-            this.state = {
-                checkedRadioButton: 'none',
-                message: 'none'
-            };
-            this.useState({ checkedRadioButton: 'none', message: 'none' });
-        } */
-
-    const [checkedRadioButton, setCheckedRadioButton] = useState('none');
-    const [message, setMessage] = useState('none');
-
-    const handleRadioChange = event => {
-        setCheckedRadioButton(event.target.value);
-    };
-
-    const useButtonSelection = () => {
-        setMessage("You've chosen " + checkedRadioButton);
-    };
-
-
-    const bookRadios = (
-        <div>
-            <input
-                type="radio"
-                name="book-radio"
-                value="RadioOne"
-                id="radio-one"
-                checked={checkedRadioButton === 'RadioOne'}
-                onChange={handleRadioChange}
-            />
-            <label htmlFor="radio-one" className="book-radio">
-                Book Radio 1
-            </label>
-            <input
-                type="radio"
-                name="book-radio"
-                value="RadioTwo"
-                id="radio-two"
-                checked={checkedRadioButton === 'RadioTwo'}
-                onChange={handleRadioChange}
-            />
-            <label htmlFor="radio-two" className="book-radio">
-                Book Radio 2
-            </label>
-        </div>
-    );
-
-    return (
-        <Fragment>
-            <div className="AppBtn">
-                <section>
-                    <hr />
-                    {bookRadios}
-                    <hr />
-                    <p>
-                        Selected radio button: {checkedRadioButton}
-                    </p>
-                    <p>Message: {message}</p>
-                    <button type="button" onClick={useButtonSelection}>
-                        Use Radio Button to set the message
-                    </button>
-                </section>
-            </div>
-        </Fragment>
-    );
+	return <h1 className="center">Hello world! I am using React</h1>;
 }
 
 export default AppBtn;
--- JavaScript/React/simple-radio-buttons/src/index.js
+++ JavaScript/React/simple-radio-buttons/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppBtn from './AppBtn.js';
+import AppBtn from './AppBtn.js.save';
 import * as serviceWorker from './serviceWorker';
 import { log } from './logger.js';
 
