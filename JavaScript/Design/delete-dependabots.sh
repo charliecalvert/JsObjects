15cc37c005727302b5986947c2b4f0f7b4d8615b
charlie on CascadeTrails <charlie@elvenwaree.com>
2024-10-19 11:20:10 -0700

    Add scripts to delete old dependabot branches

---
+++ JavaScript/Design/delete-dependabots.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Delete all dependabot branches
+git push origin --delete dependabot/npm_and_yarn/Cordova/ElvenGeo/minimatch-3.1.2
+git push origin --delete dependabot/npm_and_yarn/Cordova/ElvenGeo/plist-3.0.6
+git push origin --delete dependabot/npm_and_yarn/JavaScript/Design/FactorySimple01/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/Design/IBitDay2016/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/Design/OldAngular/AngularTestBackend/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/Design/OldAngular/AngularTestBackend/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/Design/OldAngular/AngularTestController/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/Design/OldAngular/AngularTestController/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/Design/OldAngular/AngularTestControllerAs/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/Design/OldAngular/AngularTestControllerAs/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/NodeCode/NodeRoutesQuery/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/React/gatsby-site/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/AngularFactoryTest/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/AngularFactoryTest/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/AsyncJsonReader/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/AsyncJsonReader/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/InjectHtmlFixture/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/InjectHtmlFixture/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine01/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine01/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine02/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine02/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine02a/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine02a/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine03/engine.io-6.4.2
+
---
+++ JavaScript/Design/delete-dependabots02.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# Delete all dependabot branches
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine03/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine04/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine04/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine06/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine06/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine08/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/Jasmine08/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineJQueryTest/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineJQueryTest/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineMockAjax/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineMockAjax/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineNoRequire01/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineNoRequire01/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineNoRequire02/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineNoRequire02/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineNode02/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineNode02/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineRequireJs/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineRequireJs/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineSpyOn/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/JasmineSpyOn/socket.io-parser-4.2.3
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/MochaRequireJsKarma/engine.io-6.4.2
+git push origin --delete dependabot/npm_and_yarn/JavaScript/UnitTests/MochaRequireJsKarma/socket.io-parser-4.2.3
